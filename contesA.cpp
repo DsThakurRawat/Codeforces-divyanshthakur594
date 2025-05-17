46a7301926bcceed37c1f6ba6a7ae8c944612e4b
DsThakurRawat <divyanshthakur594@gmail.com>
2025-05-17 20:19:11 +0530

    Auto-sync: 2025-05-17 20:19:11

--- contesA.cpp
+++ contesA.cpp
@@ -13,7 +13,9 @@ int main(){
         }
 
         for(int i =0; i < v.size(); i++){
-            if(v[i] == v[i+1]) 
+            if(v[i] == v[i+1]) {
+                
+            }
         }
 
     }
