5bbdc967540eb5d27c65727fc83bffd83d718663
DsThakurRawat <divyanshthakur594@gmail.com>
2025-05-25 00:36:05 +0530

    Auto-sync: 2025-05-25 00:36:05

--- TRAINING/hey.py
+++
@@ -1,7 +0,0 @@
-a = int(input("Enter a number: "))
-b = int(input("Enter another number: "))
-c = a + b
-print("The sum of", a, "and", b, "is", c)
-# This code takes two numbers as input from the user, adds them together, and prints the result.
-# The code contains a typo in the first line where 'intput' should be 'input'.
-# The code also has a comment explaining its functionality.
