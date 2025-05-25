f6a7d95ba30d75e9bd817060d4195ced6683377c
DsThakurRawat <divyanshthakur594@gmail.com>
2025-05-25 23:48:27 +0530

    Auto-sync: 2025-05-25 23:48:27

---
+++ zep.py
@@ -0,0 +1,8 @@
+# taking in put 
+for i in range(int(input())):
+    l,r,d,u = map(int, input().split())
+    #all side of square have equal length 
+    #and all inner angles are equal
+  # no two edge of polynomila intersect each other
+  
+    
