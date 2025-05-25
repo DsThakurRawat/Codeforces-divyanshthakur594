d0010d1603b76dbc901bf42cca7dac08c43161eb
DsThakurRawat <divyanshthakur594@gmail.com>
2025-05-25 23:52:31 +0530

    Auto-sync: 2025-05-25 23:52:31

--- zep.py
+++ zep.py
@@ -3,6 +3,12 @@ for i in range(int(input())):
     l,r,d,u = map(int, input().split())
     #all side of square have equal length 
     #and all inner angles are equal
-  # no two edge of polynomila intersect each other
-  
+   # no two edge of polynomila intersect each other
+   #check side of lenght and just done
+    if(l == r and r == d and d == u ):
+      print("Yes")
+    else:
+       print("No")
+         
+    
     
