6570fbbae4785e805928c5ddb7012bb7f5fe588f
DsThakurRawat <DsThakurRawat@users.noreply.github.com>
2025-05-26 00:54:43 +0530

    Auto-sync: 2025-05-26 00:54:43

---
+++ test.py
@@ -0,0 +1,10 @@
+# when s is transcribed as amog then singular amogus 
+# and the plural form is amogi 
+# given transcribed amog'u in singular form - > convert it in plural form
+# we just need to do two step
+# step1 : remove us from given string 
+# step 2 : add i to modified string 
+for _ in range(int(input())):
+    s = input()
+    print(s.replace("us","")+"i")
+    
