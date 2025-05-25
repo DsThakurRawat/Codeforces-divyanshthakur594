24e126471e6ec22b528efc9ca8fe4fa5a8684013
DsThakurRawat <divyanshthakur594@gmail.com>
2025-05-26 00:28:15 +0530

    Auto-sync: 2025-05-26 00:28:15

--- zep.py
+++ zep.py
@@ -1,11 +1,16 @@
+import sys
+input = sys.stdin.readline
+
+
 for _ in range(int(input())):
-    #give an integer n process all integers from 0 to n 
-    # check for each i if i % 3 ==  i % 5 print(fizzBuzz)
-    
-    count =0
-    for i in range(int(input())+1):
-        if( i % 3 == i % 5):
-            count = count+1
-        continue   
-            
-    print(count)
+        #give an integer n process all integers from 0 to n 
+        # check for each i if i % 3 ==  i % 5 print(fizzBuzz)
+        n = int(input())
+        
+        block = n // 15 
+        rem = n % 15
+
+        extra = sum(1 for i in range(rem + 1) if i % 3 == i % 5)
+
+        count = block*3 + extra
+        print(count)
