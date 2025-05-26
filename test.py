853b8c9cb17ff55760a6665b0b78553d2a7dcece
DsThakurRawat <DsThakurRawat@users.noreply.github.com>
2025-05-26 20:20:33 +0530

    Auto-sync: 2025-05-26 20:20:33

--- test.py
+++ test.py
@@ -1,13 +1,14 @@
 import  math 
 for _ in range(int(input())):
-    s = input()
+    s = input().stri()
     num = int(s)
     #now we need to check if numb can be writttens as num = (a+b)^2
     root = math.isqrt(num)
     if root * root == num:
-        a = 0
-        b = root
+        a = root // 2
+        b = root - a
         print(a, b)
+        
     else:
         print(-1)
         
