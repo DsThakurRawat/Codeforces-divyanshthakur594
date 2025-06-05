e2522cc2649a2bd977fbed56c60a982b4985edac
DsThakurRawat <DsThakurRawat@users.noreply.github.com>
2025-06-05 09:26:17 +0530

    Auto-sync: 2025-06-05 09:26:17

--- test.cpp
+++ test.cpp
@@ -6,10 +6,29 @@ using namespace std;
 int main(){
     string s;
     cin>>s;
-   string  lower_s = s.lower();
+    transform(s.begin(), s.end(), s.begin(), ::tolower);
+    // delete all vowels
+    string res ="";
+    for(int i =0; i<s.size(); i++){
+        if(s[i] != 'a' || s[i] != 'e' || s[i] != 'i' || s[i] != 'o'|| s[i] != 'u' ){
+            res = res + s[i];
+
+        }
+    }
+    // just want to add . before every consonant
+       string result= "";
+        for(auto c : res){
+            if(c != 'a' || c != 'e' || c != 'i' || c != 'o' || c =='u' ) result += '.';
+      
+           result +=c;
+
+        }
+        cout << result << endl; 
+
+ 
    
    
 
 
     
-}
+} 
