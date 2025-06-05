f04aa2551862a2c82100dfbbcd9463b0abaa9fa7
DsThakurRawat <DsThakurRawat@users.noreply.github.com>
2025-06-05 12:52:46 +0530

    Auto-sync: 2025-06-05 12:52:46

--- theatre.cpp
+++ theatre.cpp
@@ -7,8 +7,8 @@ int main(){
    
   long long n,m,a;
   cin >> n >> m >> a;
- int  r =( n+a-1)/a;
- int l = (m+a-1)/a;
+ long long   r =( n+a-1)/a;
+ long long  l = (m+a-1)/a;
   cout<< r*l << endl;
 return 0;
 
