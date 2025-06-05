ec103b2158d5beb886449c7dc1eda7b6830b5d61
DsThakurRawat <DsThakurRawat@users.noreply.github.com>
2025-06-05 09:28:09 +0530

    Auto-sync: 2025-06-05 09:28:09

--- test.cpp
+++ test.cpp
@@ -11,9 +11,10 @@ int main(){
     string res ="";
     for(int i =0; i<s.size(); i++){
         if(s[i] != 'a' || s[i] != 'e' || s[i] != 'i' || s[i] != 'o'|| s[i] != 'u' ){
-            res = res + s[i];
+        
 
         }
+            res = res + s[i];
     }
     // just want to add . before every consonant
        string result= "";
