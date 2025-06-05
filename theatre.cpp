acde81b26f5e8e6329f92aecf7c26f985f565275
DsThakurRawat <DsThakurRawat@users.noreply.github.com>
2025-06-05 12:48:47 +0530

    Auto-sync: 2025-06-05 12:48:47

---
+++ theatre.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+   // theatre squad in the capital city 
+   
+  long long n,m,a;
+  cin >> n >> m >> a;
+  cout<< ((n+a-1)*(m+a-1))/a*a << endl;
+
+
+
+    
+} 
