c87d4ce5cff630e649c577c88e489ddd5dcf3bab
DsThakurRawat <DsThakurRawat@users.noreply.github.com>
2025-06-05 09:36:25 +0530

    Auto-sync: 2025-06-05 09:36:25

--- 118A.cpp
+++ 118A.cpp
@@ -10,7 +10,7 @@ int main(){
     // delete all vowels
     string res ="";
     for(int i =0; i<s.size(); i++){
-        if(s[i] != 'a' && s[i] != 'e' && s[i] != 'i' && s[i] != 'o' && s[i] != 'u' && s[i] != 'y' && s[i] != 'w' ){
+        if(s[i] != 'a' && s[i] != 'e' && s[i] != 'i' && s[i] != 'o' && s[i] != 'u' && s[i] != 'y' ){
            res = res + s[i];
 
         }
