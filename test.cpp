7048e75dce117ca6e55f873df9b18003ec65e285
DsThakurRawat <DsThakurRawat@users.noreply.github.com>
2025-06-10 22:23:00 +0530

    Auto-sync: 2025-06-10 22:23:00

--- test.cpp
+++ test.cpp
@@ -1,30 +1,22 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-bool islucky(int n ){
-    if(n == 0) return false;
 
-    while(n > 0){
-        int digit = n % 10;
-        if(digit != 4 && digit != 7) return false;
 
-        n = n /10;
-    }
-    return true;
-}
+
+
 
 
 int main(){
-   int n;
-   cin >> n;
-   for(int i =1; i<=n; i++){
-    if(islucky(i) && n % i == 0){
-        cout << "Yes" << endl;
-        return 0;
-    }
+   int t;
+   cin >> t;
+   while(t--){ 
+      int n;
+      cin >> n;
+      
+
+
    }
-   cout<< "No" << endl;
-   return 0;
 
     
 } 
