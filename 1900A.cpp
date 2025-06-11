3bd63d0d615848f336a33bb50f2879d94780fb58
DsThakurRawat <DsThakurRawat@users.noreply.github.com>
2025-06-11 12:39:02 +0530

    Auto-sync: 2025-06-11 12:39:02

---
+++ 1900A.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+   int t;
+   cin >> t;
+   while(t--){ 
+    // need to find minimum nombe of operation
+    // . is empty cell
+    // # is blocked can't fill or can't removed
+    // if i have 3 continuos empty cell , answer is 2 else answer is count of all empty cell
+   
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+    bool coninuosempty = false;
+    int total_count =0;
+
+    for(int i = 0; i < n; i++ ){
+      if(i + 2 < n && s[i] == '.' && s[i+1] == '.' && s[i+2] == '.'){
+         
+      }
+    }
+     
+   
+      
+      
+
+
+   }
+
+    
+}
