71bbc326915a183e8c9fe7f5a9fb284e92407212
DsThakurRawat <DsThakurRawat@users.noreply.github.com>
2025-06-11 12:41:37 +0530

    Auto-sync: 2025-06-11 12:41:37

--- 1900A.cpp
+++ 1900A.cpp
@@ -19,8 +19,15 @@ int main(){
 
     for(int i = 0; i < n; i++ ){
       if(i + 2 < n && s[i] == '.' && s[i+1] == '.' && s[i+2] == '.'){
+         coninuosempty = true;
+         break;
          
       }
+      if(s[i] == "."){
+         total_count++;
+      }
+      if(coninuosempty) cout << 2 << endl;
+      else cout << total_count << endl;
     }
      
    
