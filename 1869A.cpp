2042a21354d7447634f394b8c85ccf0436ef280b
DsThakurRawat <DsThakurRawat@users.noreply.github.com>
2025-06-12 10:54:44 +0530

    Auto-sync: 2025-06-12 10:54:44

--- 1869A.cpp
+++ 1869A.cpp
@@ -5,7 +5,13 @@ int main(){
    int t;
    cin >> t;
    while(t--){ 
+      //  operation  -- select an index from 2 to n - 1 such tha ai-1 < ai and ai > ai+1;
+      // swap ai and ai+1
+      int n ;
+      cin >> n;
       
+      
+
       
 
 
