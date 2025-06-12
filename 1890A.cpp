246adb03ef98ee24ed9998e5eba65b1a26b3ef62
DsThakurRawat <DsThakurRawat@users.noreply.github.com>
2025-06-12 13:22:32 +0530

    Auto-sync: 2025-06-12 13:22:32

--- 1890A.cpp
+++ 1890A.cpp
@@ -6,11 +6,45 @@ int main(){
    cin >> t;
    while(t--){ 
      int n;
-     int arr[n];
-     for (int i = 0; i < n; i++) cin >> arr[i];
+    cin >> n;
+     // from statement says 
+     /* b1 + b2 = b2 + b3 
+     means b1 = b3
+     or specifically
+     bi + b(i+1) = b(i+1) + b(i + 2)     
+     mean 
+     bi = bi+2;
+     so b1 = b3
+        b2 = b4
+        b3=b5
+
 
      
+     */
+   
 
+     map<int,int>mpp;// will store frequency here
+
+     for (int i = 0; i < n; i++){
+       int x;
+       cin >> x;
+       mpp[x]++;
+
+     }
+     if(mpp.size() >= 3) cout << "YES" << endl;
+     else{
+      if(abs(mpp.begin()->second - mpp.rbegin() -> second) <= 1){
+         cout << "YES"<<endl;
+      }
+      else{
+         cout << "NO" << endl;
+      }
+
+
+     }
+       
+
+   
 
 
        
