5c42a8d0674c676b16d5099ee355904c8303199d
DsThakurRawat <DsThakurRawat@users.noreply.github.com>
2025-06-12 10:13:57 +0530

    Auto-sync: 2025-06-12 10:13:57

--- 1899A.cpp
+++ 1899A.cpp
@@ -12,7 +12,9 @@ int main(){
       else cout<< "First" << endl;
 
 
-      
+      // n % 3 == 0 [ 3,6,9,12...] if players do play optimally then there is no chance that the vanya can win in this case
+      // n % 3 == 1 [1,4,7,10...] if vanya want to win he will sub - 1 and hence num % 3 ==0
+      // n % 3 == 2 [2,5,8,11...] if vanya wants to win he will  definately add +1 and make n % 3 =0
 
       
 
