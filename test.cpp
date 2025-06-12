d20278dc328be8cc7e2dc87b77488fcd98797632
DsThakurRawat <DsThakurRawat@users.noreply.github.com>
2025-06-13 01:06:48 +0530

    Auto-sync: 2025-06-13 01:06:48

--- test.cpp
+++ test.cpp
@@ -6,16 +6,7 @@ int main(){
    cin >> t;
    while(t--){ 
 
-      // 4 2 3 5 
-      // 4 5 2 3
-       int n ;
-       cin >> n;
-       int a[n];
-       for(int i = 0; i < n; i++) cin >> a[i];
-
-       cout << (a[0] == 1 ? "YES" : "NO") << endl;
-       
-
+      
 
 
 
