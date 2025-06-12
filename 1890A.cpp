6bcf19e41740f2dc444b1db268c7168c19bd68f7
DsThakurRawat <DsThakurRawat@users.noreply.github.com>
2025-06-12 12:50:48 +0530

    Auto-sync: 2025-06-12 12:50:48

---
+++ 1890A.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+   int t;
+   cin >> t;
+   while(t--){ 
+     int n;
+     int arr[n];
+     for (int i = 0; i < n; i++) cin >> arr[i];
+
+     
+
+
+
+       
+
+
+
+
+      
+       
+      
+
+
+   }
+   return 0;
+
+    
+} 
