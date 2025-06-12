e8424bf581b0e7c8decca7cc91b3086b7e3488b0
DsThakurRawat <DsThakurRawat@users.noreply.github.com>
2025-06-13 01:06:47 +0530

    Auto-sync: 2025-06-13 01:06:47

---
+++ 2118C.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+   int t;
+   cin >> t;
+   while(t--){ 
+
+      // ummmmm
+      int n,k;
+      cin >> n >> k;
+      
+
+
+
+
+      
+       
+      
+
+
+   }
+   return 0;
+
+    
+} 
