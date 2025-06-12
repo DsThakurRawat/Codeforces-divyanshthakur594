d341cd6d0d9d67a86c83b5fe5e4e922bc19d23ab
DsThakurRawat <DsThakurRawat@users.noreply.github.com>
2025-06-12 18:34:47 +0530

    Auto-sync: 2025-06-12 18:34:47

---
+++ 1881A.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+   int t;
+   cin >> t;
+   while(t--){ 
+    int n,m;
+    string x,s;
+    cin >> x >> s;
+    
+    for(int i = 0; i < 6; i++){
+      if(x.find(s) != string::npos){
+         cout << i << endl;
+         return 0;
+      }
+    }
+    x +=x;
+
+
+    
+
+      
+
+
+
+
+      
+       
+      
+
+
+   }
+   cout << -1 << endl;
+   return 0;
+
+    
+} 
