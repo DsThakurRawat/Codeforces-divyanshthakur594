96ec6f09710e1e7d5ec96a9837fdbb46d9bdd09d
DsThakurRawat <DsThakurRawat@users.noreply.github.com>
2025-06-12 12:24:20 +0530

    Auto-sync: 2025-06-12 12:24:20

--- test.cpp
+++ test.cpp
@@ -1,22 +1,27 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-
-
-
-
-
-
 int main(){
    int t;
    cin >> t;
    while(t--){ 
-      int n;
-      cin >> n;
+
+      // 4 2 3 5 
+      // 4 5 2 3
+       int n ;
+       cin >> n;
+       int a[n];
+       for(int i = 0; i < n; i++) cin >> a[i];
+
+       cout << (a[0] == 1 ? "YES" : "NO") << endl;
+
+      
+       
       
 
 
    }
+   return 0;
 
     
 } 
