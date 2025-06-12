bb4ba6e8aee4a1191662311315ef09192481335d
DsThakurRawat <DsThakurRawat@users.noreply.github.com>
2025-06-13 01:06:54 +0530

    Auto-sync: 2025-06-13 01:06:54

--- 1881A.py
+++ 1881A.py
@@ -8,7 +8,16 @@ def solve():
             return
         x += x
     print(-1)
-
+    """
+    equivalent cpp code
+     for (int i = 0; i < 6; i++) {
+        if (x.find(s) != string::npos) {
+            cout << i << endl;
+            return 0;
+        }
+        x += x;
+    }
+    """
 
 for _ in range(int(input())):
     solve()
