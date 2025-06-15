4b1826d2024c489ff261634b305e340d301af98e
DsThakurRawat <DsThakurRawat@users.noreply.github.com>
2025-06-15 22:59:14 +0530

    Auto-sync: 2025-06-15 22:59:14

--- 1877A.cpp
+++ 1877A.cpp
@@ -6,23 +6,10 @@ int main(){
    cin >> t;
    while(t--){
     int n; // number  of teams
+    cin >> n;
 
     vector<int>v;
-    /*
-    approach and important facts
-    eff = goal by team - goal by opposition;
-    # they have given n-1 integers {a1,a2,a3,a(n-1)}
-    {t1 , t3} = {5,2}
-
-    t1-> t2
-         t3
-         t4
-         (s1 + s2+ s3) -[s1' + s2' + s3'] = a1
-         arg :
-         the answer is simply  -ve of sum of all the n-1 efficiency
-
-
-    */
+   
    int sum = 0;
     for(int i =0; i<n; i++){
         int x;
