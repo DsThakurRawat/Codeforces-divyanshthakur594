f3a23fd86a9111f89933ab3bef78b4067c1cb6ed
DsThakurRawat <DsThakurRawat@users.noreply.github.com>
2025-06-15 20:19:17 +0530

    Auto-sync: 2025-06-15 20:19:17

--- 1878A.cpp
+++ 1878A.cpp
@@ -5,7 +5,33 @@ int main(){
    int t;
    cin >> t;
    while(t--){ 
+    // if k is present in the array anywhere answer is yes
+    // if u take even single element  1->sized subarray that is a subsegment
+    //
+    int n,k;
+    cin >> n >> k;
+    vector < int > v;
+    for(int i =0; i < n; i++) {
+        int x;
+        cin >> x;
+        v.push_back(x);
+    }
+   int flag = 0;
+
+    for(int i =0; i < n; i++){
+        if( k == v[i]) {
+            flag = 1;
+            break;
     
+    }
+}
+ 
+
+ cout << ((flag != 0) ? "YES" : "NO") << endl;
+
+
+
+
 
       
 
