0457305dcb0011e12701901d6303adced9522587
DsThakurRawat <DsThakurRawat@users.noreply.github.com>
2025-06-15 19:37:30 +0530

    Auto-sync: 2025-06-15 19:37:30

--- 1881A.cpp
+++ 1881A.cpp
@@ -4,12 +4,13 @@ using namespace std;
 int main(){
    int t;
    cin >> t;
+   int ans;
    while(t--){ 
     int n,m;
     cin >> n >> m;
     string x,s;
     cin >> x >> s;
-    int ans=-1;
+     ans=-1;
     
     for(int i = 0; i < 6; i++){
       if(x.find(s) != string::npos){ // this line check wheather s exist inside the x 
