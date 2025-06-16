9524d4331cd12e7735fba84a1c8b43226344368a
DsThakurRawat <DsThakurRawat@users.noreply.github.com>
2025-06-16 10:50:33 +0530

    Auto-sync: 2025-06-16 10:50:33

--- 1862A.cpp
+++ 1862A.cpp
@@ -5,13 +5,25 @@ int main(){
    int t;
    cin >> t;
    while(t--){ 
+    int n;
+    cin >> n;
+    vector<int>v;
+
+    for(int i =0; i < n; i++){
+        int x;
+        cin >> x;
+        v.push_back(x);
+    }
+    /*   // now we need to guess 
+    // length of string a that is m
+    // in 2nd line output m integers a1,a2,a3,a4....am
 
       
 
 
 
 
-      
+      */
        
       
 
