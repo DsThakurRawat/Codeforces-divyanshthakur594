7690a0a3c4570d45c4d8ef8b7b713f9c0e59b72c
DsThakurRawat <DsThakurRawat@users.noreply.github.com>
2025-06-16 11:15:06 +0530

    Auto-sync: 2025-06-16 11:15:06

--- 1862A.cpp
+++ 1862A.cpp
@@ -17,6 +17,26 @@ int main(){
     /*   // now we need to guess 
     // length of string a that is m
     // in 2nd line output m integers a1,a2,a3,a4....am
+    expected time complexity
+    1s allows = 10^8 operations
+    1 test = 2s = 2 x 10^8 operation
+    sum of valuse of < 2x10^5
+    n1 values n2 values 
+    n1 + n2 + n3 +............ <= 2x10^5
+    possible tc
+    O(n^2)--- upper bound
+    ... and anyhting below it
+  -> every bi, if has been jotted from 
+  a , must be greater or equal to the 
+  prev
+     ______________________________________________________________________________________________________________________________________________________________________________________
+    |
+  ---
+    |________________________________________________________________________________________________________________________________________________________________________________________
+
+
+
+
 
       
 
