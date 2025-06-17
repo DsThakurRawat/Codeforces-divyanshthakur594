2ea7cf1e71b7cd65cc07d51f06dd3f3290ffe73e
DsThakurRawat <DsThakurRawat@users.noreply.github.com>
2025-06-17 21:33:16 +0530

    Auto-sync: 2025-06-17 21:33:16

--- 2121B.cpp
+++ 2121B.cpp
@@ -29,13 +29,13 @@ int main(){
 
    for(int i = 1; i < s.size()-1; i++){
         for(int j = 1; j + i < n; j++){
-     string b = s.substr(i, j);     
-         string a = s.substr(0, i);      
-        string c = s.substr(i+j);       
-            if(a.empty() || b.empty() || c.empty()) continue;
+     string s2 = s.substr(i, j);     
+         string s1 = s.substr(0, i);      
+        string s3 = s.substr(i+j);       
+            if(s1.empty() || s2.empty() || s3.empty()) continue;
 
-            string ac = a + c;              
-            size_t pos = ac.find(b);        
+            string ac = s1 + s3;              
+            size_t pos = ac.find(s2);        
             if(pos != string::npos){
                 flag = 1;
                 break;
