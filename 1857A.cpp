d7e05aee90dd8fc457736f08657857286472c8bd
DsThakurRawat <DsThakurRawat@users.noreply.github.com>
2025-06-17 10:23:53 +0530

    Auto-sync: 2025-06-17 10:23:53

---
+++ 1857A.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+   int t;
+   cin >> t;
+   while(t--){ 
+// in mathematics same parity means 
+// both even oe both odd
+   int n;
+   cin >> n;
+
+   vector < int > v;
+   for( int i = 0; i < n; i++){
+    int x;
+    cin >> x;
+    v.push_back(x);
+   }
+   // so just got it we know what to do and now probelam is how we will implement this 
+   // think like this we have to find 
+
+
+      
+   }
+
+
+
+      
+       
+      
+
+
+   
+   return 0;
+
+    
+} 
