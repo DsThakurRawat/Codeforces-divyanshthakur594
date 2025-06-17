e3a270a7a84cfe4586a90e778e8e236384cf5e33
DsThakurRawat <DsThakurRawat@users.noreply.github.com>
2025-06-17 12:00:36 +0530

    Auto-sync: 2025-06-17 12:00:36

--- 1853A.cpp
+++ 1853A.cpp
@@ -5,7 +5,21 @@ int main(){
    int t;
    cin >> t;
    while(t--){ 
-    
+    int n;
+    cin >> n;
+    vector < int > v;
+    for( int i = 0; i < n; i++){
+        int x;
+        cin >> x;
+        v.push_back(x);
+    }
+    /*
+    we will be given sorted
+    array we need output minimum process to make an array unsorted
+    nji
+    */
+
+
 
       
    }
