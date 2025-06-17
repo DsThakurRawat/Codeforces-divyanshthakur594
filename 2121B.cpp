5b0bbac46fe81d9788b3f93e9375d5ac091fd37f
DsThakurRawat <DsThakurRawat@users.noreply.github.com>
2025-06-17 21:35:37 +0530

    Auto-sync: 2025-06-17 21:35:37

--- 2121B.cpp
+++ 2121B.cpp
@@ -2,65 +2,44 @@
 using namespace std;
 
 int main(){
+   ios_base::sync_with_stdio(false);
+   cin.tie(NULL);
+   
    int t;
    cin >> t;
    while(t--){ 
-    // okay so a + b + c // both should be non-empty
-    // condition check brrrrro
-    /*
-    s = should made up of a + b + c
-    and 
-    b = a+c
-    // so we need to chek if there is such string where 
-    s = a + b + c
-    c = a+b;
-    // how 
-    s = a + b + a + b
-    s = 2*(a + b) = a+b + a+ b
-    s 
-
-
-    */
+    // ...existing code and comments...
+   
    int n;
    cin >> n;
    string s;
    cin >> s;
    int flag = 0;
 
-   for(int i = 1; i < s.size()-1; i++){
-        for(int j = 1; j + i < n; j++){
-     string s2 = s.substr(i, j);     
-         string s1 = s.substr(0, i);      
-        string s3 = s.substr(i+j);       
-            if(s1.empty() || s2.empty() || s3.empty()) continue;
-
-            string ac = s1 + s3;              
-            size_t pos = ac.find(s2);        
-            if(pos != string::npos){
+   // Optimized solution: Only need to check single characters
+   for(int i = 1; i < n-1; i++){
+        char b = s[i];  // middle character
+        
+        // Check if b appears in prefix
+        for(int j = 0; j < i && !flag; j++){
+            if(s[j] == b){
                 flag = 1;
                 break;
             }
         }
+        
+        // Check if b appears in suffix
+        for(int j = i+1; j < n && !flag; j++){
+            if(s[j] == b){
+                flag = 1;
+                break;
+            }
+        }
+        
         if(flag) break;
    }
-   if(flag) cout << "Yes" << endl;
-   else cout << "No" << endl;
-
-
-
-
-      
-   }
-
-
-
-      
-       
-      
-
-
    
+   cout << (flag ? "Yes" : "No") << '\n';
+   }
    return 0;
-
-    
-} 
+}
