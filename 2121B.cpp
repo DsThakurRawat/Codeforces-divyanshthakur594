4675f3a0e3fe88e45cde974e340add2dadfe303b
DsThakurRawat <DsThakurRawat@users.noreply.github.com>
2025-06-17 21:31:55 +0530

    Auto-sync: 2025-06-17 21:31:55

--- 2121B.cpp
+++ 2121B.cpp
@@ -25,22 +25,26 @@ int main(){
    cin >> n;
    string s;
    cin >> s;
-   int flag =0;
+   int flag = 0;
 
    for(int i = 1; i < s.size()-1; i++){
-    for(int j = 1; j + i < n ; j++){
-        string s1 = s.substr(i,j);
-        string s2 = s.substr(0, i);
-        string s3 = s.substr(i+j);
-        if(s1.empty() || s2.empty() || s3.empty()) continue;
+        for(int j = 1; j + i < n; j++){
+     string b = s.substr(i, j);     
+         string a = s.substr(0, i);      
+        string c = s.substr(i+j);       
+            if(a.empty() || b.empty() || c.empty()) continue;
 
-        string ac = s1 + s3;
-        size_t pos = ac.find(s2);
-        if(pos != string :: npos) flag = 1;
-    }
+            string ac = a + c;              
+            size_t pos = ac.find(b);        
+            if(pos != string::npos){
+                flag = 1;
+                break;
+            }
+        }
+        if(flag) break;
    }
-   if( flag == 1) cout << "YES" << endl;
-   else cout << "NO" << endl;
+   if(flag) cout << "Yes" << endl;
+   else cout << "No" << endl;
 
 
 
