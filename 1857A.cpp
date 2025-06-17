583677e5e1260a7ac9233869d02fead64caff24c
DsThakurRawat <DsThakurRawat@users.noreply.github.com>
2025-06-17 11:38:01 +0530

    Auto-sync: 2025-06-17 11:38:01

--- 1857A.cpp
+++ 1857A.cpp
@@ -9,11 +9,13 @@ int main(){
 // both even oe both odd
    int n;
    cin >> n;
+   int sum = 0;
 
    vector < int > v;
    for( int i = 0; i < n; i++){
     int x;
     cin >> x;
+    sum = sum + x;
     v.push_back(x);
    }
    // so just got it we know what to do and now probelam is how we will implement this 
@@ -23,8 +25,11 @@ int main(){
    // odd + even = odd
    // even + even = even
    // odd + odd = even
+   //If sum of all numbers is odd, then answer is NO, else YES....
+  (sum % 2 ==0) ? cout << "YES" << endl : cout << "NO" << endl;
+   
 
-
+ 
 
 
 
