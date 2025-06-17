64f3b9e9d28db54de99a5286a41aef1663fe6052
DsThakurRawat <DsThakurRawat@users.noreply.github.com>
2025-06-17 10:44:56 +0530

    Auto-sync: 2025-06-17 10:44:56

--- 1857A.cpp
+++ 1857A.cpp
@@ -18,6 +18,17 @@ int main(){
    }
    // so just got it we know what to do and now probelam is how we will implement this 
    // think like this we have to find 
+   // if the array contain atleast one even and at least one odd element -- yes
+   // else no
+    bool has_even = false, has_odd = false;
+
+
+   for(auto it : v){
+    (it % 2 == 0) ? has_even = true : has_odd = true;
+
+    
+   }
+ (has_even && has_odd) ? cout << "YES" << endl : cout << "NO" << endl;
 
 
       
