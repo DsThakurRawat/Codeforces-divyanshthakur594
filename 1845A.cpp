e9b6750f61f5f1d23c4b9aa957f0b639ddd11881
DsThakurRawat <DsThakurRawat@users.noreply.github.com>
2025-06-18 09:35:19 +0530

    Auto-sync: 2025-06-18 09:35:19

--- 1845A.cpp
+++ 1845A.cpp
@@ -28,7 +28,33 @@ int main(){
     */
    int n,k,x;
    cin >> n >> k >>x;
+   if( x != 1){
+    cout << "YES" << endl;
+    cout << n << endl;
+    for(int i = 0; i < n; i++) cout << 1 << " ";
+     cout << endl;
 
+   }
+   
+    else if ( k == 1 || (k == 2 && n % 2 == 1)){
+    cout <<"NO"<< endl;
+
+   }
+else {
+    cout << "YES" << endl;
+    cout << n / 2 << endl;
+    if( n % 2 == 1){
+        cout << 3 << " ";
+
+        for(int i =1; i < n/2; i++) cout << 2 << " ";
+    }
+
+
+    else {
+        for(int i = 0; i < n/2; i++) cout << endl;
+        cout << 2 << " ";
+    }
+}
 
 
       
