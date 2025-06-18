7c87e1e258329233b2e6ced12166a3dfda814764
DsThakurRawat <DsThakurRawat@users.noreply.github.com>
2025-06-18 09:03:33 +0530

    Auto-sync: 2025-06-18 09:03:33

---
+++ 1845A.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+   int t;
+   cin >> t;
+   while(t--){ 
+    /*
+    
+
+    */
+   int n,k,x;
+   cin >> n >> k >>x;
+   
+
+
+      
+   }
+
+
+
+      
+       
+      
+
+
+   
+   return 0;
+
+    
+} 
