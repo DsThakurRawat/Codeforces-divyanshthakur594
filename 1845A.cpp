3e5d0a64abe9a204b9160431d1e77b7c8cc54231
DsThakurRawat <DsThakurRawat@users.noreply.github.com>
2025-06-18 09:37:50 +0530

    Auto-sync: 2025-06-18 09:37:50

--- 1845A.cpp
+++ 1845A.cpp
@@ -26,38 +26,39 @@ int main(){
     
 
     */
-   int n,k,x;
-   cin >> n >> k >>x;
-   if( x != 1){
-    cout << "YES" << endl;
-    cout << n << endl;
-    for(int i = 0; i < n; i++) cout << 1 << " ";
-     cout << endl;
 
-   }
-   
-    else if ( k == 1 || (k == 2 && n % 2 == 1)){
-    cout <<"NO"<< endl;
+     int n, k, x;
+        cin >> n >> k >> x;
 
-   }
-else {
-    cout << "YES" << endl;
-    cout << n / 2 << endl;
-    if( n % 2 == 1){
-        cout << 3 << " ";
-
-        for(int i =1; i < n/2; i++) cout << 2 << " ";
+        if (x != 1) {
+            cout << "YES\n";
+            cout << n << "\n";
+            for (int i = 0; i < n; i++) {
+                cout << 1 << " ";
+            }
+            cout << "\n";
+        }
+        else if (k == 1 || (k == 2 && n % 2 == 1)) {
+            cout << "NO\n";
+        }
+        else {
+            cout << "YES\n";
+            cout << n / 2 << "\n";
+            if (n % 2 == 1) {
+                cout << 3 << " ";
+                for (int i = 1; i < n / 2; i++) {
+                    cout << 2 << " ";
+                }
+            }
+            else {
+                for (int i = 0; i < n / 2; i++) {
+                    cout << 2 << " ";
+                }
+            }
+            cout << "\n";
+        }
     }
-
-
-    else {
-        for(int i = 0; i < n/2; i++) cout << endl;
-        cout << 2 << " ";
-    }
-}
-
-
-      
+     return 0;
    }
 
 
@@ -68,7 +69,6 @@ else {
 
 
    
-   return 0;
+ 
 
     
-} 
