b273935816552dd192c2916f26d1780fa0b71b69
DsThakurRawat <DsThakurRawat@users.noreply.github.com>
2025-06-18 09:19:32 +0530

    Auto-sync: 2025-06-18 09:19:32

--- 1845A.cpp
+++ 1845A.cpp
@@ -6,12 +6,29 @@ int main(){
    cin >> t;
    while(t--){ 
     /*
+    x -> is a number that  i can not have 
+    
+
+    numbers available from 1 to k;
+    n = 1 + 1 +.......+n; // this is very efficient 
+    probelam rises when when 
+    case x != 1 --> one is available 
+                   i will print n 1
+    case x == 1 --> when one is not available 
+                    1 to k basically one is not available  
+                what if k == 1 then basically    no integers available
+                what if k == 2 u only have 2 available in abudance 
+                
+     
+
+
+
     
 
     */
    int n,k,x;
    cin >> n >> k >>x;
-   
+
 
 
       
