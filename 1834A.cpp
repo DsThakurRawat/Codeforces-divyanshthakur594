39258d342811f69f7ed776144bbb2d93b9f1c591
DsThakurRawat <DsThakurRawat@users.noreply.github.com>
2025-06-19 09:42:42 +0530

    Auto-sync: 2025-06-19 09:42:42

---
+++ 1834A.cpp
@@ -0,0 +1,60 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+   int t;
+   cin >> t;
+   while(t--){ 
+    int n;
+    cin >> n;
+    vector<int>vect;
+
+    for(int i =0; i < n; i++){
+        int x;
+        cin >> x;
+        vect.push_back(x);
+
+     }
+     /*
+     valid operations
+     -1 -1 1 -1 
+     s = -2
+     p = -1
+     op = -1 -1 1 1
+     s = 0
+     p = 1
+     no of op = 1;
+     s = l x -1 + k (+1) >=0
+     -l + k >=0
+     k>=l
+     1s should be more than equal to more than -1s 
+
+     a = {1,-1}
+     -> 1
+     or 
+     -> -1 
+
+
+
+
+
+
+
+     */
+
+
+      
+   }
+
+
+
+      
+       
+      
+
+
+   
+   return 0;
+
+    
+} 
