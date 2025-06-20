cbf71637e573d2b03aca484fb09dc1f20c06db0c
DsThakurRawat <DsThakurRawat@users.noreply.github.com>
2025-06-20 21:27:07 +0530

    Auto-sync: 2025-06-20 21:27:07

---
+++ 1805A.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+   int t;
+   cin >> t;
+   while(t--){ 
+
+    int n;
+    vector<int>a;
+
+    for(int i = 0; i < n; i++){
+        int x;
+        cin >> x;
+        a.push_back(x);
+    }
+
+    for(int x = 0; x < pow(2,8); x++)
+
+
+
+
+
+
+
+
+      
+   }
+
+
+
+      
+       
+      
+
+
+   
+   return 0;
+
+    
+} 
