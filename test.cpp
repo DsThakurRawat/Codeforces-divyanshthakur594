d864aeb252a4ac809a702124e6b0dff66bcea96c
DsThakurRawat <DsThakurRawat@users.noreply.github.com>
2025-06-26 18:38:44 +0530

    Auto-sync: 2025-06-26 18:38:44

--- test.cpp
+++ test.cpp
@@ -15,9 +15,21 @@ int main(){
       /*
       we can add 0 to one end and 1 to other end of string
       if str is 
+      if string is given 
+      100 --> answer is 1 bcz in one operation only 1 and 0 can be added each sid
+      0111-> answer is 2 
+      10101 -> answer is five arg: 1 if s[0] == s[s.size()-i -1] answer is s.size()
+      101010 -> if len > 1 && s[i] != s[len-i-1]   cnt++   ans = len -  2*cnt
 
       
       */
+   int count = 0;
+
+   int n = s.size();
+   
+
+
+
 
 
       
