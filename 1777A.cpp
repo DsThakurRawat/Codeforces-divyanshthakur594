12ed4c963da9feb2a52ae4eec494513bf9d5168d
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-07-02 11:09:47 +0530

    Auto-sync: 2025-07-02 11:09:47

---
+++ 1777A.cpp
@@ -0,0 +1,51 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+
+
+
+
+
+
+
+int main(){
+   int t;
+   cin >> t;
+   while(t--){ 
+   int n;
+   cin >> n;
+
+   vector < int > v;
+   for(int i = 0; i < n; i++){
+    int x;
+    cin >> x;
+    v.push_back(x);
+
+   }
+   int count = 0;
+
+   for(int i = 0; i < n-1; i++){
+
+    if(v[i] % 2 != 0 && v[i+1] % 2 !=0) count++;
+    else if(v[i] % 2 == 0 && v[i+1] % 2 == 0) count++;
+    else continue;
+
+
+   }
+
+
+      
+    cout << count << endl;
+
+
+
+
+    
+   }
+   
+return 0;
+
+    
+} 
