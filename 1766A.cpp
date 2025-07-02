3e4a47d317ce08c6b9c1eadec2e4a6fed8d24b2a
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-07-02 12:21:31 +0530

    Auto-sync: 2025-07-02 12:21:31

--- 1766A.cpp
+++ 1766A.cpp
@@ -1,34 +1,59 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+bool check(long long x){
+    long long count_digits = 0;
+    long long count_zeroes = 0;
+    while(x) {
+        if( x % 2 == 0) count_zeroes++;
+        count_digits++;
+        x = x / 10;
+    }
+
+    return count_zeroes == count_digits - 1;
+}
+
+
+
+
+
 
 int main(){
    int t;
    cin >> t;
+   // precomputation
+   vector < long long > round_numbers;
+
+   for(long long i = 1; i <= 999999; i++){
+
+    if(check(i) == true) round_numbers.push_back(i);
+
+
+
+
+   }
+
+
+
+
+
+
+
+
+
+
+
    while(t--){ 
-    int n;
+
+    long long n;
     cin >> n;
-    int count = 0;
-    int secondcount = 0;
-    for(int i = 1; i <= n; i++){
-        // extremely round if it had only one non zero digit
+    long long answer = 0;
 
-
-      int x = i;
-      while(x > 0){
-        if(x % 10 != 0) count++;
-        x /= 10;
-      }
-      
-    if( count == 1) secondcount++;
-    
-
-
-
+    for(int i = 0; i < round_numbers.size(); i++){
+        if(round_numbers[i] <= n) answer++;
+        else break;
     }
-      
-
-   cout << secondcount << endl;
+    cout << answer << endl;
   
     
    }
