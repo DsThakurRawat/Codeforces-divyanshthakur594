a96d5fa92c304b20a6abde79892a5e36ac6d7593
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-07-02 11:26:10 +0530

    Auto-sync: 2025-07-02 11:26:10

---
+++ 1766A.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+   int t;
+   cin >> t;
+   while(t--){ 
+    int n;
+    cin >> n;
+    int count = 0;
+    for(int i = 1; i <= n; i++){
+        // extremely round if it had only one non zero digit
+
+
+      int x = i;
+      while(x > 0){
+        if(x % 10 != 0) count++;
+        x /= 10;
+      }
+      
+      
+
+
+
+
+    }
+      
+
+
+  cout << count << endl;
+    
+   }
+   
+return 0;
+
+    
+} 
