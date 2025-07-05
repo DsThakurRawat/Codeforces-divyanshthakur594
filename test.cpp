2a7668b3cffab6e68c7fc5717083b654b999d40e
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-07-05 21:59:23 +0530

    Auto-sync: 2025-07-05 21:59:23

--- test.cpp
+++ test.cpp
@@ -1,12 +1,23 @@
 #include<bits/stdc++.h>
 using namespace std;
-
+// solution by divyansh 
 
 int main(){
    int t;
    cin >> t;
    while(t--){ 
-      
+       long long a, b, x, y;
+             cin >> a >> b >> x >> y;
+       if(a == b) {
+         cout << 0 << endl;
+         continue;
+      }
+ long long c1 = abs(a - b) * x;
+     
+     
+      long long a2 = a ^ 1;
+      long long c2 = y + abs(a2 - b) * x;
+      cout << min(c1, c2) << endl;
       
     
    }
