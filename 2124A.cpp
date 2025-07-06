c0b93074e976094cec6306499a8e0d836ec5e631
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-07-06 21:40:59 +0530

    Auto-sync: 2025-07-06 21:40:59

--- 2124A.cpp
+++ 2124A.cpp
@@ -1,69 +1,69 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-// solution by divyansh  rawat thakur 
 
-int main(){
-   int t;
-   cin >> t;
-   while(t--){ 
+int main() {
+    int t;
+    cin >> t;
 
+  while (t--) {
         int n;
         cin >> n;
-        
-        vector<int>a;
+        vector<int> a(n);
 
-        for(int i = 0; i < n; i++){
-            int x;
-            cin >> x;
-            a.push_back(x);
+    for (int i = 0; i < n; ++i)
+            cin >> a[i];
 
+        if (n == 1) {
+            cout << "NO" << endl;
+            continue;
         }
 
-         if( n == 1) {
-            cout << "NO" << endl;
-            continue;
+        vector<int> b = a;
+        sort(b.begin(), b.end());
 
-         }
-
-         vector<int> b = a;
-         sort(b.begin(), b.end());
-
-         bool flag = 1;
-
-         for(int i = 0; i < n; i++){
-            if(b[i] != b[0]) flag = 0;
-         }
-
-         if(flag == 1) {
-            cout << "NO" << endl;
-            continue;
-         }
-
-         vector<int> derange = b;
-
-         rotate(derange.begin(),derange.end()+1,derange.end());
-
-         for(int i = 0; i < n; i++){
-            if(derange[i] == b[i]) {
-                swap(derange[i],derange[(i+1) % n]);
-
+        bool allSame = 1;
+    for (int i = 1; i < n; ++i) {
+            if (b[i] != b[0]) {
+                allSame = 0;
+                break;
             }
+        }
+        if (allSame) {
+            cout << "NO" << endl;
+            continue;
+        }
 
-            
-         }
-         cout << "YES" << endl;
-         for(int i = 0; i < n; i++){
-            cout << derange[i] << " ";
+        vector<int> deranged = b;
+        rotate(deranged.begin(), deranged.begin() + 1, deranged.end());
 
-         }
-         cout << endl;
-
-        
       
-    
-   }
-   
-return 0;
+        for (int i = 0; i < n; i++) {
+            if (deranged[i] == b[i]) {
+                if (i + 1 < n) {
+                    swap(deranged[i], deranged[i + 1]);
+                } else {
+                    swap(deranged[i], deranged[i - 1]);
+                }
+            }
+        }
 
-    
-} 
+        bool flag = 1;
+        for (int i = 0; i < n; i++) {
+            if (deranged[i] == b[i]) {
+                flag = 0;
+                break;
+            }
+        }
+
+        if (!flag) {
+            cout << "NO"<<endl;
+        } else {
+            cout << "YES" << endl;
+            cout << n << endl;
+            for (int x : deranged) cout << x << " ";
+            cout << endl;
+        }
+    }
+
+    return 0;
+}
