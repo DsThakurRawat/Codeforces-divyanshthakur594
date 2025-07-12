9a2095115fc3fa594b6445598f1bf8800c49e99a
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-07-12 11:55:14 +0530

    Auto-sync: 2025-07-12 11:55:14

--- test.cpp
+++ test.cpp
@@ -6,18 +6,7 @@ int main(){
    int t;
    cin >> t;
    while(t--){ 
-       long long a, b, x, y;
-             cin >> a >> b >> x >> y;
-       if(a == b) {
-         cout << 0 << endl;
-         continue;
-      }
- long long c1 = abs(a - b) * x;
-     
-     
-      long long a2 = a ^ 1;
-      long long c2 = y + abs(a2 - b) * x;
-      cout << min(c1, c2) << endl;
+       
       
     
    }
