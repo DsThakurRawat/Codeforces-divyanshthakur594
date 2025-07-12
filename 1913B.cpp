694f831a58386e564e2cd438b002700b78f83ba2
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-07-12 12:42:11 +0530

    Auto-sync: 2025-07-12 12:42:11

--- 1913B.cpp
+++ 1913B.cpp
@@ -16,8 +16,9 @@ int main(){
     int tsize = 0;
     
     for(int i = 0; i < s.size(); i++){
-        if(s[i] == '0')count0++
-        
+        if(s[i] == '0')count0++;
+        if(s[i])
+
     }
 
     
