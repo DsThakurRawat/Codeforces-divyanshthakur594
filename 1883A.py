7f2600cb8a9ddee2decbc004d2dc23ed136b88fa
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-07-14 22:45:11 +0530

    Auto-sync: 2025-07-14 22:45:11

--- 1883A.py
+++ 1883A.py
@@ -11,7 +11,7 @@ for _ in range(int(input())):
      
      n, k = map(int, input().split())
      # we need to determine minimum numbe rof operation to make product of all the numbers in the array a1.a2.a3.....an divisible by k
-     lst = list(map(int,input().split()))
+     arr = list(map(int,input().split()))
 
 
      
