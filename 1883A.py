e1d307c809ad18768528e890f378458f2b5783ed
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-07-14 22:38:09 +0530

    Auto-sync: 2025-07-14 22:38:09

--- 1883A.py
+++ 1883A.py
@@ -9,4 +9,4 @@
 for _ in range(int(input())):
      
      n, k = map(int, input().split())
-     # we need to determine minimum numbe rof operation to make product of all the numbers 
+     # we need to determine minimum numbe rof operation to make product of all the numbers in the 
