d6e54953948bc791863e7f70bf506725f780266c
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-07-14 22:56:10 +0530

    Auto-sync: 2025-07-14 22:56:10

--- 1883C.cpp
+++ 1883C.cpp
@@ -15,7 +15,7 @@ int main(){
        /*
        arg: divide and check 
        if k > arrInt then k % smallest(arrElement)
-       if all array element >= k then  smallest of all array element % k
+       if all array element > k then  smallest of all array element % k
        
        */
 
