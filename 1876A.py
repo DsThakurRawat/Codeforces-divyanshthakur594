e0002eeb945c585837dc2577949a0442de540786
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-07-15 11:57:48 +0530

    Auto-sync: 2025-07-15 11:57:48

--- 1876A.py
+++ 1876A.py
@@ -1,5 +1,5 @@
 for _ in range(int(input())):
     n,p = map(int, input().split()) #to take multiple inputs n -> number of resident and p -> cost for pak chanek to share announcement directly
     resident = list(map(int, input().split()))
-    cost
+    costresident = list(map(int, input().split()))
 
