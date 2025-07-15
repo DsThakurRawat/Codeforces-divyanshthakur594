3ef876674ae91653c2af40673ec6b54402522bc3
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-07-15 12:49:11 +0530

    Auto-sync: 2025-07-15 12:49:11

--- 1876A.cpp
+++ 1876A.cpp
@@ -12,7 +12,7 @@ int main(){
     for(int i = 0; i < n; i++)cin >> a[i];
     for( int i = 0; i < n; i++) cin >> b[i];
      
-    vector<pair<int , int> > residents(N);
+    vector<pair<int , int> > residents(n);
 
     for(int i = 0; i < n; i++){
         residents[i] = {a[i],b[i]};
