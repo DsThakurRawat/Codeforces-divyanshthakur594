d2fc20c5e1b573410e79aff97afc915ebbe96b17
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-07-17 20:15:33 +0530

    Auto-sync: 2025-07-17 20:15:33

--- 2126A.cpp
+++ 2126A.cpp
@@ -22,7 +22,7 @@ int main(){
        for(int i =0; i <=x; i++ ){
         if(find(digits.begin(), digits.end(), i) != digits.end()){
             int cur = i;
-            miny = min(min)
+            miny = min(miny,curi)
         }
        }
 
