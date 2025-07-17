6c5ef7c51a9276969e95debf279de7c4aa9eb8f9
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-07-17 21:35:02 +0530

    Auto-sync: 2025-07-17 21:35:02

--- test.cpp
+++ test.cpp
@@ -1,17 +1,36 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-// solution by divyansh 
 
-int main(){
-   int t;
-   cin >> t;
-   while(t--){ 
-       
-      
-    
-   }
-   
-return 0;
+int main() {
+    int t; cin >> t;
+    while(t--) {
+        int n, k;
+        cin >> n >> k;
+        vector<int> h(n);
+        for (int i = 0; i < n; i++) cin >> h[i];
 
-    
-} 
+        int start = k - 1;
+        int height = h[start];
+        int maxheight = *max_element(h.begin(), h.end());
+
+        if (height == maxheight) {
+            cout << "YES\n";
+            continue;
+        }
+
+        bool canReach = false;
+        for (int i = 0; i < n; ++i) {
+            if (h[i] == maxheight) {
+                int teleport_time = abs(h[i] - height);
+                if (teleport_time <= height - 1) {
+                    canReach = true;
+                    break;
+                }
+            }
+        }
+
+        cout << (canReach ? "YES" : "NO") << '\n';
+    }
+
+    return 0;
+}
