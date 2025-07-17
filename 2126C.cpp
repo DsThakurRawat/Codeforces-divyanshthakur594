a868dff0168c484591f517f1b9fa55dedc0bdedc
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-07-17 21:20:05 +0530

    Auto-sync: 2025-07-17 21:20:05

--- 2126C.cpp
+++ 2126C.cpp
@@ -28,8 +28,25 @@ a teleport from tower i to tower j takes |h[i] - h[j]|
 
    */
 
+int start = k - 1;
+    int maxHeight = *max_element(h.begin(), h.end());
 
-   
+    // Already on max height
+    if (h[start] == maxHeight)
+        return "YES";
+
+    for (int i = 0; i < n; ++i) {
+        if (h[i] == maxHeight) {
+            int teleportTime = abs(h[i] - h[start]);
+            if (teleportTime < h[start]) {
+                return "YES"; // Can reach before drowning
+            }
+        }
+    }
+
+    return "NO"; // No valid teleport path
+}
+
 
 
 
