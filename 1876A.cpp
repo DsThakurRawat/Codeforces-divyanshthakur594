44ca9b81e44a39dcd44c5504fd958a3068eafd69
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-07-17 12:53:17 +0530

    Auto-sync: 2025-07-17 12:53:17

---
+++ 1876A.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t; cin >> t;
+    while (t--) {
+        int n, p;
+        cin >> n >> p;
+        vector<int> a(n), b(n);
+        for (int i = 0; i < n; ++i) cin >> a[i];
+        for (int i = 0; i < n; ++i) cin >> b[i];
+
+        vector<int> shares;
+
+        // Collect all possible shares from residents
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < a[i]; ++j) {
+                shares.push_back(b[i]);
+            }
+        }
+
+        // Add extra 'p' cost shares from Pak Chanek (enough to cover all cases)
+        for (int i = 0; i < n; ++i) {
+            shares.push_back(p);
+        }
+
+        // Sort and take n-1 cheapest
+        sort(shares.begin(), shares.end());
+        long long total_cost = p;  // First share must be from Pak Chanek
+        for (int i = 0; i < n - 1; ++i) {
+            total_cost += shares[i];
+        }
+
+        cout << total_cost << '\n';
+    }
+
+    return 0;
+}
