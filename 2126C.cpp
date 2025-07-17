f3ad5739c7771efc425499f9bfe91b31c43e780b
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-07-17 21:17:53 +0530

    Auto-sync: 2025-07-17 21:17:53

--- 2126C.cpp
+++ 2126C.cpp
@@ -19,7 +19,8 @@ int main(){
 
       for(int i = 0; i < n; i++) cin >> h[i];
    /*
-  hm we only need to find any one tower with max heights where we can reach befor be 
+  hm we only need to find any one tower with max heights where we can reach befor be doonbe
+  
 
    */
 
