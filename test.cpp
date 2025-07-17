315400d4b23f262db96dcd18ac6d02fc45ed59d3
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-07-17 21:36:35 +0530

    Auto-sync: 2025-07-17 21:36:35

--- test.cpp
+++ test.cpp
@@ -1,35 +1,40 @@
 #include <bits/stdc++.h>
 using namespace std;
+// solution by divyansh thakur
 
-int main() {
-    int t; cin >> t;
-    while(t--) {
+int main(){
+    int t;
+    cin >> t;
+    while(t--){ 
         int n, k;
         cin >> n >> k;
         vector<int> h(n);
-        for (int i = 0; i < n; i++) cin >> h[i];
+        for(int i = 0; i < n; i++) cin >> h[i];
 
         int start = k - 1;
-        int height = h[start];
-        int maxheight = *max_element(h.begin(), h.end());
+        int maxh = *max_element(h.begin(), h.end());
+        int currentH = h[start];
 
-        if (height == maxheight) {
+        // Already on max height
+        if (currentH == maxh){
             cout << "YES\n";
             continue;
         }
 
-        bool canReach = false;
-        for (int i = 0; i < n; ++i) {
-            if (h[i] == maxheight) {
-                int teleport_time = abs(h[i] - height);
-                if (teleport_time <= height - 1) {
-                    canReach = true;
+        bool canTeleport = false;
+        for(int i = 0; i < n; ++i){
+            if(h[i] == maxh){
+                int teleportTime = abs(currentH - h[i]);
+                int survivalTime = currentH - 1;
+
+                if(teleportTime <= survivalTime){
+                    canTeleport = true;
                     break;
                 }
             }
         }
 
-        cout << (canReach ? "YES" : "NO") << '\n';
+        cout << (canTeleport ? "YES\n" : "NO\n");
     }
 
     return 0;
