e27fa8b30ab15e5617104d6d37b7477f7cc1eac4
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-07-17 13:19:58 +0530

    Auto-sync: 2025-07-17 13:19:58

--- 1859B.cpp
+++ 1859B.cpp
@@ -11,7 +11,9 @@ int main(){
      vector<vector<int>> arrays(n); // this thing here declare list of n arrays
      /*
     1. This creates a vector of size n, where each element is itself a vector of integers.
-    2.
+    2. That is, arrays[0], arrays[1], ..., arrays[n-1] are all empty vectors initially.
+
+
      */
 
 
