cb11216ebd242e2d13a95d79310dd4484499bad3
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-07-17 20:39:32 +0530

    Auto-sync: 2025-07-17 20:39:32

--- 2126B.cpp
+++ 2126B.cpp
@@ -9,7 +9,7 @@ int main(){
     // so we have given array a of n numbers and a number k the value ai describes the weather on ith day 
     // if it rains on the day , then ai = 1; 
     // if weather is good means not rain then ai = 0;
-    // so jean want to visit as many as park ps
+    // so jean want to visit as many as park possible 
        
       
     
