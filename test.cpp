f3ce86cb38138e86c6e2bf6eb0db710d070f7a98
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-07-25 22:38:12 +0530

    Auto-sync: 2025-07-25 22:38:12

--- test.cpp
+++ test.cpp
@@ -1,7 +1,29 @@
 #include<bits/stdc++.h>
 using namespace std;
 // code by divyansh thakur
-
+#define INF 1e9
+#define INF64 9223372036854775807LL
+#define NINF64 -9223372036854775807LL
+#define MOD 998244353
+#define MOD2 1000000007
+#define PI 3.141592653589793238462
+#define int int64_t
+#define vi vector<int>
+#define all(x) (x).begin(), (x).end()
+#define endl '\n'
+#define yes cout << "YES" << "\n"
+#define no cout << "NO" << "\n"
+#define un_s unordered_set<int>
+#define un_mp unordered_map<int, int>
+#define pr_q priority_queue<int>
+#define vvi vector<vector<int>>
+#define vpii vector<pair<int,int>>
+#define vb vector<bool>
+#define pb push_back
+#define eb emplace_back
+#define ff first
+#define ss second
+#define sz(x) (int)(x).size()
 
 int main(){
    int t;
