f0335d50e43891a6f1a7a7bcd8e9db361b417f47
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-07-25 23:55:44 +0530

    Auto-sync: 2025-07-25 23:55:44

--- test.cpp
+++ test.cpp
@@ -8,23 +8,29 @@ using namespace std;
 #define MOD2 1000000007
 #define PI 3.141592653589793238462
 #define int int64_t
-#define vi vector<int>
-#define all(x) (x).begin(), (x).end()
+#define int long long
 #define endl '\n'
-#define yes cout << "YES" << "\n"
-#define no cout << "NO" << "\n"
-#define un_s unordered_set<int>
-#define un_mp unordered_map<int, int>
-#define pr_q priority_queue<int>
-#define vvi vector<vector<int>>
-#define vpii vector<pair<int,int>>
-#define vb vector<bool>
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
 #define pb push_back
-#define eb emplace_back
 #define ff first
 #define ss second
-#define sz(x) (int)(x).size()
-
+#define pii pair<int, int>
+#define vi vector<int>
+#define vii vector<pii>
+#define vvi vector<vector<int>>
+#define mii map<int, int>
+#define umii unordered_map<int, int>
+#define usi unordered_set<int>
+#define si set<int>
+#define msi multiset<int>
+#define msi2 multimap<int, int>
+#define rep(i, a, b) for (int i = a; i < b; i++)
+#define repr(i, a, b) for (int i = a; i >= b; i--)
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define minpq priority_queue<int, vector<int>, greater<int>>
+#define maxpq priority_queue<int>
 int main(){
    int t;
    cin >> t;
