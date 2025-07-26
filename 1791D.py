54d7cd0fd54b14c38b946c88a21eadc5d11baeaf
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-07-26 11:25:55 +0530

    Auto-sync: 2025-07-26 11:25:55

--- 1791D.py
+++ 1791D.py
@@ -3,6 +3,7 @@ for _ in range(int(input())):
    s = input()
    def f(x):
       return len(set(x))
+   dp = [-1] * (n + 1)  
    def recurse(s,ind):
       if ind == len(s):
          return 0
@@ -10,7 +11,9 @@ for _ in range(int(input())):
       b = s[ind:]
       total = f(a) + f(b)
       return max(total,recurse(s,ind+1))
-   
+       
+      dp[ind] = max(total, recurse(ind + 1))
+      return dp[ind]
 
    def solve():
         return recurse(s,1)
