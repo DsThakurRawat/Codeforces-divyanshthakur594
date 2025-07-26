4e2c3ce50e74594ce3a91d730dc910771ac38e02
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-07-26 11:20:21 +0530

    Auto-sync: 2025-07-26 11:20:21

---
+++ 1791D.py
@@ -0,0 +1,17 @@
+for _ in range(int(input())):
+   n = int(input())
+   s = input()
+   def f(x):
+      return len(set(x))
+   def recurse(s,ind):
+      if ind == len(s):
+         return 0
+      a = s[:ind]
+      b = s[ind:]
+      total = f(a) + f(b)
+      return max(total,recurse(s,ind+1))
+   
+
+   def solve():
+        return recurse(s,1)
+        print(solve())
