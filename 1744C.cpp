ccfb96e2c76491e3a3a76916bb626cd55d583846
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-07-28 21:38:01 +0530

    Auto-sync: 2025-07-28 21:38:01

--- 1744C.cpp
+++ 1744C.cpp
@@ -317,7 +317,7 @@ C++ Operator Keyword Equivalents (Normal Format):
  
 
 
-// it is known that traffic light repeat its colors evry n seconds 
+// it is known that traffic light repeat its colors evry n seconds an at th ith seconds 
 
      
 */
