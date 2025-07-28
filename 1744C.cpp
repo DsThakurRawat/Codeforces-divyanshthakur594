40e4dce9e91725dda8fa982019b8a23cb6c2e4a9
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-07-28 21:53:30 +0530

    Auto-sync: 2025-07-28 21:53:30

--- 1744C.cpp
+++ 1744C.cpp
@@ -357,6 +357,7 @@ void solve() {
     cin >> s; // it is guaranteed that symbol g is in the string s and as well string c
     int timer = 0;
     for(int i = 0; i < s.size(); i++){
+        if(s[i])
 
 
 
