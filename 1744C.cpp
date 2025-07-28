0069017ef5840b2d73eba70a2827a671608a6f6d
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-07-28 22:25:15 +0530

    Auto-sync: 2025-07-28 22:25:15

--- 1744C.cpp
+++ 1744C.cpp
@@ -367,7 +367,7 @@ void solve() {
 
         }
 
-        else if( s[i] != 'g' )) timer++;
+        else if( s[i] != 'g' ) timer++;
         if(s[i] == 'g' && s[i+1] != 'g') {
             int ans = timer;
             break;
