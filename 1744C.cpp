8abd8ecc6541fb6c7bf185d43c2fcef6144f1cb4
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-07-28 21:12:52 +0530

    Auto-sync: 2025-07-28 21:12:52

--- 1744C.cpp
+++ 1744C.cpp
@@ -210,7 +210,7 @@ vector<vector<int>> power(vector<vector<int>> a, int p) {
 }
 #pragma endregion
 
-// -------------------- Geometry --------------------
+// -------------------- Geometry -------------------->
 #pragma region Geometry
 const double EPS = 1e-9;
 bool eq(double a, double b) { return fabs(a - b) < EPS; }
