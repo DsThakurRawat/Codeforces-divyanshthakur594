04b5694756cb07f4b0923bb2a9e885115397b167
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-08-10 21:31:05 +0530

    Auto-sync: 2025-08-10 21:31:05

--- 2131B.cpp
+++ 2131B.cpp
@@ -331,7 +331,7 @@ void solve() {
     for (int i = 1; i <= n; ++i) {
         if (i % 2 == 1) cout << -1;
         else {
-            if (n == 2) 
+            if (i == n) 
               cout << 2; 
             else 
               cout << 3;
