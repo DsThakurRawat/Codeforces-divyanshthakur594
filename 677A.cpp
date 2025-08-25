11addbc8e11893d2946f45d028924355ca126825
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-08-25 18:50:51 +0530

    Auto-sync: 2025-08-25 18:50:51

--- 677A.cpp
+++ 677A.cpp
@@ -8,7 +8,19 @@ using namespace std;
 void solve() {
     int n , h;
     cin >> n >> h;
-    
+    vector<int>v(n);
+    for(int i =0; i < n; i++) cin >> v[i];
+    // h is the height of the fence 
+    int width = 0;
+
+    for(int i = 0; i < n; i++){
+        if(v[i] <= h ) width++;
+        else if( v[i] > h) width = width + 2;
+    }
+
+    cout << width << endl;
+
+
 
   
 }
