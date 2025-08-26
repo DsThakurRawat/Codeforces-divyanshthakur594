6cba0d8cf0b4c7558964ae5aef7024230f608fb5
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-08-26 21:15:44 +0530

    Auto-sync: 2025-08-26 21:15:44

--- 2125A.cpp
+++ 2125A.cpp
@@ -8,30 +8,11 @@ using namespace std;
 void solve() {
     string s;
     cin >> s;
-    string p;
-    string sub1 = "FFT";
-    string sub2 =  "NTT";
-    int flag = 1; // man ke chal rahe hai ki hai
+    sort(s.begin(),s.end());
+    reverse(s.begin(),s.end());
+    cout << s << endl;
 
-     if(s.find(sub1) != string::npos && s.find(sub2) != string::npos) {
-        flag = 0;
-        p = s;
-   }
-
-   for(int i = 0; i < s.size()-2; i++){
-       if( s[i] == 'F' && s[i+1] == 'F' && s[i+2] == 'T'){
-            s[i+1] = 'T';
-        
-
-       }
-       if(s[i] == 'N' && s[i+1] == 'N' && s[i+2] == 'T'){
-        s[i+1] ='T';
-       }
-   } 
-   if(flag == 0) cout << p << endl;
-   if(flag == 1) cout << s << endl;
-
-
+   
 
 
 
