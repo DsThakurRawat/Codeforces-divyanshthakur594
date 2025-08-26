cd96ab36585500e16771704b5584f65762736e0b
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-08-26 20:50:16 +0530

    Auto-sync: 2025-08-26 20:50:16

---
+++ 2117B.cpp
@@ -0,0 +1,24 @@
+
+// simple template-test2
+// code by divyansh rawat
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+
+    int n; cin >> n;
+    for(int i = 2; i <= n; i++) cout << i << ' ';
+
+    cout << 1 << endl;
+  
+}
+
+int main() {
+    int t ;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
