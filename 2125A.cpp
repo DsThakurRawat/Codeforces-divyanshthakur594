32bc03fec529b0a48deaccb017dbb9957fbe087e
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-08-26 21:12:20 +0530

    Auto-sync: 2025-08-26 21:12:20

--- 2125A.cpp
+++ 2125A.cpp
@@ -8,12 +8,14 @@ using namespace std;
 void solve() {
     string s;
     cin >> s;
+    string p;
     string sub1 = "FFT";
     string sub2 =  "NTT";
     int flag = 1; // man ke chal rahe hai ki hai
 
      if(s.find(sub1) != string::npos && s.find(sub2) != string::npos) {
         flag = 0;
+        p = s;
    }
 
    for(int i = 0; i < s.size()-2; i++){
@@ -25,7 +27,11 @@ void solve() {
        if(s[i] == 'N' && s[i+1] == 'N' && s[i+2] == 'T'){
         s[i+1] ='T';
        }
-   }
+   } 
+   if(flag == 0) cout << p << endl;
+   if(flag == 1) cout << s << endl;
+
+
 
 
 
