f825ea4185a1ffbe2f2c0464bc530515bb5b047f
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-08-29 16:07:44 +0530

    Auto-sync: 2025-08-29 16:07:44

--- template-3A-ultimate.cpp
+++ template-3A-ultimate.cpp
@@ -1,6 +1,6 @@
 /**************************************************
 * Author    : Divyansh Thakur Rawat
-* Template  :  Template-3-ultimate 
+* Template  :  Template-3
 **************************************************/
 // will work seamlessly on any os and system
 
