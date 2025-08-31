4844914eea9509563e6c91555869f5b9ec60afc2
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-08-31 09:46:33 +0530

    Auto-sync: 2025-08-31 09:46:33

--- 339A.cpp
+++ 339A.cpp
@@ -36,10 +36,10 @@ void solve() {
 
 
 int main() {
-    int t ;
-    cin >> t;
-    while (t--) {
+   // int t ;
+   // cin >> t;
+   // while (t--) {
         solve();
-    }
+  //  }
     return 0;
 }
