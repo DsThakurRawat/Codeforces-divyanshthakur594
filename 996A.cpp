fb55f840b04fe12a63ea3d00d816f5c403da7bf2
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-08-31 16:35:48 +0530

    Auto-sync: 2025-08-31 16:35:48

---
+++ 996A.cpp
@@ -0,0 +1,23 @@
+
+// simple template-test2
+// code by divyansh rawat
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+
+    vector<int>v(5,(1,5,10,20,100));
+    
+
+  
+}
+
+int main() {
+  //  int t ;
+   // cin >> t;
+ //   while (t--) {
+        solve();
+ //   }
+    return 0;
+}
