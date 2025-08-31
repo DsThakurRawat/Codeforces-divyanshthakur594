ca8eba5863412061003a924be5ce14af56fcd6f6
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-08-31 09:43:02 +0530

    Auto-sync: 2025-08-31 09:43:02

---
+++ 339A.cpp
@@ -0,0 +1,30 @@
+
+// simple template-test2
+// code by divyansh rawat
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+  string s;
+  cin >> s;
+  vector<int>v;
+
+  for(int i = 0; i < s.size(); i++){
+    if(s[i] != '+'){
+        int num = (s[i]) - '0';
+     v.push_back(num);
+  }
+
+}
+}
+
+
+int main() {
+    int t ;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
