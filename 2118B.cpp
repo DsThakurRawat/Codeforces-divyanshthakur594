a44017a565920ec4e7dc89037a7f14b4682070db
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-09-14 18:53:36 +0530

    Auto-sync: 2025-09-14 18:53:36

--- 2118B.cpp
+++ 2118B.cpp
@@ -5,29 +5,19 @@ int main(){
    int t;
    cin >> t;
    while(t--){ 
-      int n; cin >> n;
-      cout << 2*n-1<< endl;
-
-      for(int i = 1; i < n; i++){
-         cout << i << " " << 1 << " " << i << endl;
-         cout << i << " " << i + 1 << n << endl;
-      }
-      cout << n << " 1 " << n << endl;
+   
+   
+        int n; cin >> n;
+        cout << 2*n-1 << "\n";
+        for (int i = 1; i < n; i++) {
+            cout << i << " " << 1 << " " << i << "\n";
+            cout << i << " " << i+1 << " " << n << "\n";
+        }
+        cout << n << " 1 " << n << "\n";
+    }
+   
     
 
-
-      
-
-
-
-
-      
-       
-      
-
-
-   }
-   return 0;
-
-    
+     return 0;
 } 
+
