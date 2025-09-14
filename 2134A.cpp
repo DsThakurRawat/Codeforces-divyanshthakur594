76887d5fa1c13c5be8a4f2853b372537baabb2d5
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-09-14 23:03:41 +0530

    Auto-sync: 2025-09-14 23:03:41

--- 2134A.cpp
+++ 2134A.cpp
@@ -5,7 +5,26 @@
 using namespace std;
 
 void solve() {
+  int n , a , b; cin >> n >> a >> b;
+  int count = 0;
+  vector<int>v(3);
+  v.push_back(a);
+  v.push_back(b);
+  v.push_back(n);
+  for(int i = 0; i < 3; i++){
+    if(v[i] % 2 != 0) count++;
+  }
+
+  if(count == 2) cout << "NO" << endl;
+  else cout << "YES" << endl;
   
+
+
+
+  
+  
+  
+
 }
 
 int main() {
