17a47620b1633a375172a748b155914b44934a29
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-09-15 18:50:25 +0530

    Auto-sync: 2025-09-15 18:50:25

--- 2134B.cpp
+++ 2134B.cpp
@@ -9,11 +9,12 @@ void solve() {
     vector<int>v(n);
 
     for(int i = 0; i < n; i++) cin >> v[i];
+    // 2134B
 
     // we gonna make parity of element same 
     // let gcd be g = K + 1;
     int g = k + 1;
-    vector<int>ans(n);
+    vector<int>ans;
 
     for(int i = 0; i < v.size(); i++){
         if( v[i] % g == 0) ans.push_back(v[i]);
@@ -27,7 +28,7 @@ void solve() {
             }
             else if( v[i] > g){
                 int r = v[i] % g;
-                v[i] = v[i] + g;
+                v[i] = v[i] + r;
                 ans.push_back(v[i]);
             }
 
