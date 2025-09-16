22d767785d995c9d084b53a4a79ebb053c65869d
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-09-16 18:05:02 +0530

    Auto-sync: 2025-09-16 18:05:02

--- 2140B.cpp
+++ 2140B.cpp
@@ -8,7 +8,41 @@ using namespace std;
 void solve() {
     int t ; cin >> t;
     int x ; cin >> x;
-    cout << x << endl;
+    /*
+            Let's assume that the number y
+            has d(>0)
+            digits.
+
+            Now we try to express the number x#y
+            in an algebraic form.
+
+            x#y=x⋅10d+y=x⋅(10d−1)+(x+y)
+            .
+
+            For x#y
+            to be divisible by x+y
+            , we have that x+y
+            must be a factor of x⋅(10d−1)
+            .
+
+            Now that we know d>0
+            , we can surely say that the number 3⋅x
+            is a factor of x⋅(10d−1)
+            . Thus, if we make x+y=3⋅x
+            , the condition is satisfied.
+
+            So, we can just choose y
+            to be equal to 2⋅x.
+
+
+    
+    
+    
+    */
+
+
+
+    cout << 2*x << endl;
 
   
 }
