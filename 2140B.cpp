ffaf7952a8695ba5d8149a4d8c30c8b51e285e4b
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-09-16 17:52:38 +0530

    Auto-sync: 2025-09-16 17:52:38

---
+++ 2140B.cpp
@@ -0,0 +1,23 @@
+
+// simple template-test2
+// code by divyansh rawat
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    int t ; cin >> t;
+    int x ; cin >> x;
+    cout << x << endl;
+
+  
+}
+
+int main() {
+    int t ;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
