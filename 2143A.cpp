ca0d2fc94c4717bd2f92f49117a515d073f7e370
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-09-17 20:19:08 +0530

    Auto-sync: 2025-09-17 20:19:08

---
+++ 2143A.cpp
@@ -0,0 +1,36 @@
+
+// simple template-test2
+// code by divyansh rawat
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    int n; cin >> n;
+    vector<int>v(n);
+    for(int i = 0; i < n; i++) cin >> v[i];
+    // ham bas maximum ek minus kar sakte hai
+
+    sort(v.rbegin(),v.rend());
+    for(int i = 0; i < n; i++)
+    {
+        if(v[i] > i+1) cout << "NO" << endl; return;
+    }
+    cout << "YES" << endl;
+
+
+
+
+
+
+  
+}
+
+int main() {
+    int t ;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
