ed1f62b9cc6adb772565682a25e0aed34348a0dd
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-09-20 16:30:25 +0530

    Auto-sync: 2025-09-20 16:30:25

---
+++ 26A.cpp
@@ -0,0 +1,39 @@
+
+// simple template-test2
+// code by divyansh rawat
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    int n; cin >> n;
+    vector<int>prime(n+1,1);
+    // designing sieve
+    
+    for(long long i = 2; i <= n; i++){
+        if(prime[i] == 1 && i*i <= n){
+            for(int j = i*i; j <= n; j+=i){
+            prime[j] = 0;
+            }
+        }
+    }
+    //A number is called almost prime if it has exactly two distinct prime divisors.
+    
+
+    
+
+
+
+
+
+  
+}
+
+int main() {
+   // int t ;
+   // cin >> t;
+   // while (t--) {
+        solve();
+   // }
+    return 0;
+}
