272c901d9a41b30667374b855bf6d424937e68c2
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-09-20 23:24:49 +0530

    Auto-sync: 2025-09-20 23:24:49

--- 17A.cpp
+++ 17A.cpp
@@ -1,7 +1,5 @@
-
 // simple template-test2
 // code by divyansh rawat
-
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -12,16 +10,37 @@ void solve() {
   
     
     for(long long i = 2; i <= n; i++){
-        if(prime[i] == 1 && i*i <= n){
+        if(prime[i] == 1 ){
             for(int j = i*i; j <= n; j+=i){
             prime[j] = 0;
             }
         }
     }
 
+    vector<int>primes;
+    for(int i = 2; i <= n; i++)
+            if(prime[i] == 1) primes.push_back(i);
+
+    int count = 0;
+
+    for(int i = 0; i + 1 < primes.size(); i++){
+        int sum = primes[i] + primes[i+1] + 1;
+        if(sum <= n && prime[sum]) count++;
+    }
+    if(count >= k) cout << "YES" << endl;
+    else cout << "no\n";
+
+
+
+
+    
+       
+}
+    
+
 
   
-}
+
 
 int main() {
     int t ;
