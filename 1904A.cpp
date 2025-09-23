8df625a2c689e7f51ae7d142e41b0b2eb1f64e8a
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-09-23 12:59:40 +0530

    Auto-sync: 2025-09-23 12:59:40

---
+++ 1904A.cpp
@@ -0,0 +1,19 @@
+
+// simple template-test2
+// code by divyansh rawat
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+  
+}
+
+int main() {
+    int t ;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
