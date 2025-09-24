c0571b99b92119c38ff4a946bdb424a207f818ca
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-09-24 19:32:33 +0530

    Auto-sync: 2025-09-24 19:32:33

---
+++ 1878C.cpp
@@ -0,0 +1,56 @@
+
+// simple template-test2
+// code by divyansh rawat
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+
+//f he can choose k distinct integers between 1and n such that their sum is equal to x.
+
+int n,k,x;
+cin >> n >> k >> x;
+int sum = x;
+int total_eletochoose = k;
+// for nums first;
+// the simple appraoch for this problem is find min solution and maximumum solution 
+// calculate the minimum possible sum of k distinct integers 
+// sum of first k natural numbers
+
+long long min_sum = (k*(k+1))/2;
+long long max_sum = (n * (n + 1)) / 2 - ((n - k) * (n - k + 1)) / 2;
+
+if (x >= min_sum && x <= max_sum)
+			cout << "YES" << endl; 
+		else
+			cout << "NO" << endl; 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+}
+
+int main() {
+    int t ;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
