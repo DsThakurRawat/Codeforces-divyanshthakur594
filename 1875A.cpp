0651565f2b78cee4ce196cbdce15afed57a65001
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-09-28 23:05:42 +0530

    Auto-sync: 2025-09-28 23:05:42

--- 1875A.cpp
+++ 1875A.cpp
@@ -27,9 +27,9 @@ void solve() {
     // b intitial value of bombs timer
     // n number of tools
     // for each test case output maxi
-    int sum = 0;
+    int sum = ;
     for(int i = 0; i <= n; i++){
-        sum = sum + min(a-1,v[i]);
+        sum = sum + min(a-1,v[i]) + b;
     }
     cout << sum << endl;
 
