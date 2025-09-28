4fb210164aa698c0e4e93eec47747a93bf5077e0
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-09-28 21:46:24 +0530

    Auto-sync: 2025-09-28 21:46:24

--- 1875A.cpp
+++ 1875A.cpp
@@ -8,8 +8,25 @@ using namespace std;
 void solve() {
     /*
     timer set = b;
-    ev
+    everys second timer dec is by 1
+    bomb explodes when timer reaches 0
+    if i used ith tool timer will increase by xi
+    newtime = min(c + Xi , a);
+    timer dec is by 1;
+    if timer reaches 0,the bomb explodes;
+
+
     */
+
+    int a , b , n;
+    cin >> a >> b >> n;
+    vector<int>v(n);
+
+    for(int i = 0; i < n; i++) cin >> v[i];
+    // a the maximum value of bombs timer
+    // b intitial value of bombs timer
+    // n number of tools
+    // for each test case output maxi
 
   
 }
