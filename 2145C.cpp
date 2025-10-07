d612778ab87ad6602a66420d7f27fac4b438961e
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-10-08 01:02:56 +0530

    Auto-sync: 2025-10-08 01:02:56

--- 2145C.cpp
+++ 2145C.cpp
@@ -12,29 +12,47 @@ void solve() {
    
 
 
-        vector<int>v(n);
-        for(int i = 0; i < n; i++){
-            if(s[i] == 'a') v[i] = 1;
-            else v[i] = -1;
-        }
-      
+    int A = 0;
+    int B = 0;
+
+    for(auto c : s){
+        if ( c == 'a') A++;
+        else B++;
+    }
+    if( A == B){
+        cout << 0 << endl;
+        return;
+
+    }
+    int diff = A - B;
+
+    map<int,int> freq;
+
+    freq[0] = 0;
+    int presum = 0, ans = INT_MAX;
+
+    vector<int>temp(n);
+    for(int i = 0; i < n; i++){
+        if(s[i] == 'a') temp[i] = 1;
+        else temp[i] = -1;
+    }
+   int total_sum = accumulate(temp.begin(),temp.end(),0);
+
+
+
+
+    for(int i = 1; i <= n; i++){
+        presum +=temp[i];
+        int target = total_sum - presum;
+        if(freq.count(target)) ans = min(ans, i-freq[target]+1);
+
+        freq[presum] = i + 1;
+    }
 
         
-        int total_sum = accumulate(v.begin(),v.end(),0);
-        map<int,int>pos;
-
-        pos[0] = 0;
-        int prefixsum = 0,ans = INT_MAX;
-        for(int r = 0; r < n; r++){
-            prefixsum += v[r];
-            int target = prefixsum - total_sum;
-            if(pos.count(target)) ans = min(ans,r-pos[target] + 1);
-            pos[prefixsum] = r + 1;
-        }
-        if( ans == INT_MAX) cout << -1 << endl;
-        else cout << ans << endl;
-
-
+      
+  if(ans == INT_MAX ) cout << -1 << endl;
+  else cout << ans << endl;
 
 
 
