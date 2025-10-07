38519e6ff456e44d1401ee02a90b5510910e27f5
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-10-08 01:20:59 +0530

    Auto-sync: 2025-10-08 01:20:59

--- 2145C.cpp
+++ 2145C.cpp
@@ -9,6 +9,22 @@ void solve() {
    
     int n; cin >> n;
     string s; cin >> s;
+    int curr = count(s.begin(),s.end(),'a') - count(s.begin(), s.end(),'b');
+
+    map<int,int>mpp;
+
+    int pr = 0;
+    mpp[pr] = -1;
+    int ans = n;
+
+    for(int i = 0; i < n; i++){
+        if(s[i] == 'a') pr += 1;
+        else pr += -1;
+
+        if(mpp.count(pr-curr)) ans = min(ans,i - mpp[pr - curr]);
+    }
+
+    cout << (ans == n ? -1 : ans) << endl;
     
    
 
