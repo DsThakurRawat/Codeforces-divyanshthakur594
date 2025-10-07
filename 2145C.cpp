d34c075fa35dbcf45a266be48744c1fbb73eca79
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-10-08 01:07:17 +0530

    Auto-sync: 2025-10-08 01:07:17

--- 2145C.cpp
+++ 2145C.cpp
@@ -1,76 +1,57 @@
-
-// simple template-test2
-// code by divyansh rawat
-
 #include <bits/stdc++.h>
 using namespace std;
 
 void solve() {
-   
-    int n; cin >> n;
-    string s; cin >> s;
-   
+    int n;
+    string s;
+    cin >> n >> s;
 
+    // Count number of 'a' and 'b'
+    int count_a = count(s.begin(), s.end(), 'a');
+    int count_b = count(s.begin(), s.end(), 'b');
 
-    int A = 0;
-    int B = 0;
-
-    for(auto c : s){
-        if ( c == 'a') A++;
-        else B++;
-    }
-    if( A == B){
-        cout << 0 << endl;
+    // If already balanced
+    if (count_a == count_b) {
+        cout << 0 << "\n";
         return;
-
-    }
-    int diff = A - B;
-
-    map<int,int> freq;
-
-    freq[0] = 0;
-    int presum = 0, ans = INT_MAX;
-
-    vector<int>temp(n);
-    for(int i = 0; i < n; i++){
-        if(s[i] == 'a') temp[i] = 1;
-        else temp[i] = -1;
-    }
-   int total_sum = accumulate(temp.begin(),temp.end(),0);
-
-
-
-
-    for(int i = 0; i < n; i++){
-        presum +=temp[i];
-        int target = -total_sum + presum;
-        if(freq.count(target)) ans = min(ans, i-freq[target]+1);
-        if(!freq.count(presum))  freq[presum] = i + 1;
-         
     }
 
-        
-      
-  if(ans == INT_MAX ) cout << -1 << endl;
-  else cout << ans << endl;
+    // Impossible case: difference is larger than string length? Actually always possible
+    // but let's use prefix sum + map to find minimum segment to remove
+    vector<int> v(n);
+    for (int i = 0; i < n; i++)
+        v[i] = (s[i] == 'a' ? 1 : -1);
 
+    int total_sum = count_a - count_b;  // difference to fix
+    map<int, int> pos;  // stores first occurrence of prefix sums
+    pos[0] = 0;
 
+    int prefixsum = 0;
+    int ans = INT_MAX;
 
+    for (int r = 0; r < n; r++) {
+        prefixsum += v[r];
+        int target = prefixsum - total_sum; // segment sum needed
+        if (pos.count(target))
+            ans = min(ans, r - pos[target] + 1);
+        if (!pos.count(prefixsum))
+            pos[prefixsum] = r + 1;  // store first occurrence
+    }
 
-
-
-
-
-
-
-  
+    if (ans == INT_MAX)
+        cout << -1 << "\n";
+    else
+        cout << ans << "\n";
 }
 
 int main() {
-    int t ;
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
     cin >> t;
-    while (t--) {
+    while (t--)
         solve();
-    }
+
     return 0;
 }
