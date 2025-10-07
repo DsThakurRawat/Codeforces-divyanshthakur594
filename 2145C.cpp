6576939fc3981f49a9649b21da44f9351e28ed5e
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-10-07 23:10:02 +0530

    Auto-sync: 2025-10-07 23:10:02

--- 2145C.cpp
+++ 2145C.cpp
@@ -9,14 +9,38 @@ void solve() {
     // minimum number of consecutive letters that monocarp needs to remove from his string
     int n; cin >> n;
     string s; cin >> s;
-    map<char,int>mpp;
-    for(int i = 0; i < s.size(); i++){
-        mpp[s[i]]++;
-    }
-    if(mpp['a'] == mpp['b']) cout << 0 << endl;
-    if(mpp['a'] == 0 || mpp['b'] == 0 ) cout << -1 << endl;
-                 
-    
+   // map<char,int>mpp;
+  //  for(int i = 0; i < s.size(); i++){
+   //     mpp[s[i]]++;
+   // }
+  //  if(mpp['a'] == mpp['b']) cout << 0 << endl;
+   // if(mpp['a'] == 0 || mpp['b'] == 0 ) cout << -1 << endl;
+    // we have to effectively find the shortest substring having equal number of a and b
+
+
+        vector<int>v(n);
+        for(int i = 0; i < n; i++){
+            if(s[i] == 'a') v[i] = -1;
+            else s[i] = 1;
+        }
+        // calculationg using prefix sum
+
+        
+        int total_sum = accumulate(v.begin(),v.end(),0);
+        map<int,int>pos;
+
+        pos[0] = 0;
+        int prefixsum = 0,ans = INT_MAX;
+        for(int r = 0; r < n; r++){
+            prefixsum += v[r];
+            int target = prefixsum - total_sum;
+            if(pos.contains(target)) ans = min(ans,r-pos[target] + 1);
+        }
+
+
+
+
+
 
 
 
