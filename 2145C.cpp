e5b92013b37b1b1c9c2157cd9b963022defc2dc7
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-10-08 01:22:16 +0530

    Auto-sync: 2025-10-08 01:22:16

--- 2145C.cpp
+++ 2145C.cpp
@@ -1,51 +1,31 @@
-
-// simple template-test2
-// code by divyansh rawat
-
 #include <bits/stdc++.h>
+ 
 using namespace std;
-
-void solve() {
-   
-    int n; cin >> n;
-    string s; cin >> s;
-    int curr = count(s.begin(),s.end(),'a') - count(s.begin(), s.end(),'b');
-
-    map<int,int>mpp;
-
+ 
+#define forn(i, n) for(int i = 0; i < int(n); i++) 
+ 
+void solve() {                    
+    int n;
+    string s;
+    cin >> n >> s;
+    int cur = count(s.begin(), s.end(), 'a') - count(s.begin(), s.end(), 'b');
+    map<int, int> lst;
     int pr = 0;
-    mpp[pr] = -1;
+    lst[pr] = -1;
     int ans = n;
-
-    for(int i = 0; i < n; i++){
-        if(s[i] == 'a') pr += 1;
-        else pr += -1;
-
-        if(mpp.count(pr-curr)) ans = min(ans,i - mpp[pr - curr]);
+    forn(i, n){
+        pr += s[i] == 'a' ? 1 : -1;
+        lst[pr] = i;
+        if (lst.count(pr - cur))
+            ans = min(ans, i - lst[pr - cur]);
     }
-
-    cout << (ans == n ? -1 : ans) << endl;
-    
-   
-
-
-    
-
-
-
-
-
-
-
-
-  
+    cout << (ans == n ? -1 : ans) << '\n';
 }
-
+ 
 int main() {
-    int t ;
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int t;
     cin >> t;
-    while (t--) {
-        solve();
-    }
-    return 0;
+    forn(i, t) solve();
 }
