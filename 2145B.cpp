575f0c8b4f768998810ae592cdfb45c1fdc9821e
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-10-09 02:06:44 +0530

    Auto-sync: 2025-10-09 02:06:44

--- 2145B.cpp
+++ 2145B.cpp
@@ -11,9 +11,7 @@ void solve() {
     // char 0 mono remove the top cards
     // char 1 mono remove bottom card
     // char 2 if either top or bottom card can be removed
-
     int top(0),bottom(0), uncretain(0);
-
     for(int i = 0; i < k; i++){
         if(s[i] == '0') top++;
         else if(s[i] == '1') bottom++;
@@ -29,15 +27,7 @@ void solve() {
     }
     for(int i = top; i < top+uncretain; i++) ans[i] = '?';
     for(int i = n - bottom - uncretain; i < n-bottom; i++) ans[i] = '?';
-    cout << ans << endl;
-
-
-
-
-
-
-
-  
+    cout << ans << endl;  
 }
 
 int main() {
