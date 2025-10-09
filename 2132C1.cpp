a2ca9f11f2e077e4439d8deade3f5d86c0d0525a
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-10-09 23:51:39 +0530

    Auto-sync: 2025-10-09 23:51:39

--- 2132C1.cpp
+++ 2132C1.cpp
@@ -1,8 +1,24 @@
+
+// simple template-test2
+// code by divyansh rawat
+
+
 #include <bits/stdc++.h>
 using namespace std;
+long long intLog3(long long n) {
+    long long x = 0;
+    long long val = 1;
+    while (val * 3 <= n) {
+        val *= 3;
+        x++;
+    }
+    return x;
+}
+ 
 
-int main() {
-    long long n;
+void solve() {
+  // requires determining the minimum cost with the least number of deals
+   long long n;
     cin >> n;
 
     // Compute log base 3 using floating point
@@ -15,4 +31,15 @@ int main() {
     long long complete = res1 + res3;
 
     cout << complete << endl;  // 2250964728
+  
+
+}
+
+int main() {
+    int t ;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
 }
