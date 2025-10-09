7e7823ffc9f3322b90c96c96ee05d628a1e1501d
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-10-10 00:04:41 +0530

    Auto-sync: 2025-10-10 00:04:41

--- 2132C1.cpp
+++ 2132C1.cpp
@@ -5,33 +5,29 @@
 
 #include <bits/stdc++.h>
 using namespace std;
-long long intLog3(long long n) {
-    long long x = 0;
-    long long val = 1;
-    while (val * 3 <= n) {
-        val *= 3;
-        x++;
-    }
-    return x;
-}
  
 
 void solve() {
   // requires determining the minimum cost with the least number of deals
-   long long n;
-    cin >> n;
 
-    // Compute log base 3 using floating point
-    long double x = log(n) / log(3.0);   // full precision
-    long long x_int = (long long)x;      // integer part of x
+   long long  n;
+  cin >> n;
+  long double x = log(n)/log(3);
 
-    // Compute the formula carefully using integer division
-    long long res1 = 3 * n;
-    long long res3 = (x_int * n) / 3;    // multiply first, divide after
-    long long complete = res1 + res3;
+  // making eqn
 
-    cout << complete << endl;  // 2250964728
+  long long x_int = (long long)x;
+ 
   
+
+  long long   res1 = 3*n;
+
+  long long res3 = (long long)((x_int * (long double)n) / 3.0);
+
+  long long  complete = res1 + res3;
+ 
+ cout << complete << endl;
+
 
 }
 
