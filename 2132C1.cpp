e56785917c7315fe09afce67534c706cf83e828e
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-10-09 23:48:32 +0530

    Auto-sync: 2025-10-09 23:48:32

--- 2132C1.cpp
+++ 2132C1.cpp
@@ -9,27 +9,17 @@ using namespace std;
 
 void solve() {
   // requires determining the minimum cost with the least number of deals
+long long n;
+    cin >> n;
 
-   long long  n;
-  cin >> n;
-  long double x = log(n)/log(3);
+    long double x = log(n)/log(3);              // full double, not truncated yet
+    long long res1 = 3 * n;
+    long long res3 = (long long)((x * n) / 3);  // multiply and divide as double, truncate once
+    long long complete = res1 + res3;
 
-  // making eqn
-
-  long long x_int = (long long)x;
- 
-  
-
-  long long   res1 = 3*n;
-
-  long long res3 = (long long)((x_int * (long double)n) / 3.0);
-
-  long long  complete = res1 + res3;
- 
- cout << complete << endl;
-
-
+    cout << complete << endl;  // 2,250,964,728
 }
+  
 
 int main() {
     int t ;
