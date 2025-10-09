891cfcbab97075f26fbcd84ca324835965dd10dc
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-10-09 23:25:13 +0530

    Auto-sync: 2025-10-09 23:25:13

--- 2132C1.cpp
+++ 2132C1.cpp
@@ -2,6 +2,7 @@
 // simple template-test2
 // code by divyansh rawat
 
+
 #include <bits/stdc++.h>
 using namespace std;
  
@@ -13,16 +14,16 @@ void solve() {
   cin >> n;
 
   // making eqn
-  int x = log(n)/log(3);
-  int direc = n;
+  // Step 1: compute x
+    long double x = log(n) / log(3);      // natural logs
+    long long x_int = (long long)x;       // integer part only
 
-  int res1 = 3*direc;
-  int res2 = direc/3;
- int res3 = x*res2;
+    // Step 2: compute formula using integer arithmetic
+    long long part1 = 3 * n;
+    long long part2 = (x_int * n) / 3;    // integer division
+    long long ans = part1 + part2;
 
-  int complete = res1 + res3;
- 
- cout << complete << endl;
+    cout << ans << endl;
 
 
 }
