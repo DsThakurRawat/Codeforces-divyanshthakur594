aa745c8024b6ce47e90a955f5f31c762e70675d6
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-10-09 23:43:25 +0530

    Auto-sync: 2025-10-09 23:43:25

--- 2132C1.cpp
+++ 2132C1.cpp
@@ -10,18 +10,18 @@ using namespace std;
 void solve() {
   // requires determining the minimum cost with the least number of deals
 
-   int  n;
+   long long  n;
   cin >> n;
-
-  // making eqn
   int x = log(n)/log(3);
-  int direc = n;
+  // making eqn
+ 
+  long long direc = n;
 
-  int  res1 = 3*direc;
-  long res2 = int(direc/3);
- int res3 = x*res2;
+  long long   res1 = 3*direc;
+  long long  res2 = (direc/3);
+  long long  res3 = x*res2;
 
-  int complete = res1 + res3;
+  long long  complete = res1 + res3;
  
  cout << complete << endl;
 
