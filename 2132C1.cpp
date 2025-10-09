fae289f6e7c94f3e49f6b1b3149d068cacb66966
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-10-09 23:51:16 +0530

    Auto-sync: 2025-10-09 23:51:16

--- 2132C1.cpp
+++ 2132C1.cpp
@@ -1,42 +1,18 @@
-
-// simple template-test2
-// code by divyansh rawat
-
-
 #include <bits/stdc++.h>
 using namespace std;
-long long intLog3(long long n) {
-    long long x = 0;
-    long long val = 1;
-    while (val * 3 <= n) {
-        val *= 3;
-        x++;
-    }
-    return x;
-}
- 
 
-void solve() {
-  // requires determining the minimum cost with the least number of deals
+int main() {
     long long n;
     cin >> n;
 
-    long long x = intLog3(n);        // integer part of log3(n)
+    // Compute log base 3 using floating point
+    long double x = log(n) / log(3.0);   // full precision
+    long long x_int = (long long)x;      // integer part of x
+
+    // Compute the formula carefully using integer division
     long long res1 = 3 * n;
-    long long res3 = (x * n) / 3;    // multiply first, divide by 3 (integer division)
+    long long res3 = (x_int * n) / 3;    // multiply first, divide after
     long long complete = res1 + res3;
 
-    cout << complete << endl;  
-
-  
-
-}
-
-int main() {
-    int t ;
-    cin >> t;
-    while (t--) {
-        solve();
-    }
-    return 0;
+    cout << complete << endl;  // 2250964728
 }
