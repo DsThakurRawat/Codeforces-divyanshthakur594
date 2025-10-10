d43954b2ffb5442d63758a5c330502ea11158b4f
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-10-10 17:16:31 +0530

    Auto-sync: 2025-10-10 17:16:31

---
+++ 2041A.cpp
@@ -0,0 +1,27 @@
+
+// simple template-test2
+// code by divyansh rawat
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    int sum = 15;
+    vector<int>nums(5);
+    for(int i = 0; i < 5; i++) cin >> nums[i];
+    int sum2 = accumulate(nums.begin(),nums.end(),0);
+
+    cout << sum - sum2 << endl;
+
+
+
+  
+}
+
+int main() {
+    int t ;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
