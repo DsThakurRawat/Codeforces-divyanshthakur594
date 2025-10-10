a2371e375e5d22c132e5b750c881f7c77dcc2dc6
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-10-10 17:17:08 +0530

    Auto-sync: 2025-10-10 17:17:08

--- 2041A.cpp
+++ 2041A.cpp
@@ -18,10 +18,10 @@ void solve() {
 }
 
 int main() {
-    int t ;
-    cin >> t;
-    while (t--) {
+   // int t ;
+   // cin >> t;
+   // while (t--) {
         solve();
-    }
+    //}
     return 0;
 }
