1717cef7d061bfbe08b56759cc5432efa435cc86
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-10-10 20:32:46 +0530

    Auto-sync: 2025-10-10 20:32:46

--- 2153B.cpp
+++ 2153B.cpp
@@ -6,14 +6,14 @@
 using namespace std;
 
 void solve() {
-  int x, y, z;
+  long long x, y, z;
     cin >> x >> y >> z;
     bool flag  = 1;
     for (int i = 0; i < 30; i++) {
-        int xb = (x >>i ) & 1;
-        int yb = (y >> i) & 1;
-        int zb = (z >> i) & 1;
-        int sum = xb + yb + zb;
+        long long  xb = (x >>i ) & 1;
+        long long  yb = (y >> i) & 1;
+        long long  zb = (z >> i) & 1;
+        long long  sum = xb + yb + zb;
      
         if (sum == 2) {
             flag = 0;
