54d8095c8136dc69fbe25c52b50f2738a2a4ba1c
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-10-10 20:40:31 +0530

    Auto-sync: 2025-10-10 20:40:31

--- 2153B.cpp
+++ 2153B.cpp
@@ -9,11 +9,14 @@ void solve() {
   long long x, y, z;
     cin >> x >> y >> z;
     bool flag  = 1;
-    for (int i = 0; i < 30; i++) {
-        long long  xb = (x >>i ) & 1;
-        long long  yb = (y >> i) & 1;
-        long long  zb = (z >> i) & 1;
-        long long  sum = xb + yb + zb;
+    bitset<30>res1(x),res2(y),res3(z);
+    for(int i = 0; i < 30; i++) {
+        long long   r1 = res1[i];       
+        long long   r2  = res2[i];
+         long long  r3  = res3[i];
+
+        long long  sum = r1 + r2 + r3;
+
      
         if (sum == 2) {
             flag = 0;
