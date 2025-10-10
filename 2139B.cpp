aac09766dcb645c16a400b1975e484202ddd4a9d
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-10-10 18:57:47 +0530

    Auto-sync: 2025-10-10 18:57:47

---
+++ 2139B.cpp
@@ -0,0 +1,29 @@
+
+// simple template-test2
+// code by divyansh rawat
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    long long  n,m;
+    cin >> n >> m;
+    vector<long long > cakes(n);
+    for(int i = 0; i < n; i++) cin >> cakes[i];
+    
+
+
+
+
+
+  
+}
+
+int main() {
+    int t ;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
