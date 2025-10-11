22dabd5f3227435e95c7738c3b367c52b75c099a
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-10-11 08:59:25 +0530

    Auto-sync: 2025-10-11 08:59:25

--- 2153D.cpp
+++ 2153D.cpp
@@ -3,17 +3,15 @@
 
 #include <bits/stdc++.h>
 using namespace std;
-
 using ll = long long;
 const ll INF = 4e18;
 
-
-ll block_cost(const vector<ll>& arr, int l, int r) {
-    vector<ll> seg(arr.begin() + l, arr.begin() + r + 1);
-    sort(seg.begin(), seg.end());
-    ll m = seg[seg.size() / 2];
+ll median_cost(const vector<ll>& seg) {
+    vector<ll> temp = seg;
+    sort(temp.begin(), temp.end());
+    ll m = temp[temp.size() / 2];
     ll cost = 0;
-    for (ll x : seg) cost += llabs(x - m);
+    for (ll x : temp) cost += abs(x - m);
     return cost;
 }
 
@@ -31,35 +29,31 @@ void dp() {
         return;
     }
 
-  
     vector<ll> ad(2 * n);
     for (int i = 0; i < 2 * n; ++i)
         ad[i] = a[i % n];
 
-    
-    vector<vector<ll>> cost(2 * n, vector<ll>(2 * n, INF));
-    for (int i = 0; i < 2 * n; ++i) {
-        for (int j = i + 1; j < min(2 * n, i + n); ++j)
-            cost[i][j] = block_cost(ad, i, j);
-    }
-
-    ll ans = INF;
-
    
-    for (int s = 0; s < n; ++s) {
-        vector<ll> dp(n, INF);
-        for (int i = 0; i < n; ++i) {
-            for (int j = -1; j < i; ++j) {
-                int len = i - j;
-                if (len < 2) continue; 
-                ll prev = (j == -1 ? 0 : dp[j]);
-                dp[i] = min(dp[i], prev + cost[s + j + 1][s + i]);
-            }
+    vector<vector<ll>> cost(n, vector<ll>(n, INF));
+    for (int l = 0; l < n; ++l) {
+        vector<ll> seg;
+        for (int r = l; r < n; ++r) {
+            seg.push_back(a[r]);
+            if (r - l + 1 >= 2)
+                cost[l][r] = median_cost(seg);
         }
-        ans = min(ans, dp[n - 1]);
     }
 
-    cout << ans << '\n';
+    
+    vector<ll> dp(n, INF);
+    for (int i = 1; i < n; ++i) {
+        for (int j = -1; j < i - 1; ++j) {
+            ll prev = (j == -1 ? 0 : dp[j]);
+            dp[i] = min(dp[i], prev + cost[j + 1][i]);
+        }
+    }
+
+    cout << dp[n - 1] << '\n';
 }
 
 int main() {
