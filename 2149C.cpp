bb2e0879b9184eb8964f0dbf620f1f9eb8add5a4
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-10-11 09:20:40 +0530

    Auto-sync: 2025-10-11 09:20:40

--- 2149C.cpp
+++ 2149C.cpp
@@ -8,6 +8,7 @@ using namespace std;
 void solve() {
     int n,k; cin >> n >> k;
     vector<int>nums(n);
+    map<int,int>mpp;
     for(int i = 0; i < n; i++) cin >> nums[i];
 
   //For each set of input data, output one integer — the minimum number of operations required to satisfy the condition MEX(a)=k
@@ -15,8 +16,17 @@ void solve() {
 
   for(int i = 0; i < n; i++){
     if(nums[i] == k) count++;
+    if(nums[i] < k) mpp[nums[i]]++;
   }
-  cout << count << endl;
+  int ans = 0;
+
+  for(int i = 0; i <n; i++){
+    if(!mpp[i]){
+        ans++;
+    }
+  }
+  cout << max(ans,count) << endl;
+ 
 
 
 }
