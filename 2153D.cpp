15d25fbf0434195de5834c57e1bf2be1bac83226
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-10-11 09:02:19 +0530

    Auto-sync: 2025-10-11 09:02:19

--- 2153D.cpp
+++ 2153D.cpp
@@ -1,66 +1,60 @@
-// simple template-test2
-// code by divyansh rawat
-
 #include <bits/stdc++.h>
 using namespace std;
+ 
 using ll = long long;
-const ll INF = 4e18;
-
-// Function to compute minimal cost to make a segment equal-valued
-ll median_cost(const vector<ll>& seg) {
-    vector<ll> temp = seg;
-    sort(temp.begin(), temp.end());
-    ll m = temp[temp.size() / 2]; // median
-    ll cost = 0;
-    for (ll x : temp) cost += llabs(x - m);
-    return cost;
+ 
+const ll LINF = 1000000000000000005;
+const int MAXN = 500005;
+ 
+int t;
+int n;
+int a[MAXN];
+ll dp[MAXN];
+ 
+ll cost(int x, int y) { return abs(x - y); }
+ 
+ll cost(int x, int y, int z) {
+    if (x > y) {
+        swap(x, y);
+    }
+    if (y > z) {
+        swap(y, z);
+    }
+    if (x > y) {
+        swap(x, y);
+    }
+    return z - x;
 }
-
-void dp() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-
-    int n;
-    cin >> n;
-    vector<ll> a(n);
-    for (int i = 0; i < n; ++i) cin >> a[i];
-
-    if (n == 1) {
-        cout << 0 << endl;
-        return;
-    }
-
-    // Precompute cost for each segment [l..r] (length >= 2)
-    vector<vector<ll>> cost(n, vector<ll>(n, INF));
-    for (int l = 0; l < n; ++l) {
-        vector<ll> seg;
-        for (int r = l; r < n; ++r) {
-            seg.push_back(a[r]);
-            if (r - l + 1 >= 2)
-                cost[l][r] = median_cost(seg);
-        }
-    }
-
-    // DP: dp[i] = min cost to make prefix [0..i] nice
-    vector<ll> dp(n, INF);
-    for (int i = 1; i < n; ++i) {
-        for (int j = -1; j < i - 1; ++j) {
-            ll prev = (j == -1 ? 0 : dp[j]);
-            dp[i] = min(dp[i], prev + cost[j + 1][i]);
-        }
-    }
-
-    cout << dp[n - 1] << endl;
-}
-
+ 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-
-    int t;
+    ios::sync_with_stdio(0), cin.tie(0);
     cin >> t;
     while (t--) {
-        dp();
+        cin >> n;
+        for (int i = 1; i <= n; i++) {
+            cin >> a[i];
+        }
+ 
+        ll ans = LINF;
+        for (int cyc = 0; cyc < 4; cyc++) {
+            dp[0] = 0;
+            dp[1] = LINF;
+            for (int i = 2; i <= n; i++) {
+                dp[i] = dp[i - 2] + cost(a[i - 1], a[i]);
+                if (i >= 3) {
+                    dp[i] =
+                        min(dp[i], dp[i - 3] + cost(a[i - 2], a[i - 1], a[i]));
+                }
+            }
+            ans = min(ans, dp[n]);
+ 
+            // cyclic shift anti-clockwise
+            for (int i = 2; i <= n; i++) {
+                swap(a[i], a[i - 1]);
+            }
+        }
+ 
+        cout << ans << '\n';
     }
     return 0;
 }
