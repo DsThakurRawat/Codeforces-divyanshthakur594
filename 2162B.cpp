a061c4968544a1cc52242b978e460982e436db5e
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-10-17 20:50:03 +0530

    Auto-sync: 2025-10-17 20:50:03

--- 2162B.cpp
+++ 2162B.cpp
@@ -5,71 +5,46 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
-
-bool isPalindrome(const string &s) {
-    int n = s.size();
-    for(int i = 0; i < n/2; i++)
-        if(s[i] != s[n-1-i])
-            return false;
-    return true;
-}
-
 void solve() {
     int n;
     string s;
     cin >> n >> s;
     
+    vector<int> p;
+    int l = 0, r = n-1;
     
-    if(isPalindrome(s)) {
+    while(l < r && s[l] == '1' && s[r] == '0') l++, r--; 
+    
+    int first_zero = -1, last_one = -1;
+    for(int i = 0; i < n; i++) {
+        if(s[i] == '0') { first_zero = i; break; }
+    }
+    for(int i = n-1; i >= 0; i--) {
+        if(s[i] == '1') { last_one = i; break; }
+    }
+    
+    if(first_zero == -1 || last_one == -1 || first_zero > last_one) {
         cout << 0 << "\n";
         return;
     }
     
-   
-    for(int i = 0; i < n; i++) {
-        string x = s.substr(0,i) + s.substr(i+1);
-        if(isPalindrome(x)) {
-            cout << 1 << "\n" << i+1 << "\n";
-            return;
-        }
+    for(int i = 0; i <= last_one; i++) {
+        if(s[i] == '0') p.push_back(i+1);
+    }
+    for(int i = first_zero; i < n; i++) {
+        if(s[i] == '1') p.push_back(i+1);
     }
     
-    
-    for(int i = 0; i < n; i++) {
-        for(int j = i+1; j < n; j++) {
-            if(s[i] <= s[j]) {
-                string x = "";
-                for(int k = 0; k < n; k++) {
-                    if(k != i && k != j) x += s[k];
-                }
-                if(isPalindrome(x)) {
-                    cout << 2 << "\n" << i+1 << " " << j+1 << "\n";
-                    return;
-         }
-     }
-        }
-    }
-    
- 
-    vector<int> indices;
-    for(int i = 0; i < n; i++) {
-        if(s[i] == '0') indices.push_back(i+1);
-    }
-    for(int i = 0; i < n; i++) {
-        if(s[i] == '1') indices.push_back(i+1);
-    }
-    cout << indices.size() << "\n";
-    for(int idx : indices) cout << idx << " ";
+    sort(p.begin(), p.end());
+    cout << p.size() << "\n";
+    for(int idx : p) cout << idx << " ";
     cout << "\n";
 }
 
-
 int main() {
-    int t ;
-    cin >> t;
-    while (t--) {
-        solve();
-    }
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t; cin >> t;
+    while(t--) solve();
     return 0;
 }
