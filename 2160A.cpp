115cfd8991391caa4758f887f2f2c0889bdb4758
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-10-17 20:13:28 +0530

    Auto-sync: 2025-10-17 20:13:28

--- 2160A.cpp
+++ 2160A.cpp
@@ -7,8 +7,8 @@ using namespace std;
 
 void solve() {
 
-    int n;
-    cin >> n;
+    int n;   cin >> n;
+  
     vector<int>v(n);
     for(int i = 0; i < n; i++) cin >> v[i];
     int best_avg = 0;
@@ -21,7 +21,7 @@ void solve() {
         for(int j = i; j < n; j++){
 
             sum += v[j];
-            avg = (sum)/i - j + 1;
+            avg = (sum)/(i - j + 1);
             best_avg = max(avg,best_avg);
 
 
