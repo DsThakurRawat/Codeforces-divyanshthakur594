95b85a49d58114dc00cbb1a2d0beca24d7d6c95d
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-10-18 00:26:14 +0530

    Auto-sync: 2025-10-18 00:26:14

--- 2162B.cpp
+++ 2162B.cpp
@@ -12,16 +12,12 @@ void solve() {
     cin >> n >> s;
     int len = 0;
     vector<int>indexarray;
-
     // each character is not greater than next on
     //  and x must be pallindrome
-
     for(int i = 0; i < n; i++){
         if(s[i] == '1') indexarray.push_back(i+1);
     }
-
     int x = indexarray.size();
-    
     cout << x << endl;
    for(auto it : indexarray){
     cout << it << " ";
