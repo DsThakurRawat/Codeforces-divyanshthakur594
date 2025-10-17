48302ce9e21054da4cd1de86a4f0e34da21b454a
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-10-17 20:32:23 +0530

    Auto-sync: 2025-10-17 20:32:23

---
+++ 2162B.cpp
@@ -0,0 +1,58 @@
+
+// simple template-test2
+// code by divyansh rawat
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+  int n;
+    cin >> n;
+    string s;
+    cin >> s;
+
+    for(int i = 0; i < n; i++) {
+        vector<bool> used(n, false);
+        string x;
+        char save = '0';
+        
+        for(int j = i; j < n; j++) {
+            if(s[j] >= save) {
+                used[j] = true;
+                x += s[j];
+                save = s[j];
+            }
+        }
+
+        string remaining;
+        for(int j = 0; j < n; j++) {
+            if(!used[j]) {
+                remaining += s[j];
+            }
+        }
+
+        bool isPalindrome = true;
+        for(int j = 0; j < remaining.length()/2; j++) {
+            if(remaining[j] != remaining[remaining.length()-1-j]) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        if(isPalindrome) {
+            cout << x << endl;
+            return;
+        }
+    }
+    
+    cout << "-1" << endl;
+}
+
+int main() {
+    int t ;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
