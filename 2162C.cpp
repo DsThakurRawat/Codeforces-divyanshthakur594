e2ecb23ab7de1ed9356474e11a229d36ea181ef4
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-10-17 20:22:00 +0530

    Auto-sync: 2025-10-17 20:22:00

--- 2162C.cpp
+++ 2162C.cpp
@@ -15,23 +15,25 @@ void solve() {
     }
     
     bitset<32> bits_of_a(a);
+
     bitset<32> bits_of_b(b);
+
     vector<int> ops;
     
     if (a == b){
         cout << "0" << endl;
         return;
-      }
+     }
     
     if (a != 0){
         ops.push_back(a);
         bits_of_a ^= bitset<32>(a);
-         }
+    }
     
     if (b != 0){
            ops.push_back(b);
            bits_of_b ^= bitset<32>(b);
-                }
+    }
       int m =  ops.size();
       cout << m << endl;
     for (int x : ops){
