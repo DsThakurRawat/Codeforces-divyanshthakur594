807f3c3f209e3c7cdbf5be8f9fea13e3914efa86
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-10-17 19:17:58 +0530

    Auto-sync: 2025-10-17 19:17:58

---
+++ 2160A.cpp
@@ -0,0 +1,26 @@
+
+// simple template-test2
+// code by divyansh rawat
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+
+    int n;
+    vector<int>v(n);
+    for(int i = 0; i < n; i++) cin >> v[i];
+
+    
+
+  
+}
+
+int main() {
+    int t ;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
