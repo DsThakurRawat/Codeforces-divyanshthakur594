8323a229296196f58e0210644dcb9f3487f4acb4
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-10-19 00:59:26 +0530

    Auto-sync: 2025-10-19 00:59:26

--- 2160A.cpp
+++ 2160A.cpp
@@ -10,9 +10,42 @@ void solve() {
     int n;   cin >> n;
   
     vector<int>v(n);
+
+
     for(int i = 0; i < n; i++) cin >> v[i];
 
+    int i = 0;
+    int flag = 0;
+
+    for(int i = 0; i < v.size(); i++){
+        if(v[i] != 0) flag = 1;
+
+
+    }
+     set<int> s(v.begin(), v.end());  
+    int ans = 0;
+
+    for(int i = 0; i < n; i++) {
+        if (s.find(v[i] + 1) == s.end()) { 
+            ans = v[i] + 1;
+            break;
+        }
+    }
+
+    cout << ans << endl;
+
+
+
+
     
+
+
+
+
+
+
+
+
 
 
     
