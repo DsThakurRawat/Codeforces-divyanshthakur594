3c3b831fc98611ccd49bbb9b5a8f756bfd296fa6
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-10-19 00:16:51 +0530

    Auto-sync: 2025-10-19 00:16:51

--- 2160A.cpp
+++ 2160A.cpp
@@ -11,28 +11,11 @@ void solve() {
   
     vector<int>v(n);
     for(int i = 0; i < n; i++) cin >> v[i];
-    int best_avg = -1e9;
-    int avg = 0;
-    // best avg = max sum of element in subaaryy/no of element in subaary
+
     
 
-    for(int i = 0; i < n; i++){
-        int sum = 0;
-        for(int j = i; j < n; j++){
 
-            sum += v[j];
-            avg = (double)sum/( j-i + 1);
-            best_avg = max(avg,best_avg);
-
-
-
-            
-            
-
-        }
-    }
-
-    cout << best_avg << endl;
+    
 
 
 
