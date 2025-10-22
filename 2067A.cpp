416df905e4ab7ef7850b3b1fcd7691d88395fa10
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-10-22 19:54:08 +0530

    Auto-sync: 2025-10-22 19:54:08

--- 2067A.cpp
+++ 2067A.cpp
@@ -6,7 +6,13 @@
 using namespace std;
 
 void solve() {
-    
+    int x , y;
+    cin >> x >> y;
+    if (x + 1 >= y && (x + 1 - y) % 9 == 0) {
+        cout << "Yes\n";
+    } else {
+        cout << "No\n";
+    }
   
 }
 
