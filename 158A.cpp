77eae21e6873808d3f09dadca9f14b4f082b2735
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-10-27 16:49:53 +0530

    Auto-sync: 2025-10-27 16:49:53

---
+++ 158A.cpp
@@ -0,0 +1,42 @@
+
+// simple template-test2
+// code by divyansh rawat
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+void solve() {
+
+    int n, k;
+    cin >> n >> k;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++) cin >> a[i];
+
+
+    int cutoff = a[k - 1];
+    int count = 0;
+
+    for (int score : a)
+        if (score >= cutoff && score > 0)
+            count++;
+
+    cout << count << endl;
+   
+
+
+
+  
+
+
+}
+
+int main() {
+    int t ;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
