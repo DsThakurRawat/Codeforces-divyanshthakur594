ea454d77505d9e488b0d9307331352648621388b
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-10-28 20:19:51 +0530

    Auto-sync: 2025-10-28 20:19:51

--- 2167D.cpp
+++ 2167D.cpp
@@ -7,6 +7,9 @@ using namespace std;
 
 void solve() {
     long long n; cin >> n;
+     vector<long long> primes = {
+        2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97
+    };
     vector<long long >v(n);
     for(long long i = 0; i < n; i++) cin >> v[i];
 
