fbd773f37d97bf9020d5d1f30a4b5341ad83053c
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-10-28 20:45:07 +0530

    Auto-sync: 2025-10-28 20:45:07

--- 2167C.cpp
+++ 2167C.cpp
@@ -11,7 +11,7 @@ void solve() {
         vector<long long> v(n);
         for (int i = 0; i < n; i++) cin >> v[i];
 
-        bool has_even = false, odd_flag = 0;
+        bool even_flag = false, odd_flag = 0;
         for (auto x : v) {
             if (x % 2 == 0) has_even = true;
             else odd_flag = 1;
