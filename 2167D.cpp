377c05352726c0ed2eec89fbead100517988ea55
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-10-28 20:30:06 +0530

    Auto-sync: 2025-10-28 20:30:06

--- 2167D.cpp
+++ 2167D.cpp
@@ -16,7 +16,7 @@ void solve() {
         if (is_prime[i] == 1){
             for (int j = i * i; j <= maxi; j += i)
                     is_prime[j] = 0;
-        }
+                }
     }
     vector<long long> primes;
     for (int i = 2; i <= maxi; i++) 
