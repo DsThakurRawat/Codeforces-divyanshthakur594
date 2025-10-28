4786f7cfda88a336e35e7a5e0703b12cd814ba20
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-10-28 20:51:25 +0530

    Auto-sync: 2025-10-28 20:51:25

--- 2167C.cpp
+++ 2167C.cpp
@@ -20,6 +20,9 @@ void solve() {
         sort(odd.begin(),odd.end());
         sort(evn.begin(),evn.end());
 
+        nd());
+   
+
         for (auto x : a) {
         if (x % 2 == 0) cout << e[i++] << " ";
         else cout << o[j++] << " ";
