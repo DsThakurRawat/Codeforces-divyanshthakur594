f40a4dd9a50beb3a646f43548e3c286a24a934e2
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-10-28 20:25:34 +0530

    Auto-sync: 2025-10-28 20:25:34

--- 2167D.cpp
+++ 2167D.cpp
@@ -47,7 +47,7 @@ void solve(const vector<long long> &primes) {
     else cout << -1 << "\n";
 }
 
-// ---------- main ----------
+
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
