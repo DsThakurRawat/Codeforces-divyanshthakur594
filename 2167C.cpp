cd58262480a4eda457d14d63ece69593c24360e1
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-10-28 20:43:24 +0530

    Auto-sync: 2025-10-28 20:43:24

--- 2167C.cpp
+++ 2167C.cpp
@@ -9,7 +9,20 @@ void solve() {
     int n; cin >> n;
     vector<long long> v(n);
     for(int i = 0; i < n; i++) cin >> v[i];
-    
+
+     bool has_even = false, has_odd = false;
+    for (auto x : a) {
+        if (x % 2 == 0) has_even = true;
+        else has_odd = true;
+    }
+
+    if (is_sorted(a.begin(), a.end())) {
+        cout << "YES\n";
+        return;
+    }
+
+    if (has_even && has_odd) cout << "YES\n";
+    else cout << "NO\n";
 
 
 
