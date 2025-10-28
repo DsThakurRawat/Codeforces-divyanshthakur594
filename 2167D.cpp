878823088459ee937f9b357f604e32292816f191
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-10-28 20:27:49 +0530

    Auto-sync: 2025-10-28 20:27:49

---
+++ 2167D.cpp
@@ -0,0 +1,53 @@
+// simple template-test2
+// code by divyansh rawat
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    long long n;
+    cin >> n;
+    vector<long long> v(n);
+    for (long long i = 0; i < n; i++) cin >> v[i];
+
+    // Generate prime numbers up to 1e6 (inside solve)
+    int limit = 1000000;
+    vector<int> is_prime(limit + 1, 1);
+    is_prime[0] = is_prime[1] = 0;
+
+    for (int i = 2; i * i <= limit; i++) {
+        if (is_prime[i] == 1) {
+            for (int j = i * i; j <= limit; j += i)
+                is_prime[j] = 0;
+        }
+    }
+
+    vector<long long> primes;
+    for (int i = 2; i <= limit; i++) 
+        if (is_prime[i] == 1) primes.push_back(i);
+
+    long long ans = (long long)1e19;
+
+    for (auto a : v) {
+        for (auto p : primes) {
+            if (a % p != 0) { 
+                ans = min(ans, p);
+                break;
+            }
+        }
+    }
+
+    if (ans <= (long long)1e18) cout << ans << "\n";
+    else cout << -1 << "\n";
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+    while (t--) solve();
+
+    return 0;
+}
