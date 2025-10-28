5259973b01ab100d471c9913ce86e69d91937ad3
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-10-28 21:03:45 +0530

    Auto-sync: 2025-10-28 21:03:45

--- 2167D.cpp
+++ 2167D.cpp
@@ -37,10 +37,7 @@ void solve() {
     else cout << -1 << endl;
     */
 
-       int n;
-    cin >> n;
-    vector<long long> v(n);
-    for (int i = 0; i < n; i++) cin >> v[i];
+     
 
     long long g = v[0];
     for (int i = 1; i < n; i++) g = gcd(g, v[i]);
