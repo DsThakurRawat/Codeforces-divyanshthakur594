c55caa0bccaaf3724b6aeef8420753a102c11233
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-10-28 20:21:50 +0530

    Auto-sync: 2025-10-28 20:21:50

--- 2167D.cpp
+++ 2167D.cpp
@@ -11,10 +11,10 @@ void solve() {
         2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97
     };
     vector<long long >v(n);
-    for(long long i = 0; i < n; i++) cin >> v[i];
-     int n; cin >> n;
-        long long ans = (long long)1e19;
-        for(int i=0;i<n;i++){
+        for(long long i = 0; i < n; i++) cin >> v[i];
+        int n; cin >> n;
+            long long ans = (long long)1e19;
+            for(int i=0;i<n;i++){
                 long long a; cin >> a;
                 for(long long p: prime){
                     if(a % p != 0){
