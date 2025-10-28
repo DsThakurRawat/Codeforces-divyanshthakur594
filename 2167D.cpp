e2c952eaf5e6496a3e76c2663cf8c973f4f16e3b
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-10-28 20:26:08 +0530

    Auto-sync: 2025-10-28 20:26:08

--- 2167D.cpp
+++ 2167D.cpp
@@ -7,16 +7,15 @@ using namespace std;
 
 
 vector<long long> f(int limit = 1000000) {
+    
     vector<bool> is_prime(limit + 1, true);
     is_prime[0] = is_prime[1] = false;
-
     for (int i = 2; i * i <= limit; i++) {
         if (is_prime[i]) {
             for (int j = i * i; j <= limit; j += i)
-                is_prime[j] = false;
+            is_prime[j] = false;
         }
     }
-
     vector<long long> primes;
     for (int i = 2; i <= limit; i++)
         if (is_prime[i]) primes.push_back(i);
