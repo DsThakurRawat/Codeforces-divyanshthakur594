9041d2e1ab00e29d1e9b899667f0f35bc12af5b8
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-11-01 00:34:39 +0530

    Auto-sync: 2025-11-01 00:34:39

--- 1873D.cpp
+++ 1873D.cpp
@@ -5,9 +5,14 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
+/*************  ✨ Windsurf Command ⭐  *************/
+/**
+ * This function solves a single test case.
+ */
+/*******  edad2baa-af76-4d8f-941d-5a1bbc0e6642  *******/
 void solve() {
   
+    
 }
 
 int main() {
