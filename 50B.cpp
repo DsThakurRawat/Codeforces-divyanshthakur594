190ce372820408d4a4d675589c94c0be799629e7
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-11-01 03:41:46 +0530

    Auto-sync: 2025-11-01 03:41:46

---
+++ 50B.cpp
@@ -0,0 +1,22 @@
+
+// simple template-test2
+// code by divyansh rawat
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    int n,m; cin >> n >> m;
+
+    cout << (n*m)/2 << endl;
+  
+}
+
+int main() {
+   // int t ;
+   // cin >> t;
+   // while (t--) {
+        solve();
+    //}
+    return 0;
+}
