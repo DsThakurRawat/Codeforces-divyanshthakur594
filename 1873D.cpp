df08d01341c60a435b0a211e5b09439c3ef60bc8
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-11-01 00:34:52 +0530

    Auto-sync: 2025-11-01 00:34:52

--- 1873D.cpp
+++ 1873D.cpp
@@ -7,7 +7,18 @@ using namespace std;
 
 
 void solve() {
-    
+    int n, k;
+	cin >> n >> k;
+	string s;
+	cin >> s;
+	int res = 0;
+	for (int i = 0; i < n; i++) {
+		if (s[i] == 'B') {
+			res++; i += k - 1;
+		}
+	}
+	cout << res << '\n';
+
   
 }
 
