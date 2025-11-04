2ba03de520ac0f351901173e40fdf101ef4fe6fc
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-11-04 14:18:06 +0530

    Auto-sync: 2025-11-04 14:18:06

--- test2.cpp
+++ test2.cpp
@@ -7,6 +7,23 @@ using namespace std;
 
 void solve() {
 
+     int n, m;
+        cin >> n >> m;
+        vector<long long> dp(n + 1, 0);
+        for (int i = 0; i < m; ++i) {
+            int x, y;
+            long long v;
+            cin >> x >> y >> v;
+            long long tempx = dp[x];
+            long long tempy = dp[y];
+            dp[x] = max(dp[x], tempy + v);
+            dp[y] = max(dp[y], tempx + v);
+        }
+        cout << *max_element(dp.begin() + 1, dp.end()) << endl;
+    }
+
+
+
   
 
 
