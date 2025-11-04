d378cd2bc5a790b7efe6a09b56ad2a02405e295f
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-11-04 11:12:22 +0530

    Auto-sync: 2025-11-04 11:12:22

--- 112A.cpp
+++ 112A.cpp
@@ -9,7 +9,8 @@ void solve() {
     string s1; cin >> s1;
     string s2; cin >>s2;
     // if first string is grater than 2nd print 1 and if 2nd is greater than 1st print -1 if both equal print 0;
-    
+
+    for(int i = 0; i < s.)
 
 }
 
