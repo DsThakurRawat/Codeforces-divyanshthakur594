b5ed5beb24ee7de753a10f1c6a985df6cbe9613b
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-11-04 11:27:49 +0530

    Auto-sync: 2025-11-04 11:27:49

--- 112A.cpp
+++ 112A.cpp
@@ -10,7 +10,6 @@ void solve() {
     string s2; cin >>s2;
     // if first string is grater than 2nd print 1 and if 2nd is greater than 1st print -1 if both equal print 0;
 
-    transform(s1.begin(), s1.end(), s1.begin(), ::tolower);
     transform(s2.begin(), s2.end(), s2.begin(), ::tolower);
     for(int i = 0; i < s1.size(); i++){
         if(s1[i] > s2[i]){
