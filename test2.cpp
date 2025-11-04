934e9743e569109982eabf4fb67c28f11e094acb
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-11-04 14:18:01 +0530

    Auto-sync: 2025-11-04 14:18:01

--- test2.cpp
+++ test2.cpp
@@ -7,23 +7,6 @@ using namespace std;
 
 void solve() {
 
-      int n, m;
-        cin >> n >> m;
-        vector<int> height(n + 1);
-        iota(height.begin(), height.end(), 0); 
-        vector<long long> temp(n + 1, 0);    
-        for (int i = 0; i < m; ++i) {
-            int x, y;
-            long long v;
-            cin >> x >> y >> v;
-            temp[height[x]] += v;
-            temp[height[y]] += v;
-            swap(height[x], height[y]);
-        }
-        cout << *max_element(temp.begin(), temp.end()) << endl;
-    }
-
-
   
 
 
