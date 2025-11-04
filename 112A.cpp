3c29af95c8c82be8604e11256ab71224f3b9a5c2
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-11-04 11:14:10 +0530

    Auto-sync: 2025-11-04 11:14:10

--- 112A.cpp
+++ 112A.cpp
@@ -11,7 +11,9 @@ void solve() {
     // if first string is grater than 2nd print 1 and if 2nd is greater than 1st print -1 if both equal print 0;
 
     for(int i = 0; i < s.size(); i++){
-        if(s1[i] > s[2])
+        if(s1[i] > s2[i]){
+            cout 
+        }
     }
 
 }
