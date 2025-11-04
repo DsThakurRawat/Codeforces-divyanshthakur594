0ba9de2a1953fbc470a7fb50dd50354cbf2b2d94
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-11-04 23:22:18 +0530

    Auto-sync: 2025-11-04 23:22:18

--- 112B.cpp
+++ 112B.cpp
@@ -10,7 +10,7 @@ void solve() {
     int n,x,y; cin >> n >> x >> y;
     n = n/2;
 
-    if( (x == n || x == n+1)&& ( y == n || y = n+1) cout << "NO"<<endl;
+    if( (x == n || x == n+1)&& (() y == n || y = n+1)) cout << "NO"<<endl;
     else cout <<"YES"<<endl;
 
 
