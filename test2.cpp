650fcea35502cc30c5cd6fa16f07ca67132d6232
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-11-04 14:11:52 +0530

    Auto-sync: 2025-11-04 14:11:52

--- test2.cpp
+++ test2.cpp
@@ -19,7 +19,6 @@ void solve() {
             dp[x] = max(dp[x], tempy + v);
             dp[y] = max(dp[y], tempx + v);
         }
-
         cout << *max_element(dp.begin() + 1, dp.end()) << endl;
     }
 
