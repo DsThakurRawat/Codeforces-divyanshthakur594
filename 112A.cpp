8ee0e62de007b5153670167fa2526abe8e145486
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-11-04 11:06:56 +0530

    Auto-sync: 2025-11-04 11:06:56

--- 112A.cpp
+++ 112A.cpp
@@ -9,7 +9,16 @@ void solve() {
   
 }
 
-
+/*************  ✨ Windsurf Command ⭐  *************/
+/**
+ * @brief Main function of the program
+ * 
+ * This function takes an integer t as input from the user and calls the solve function t times.
+ * 
+ * @param none
+ * @return 0 if the program is executed successfully
+ */
+/*******  5a6236a6-6c58-47cf-b1d9-325aedc29680  *******/
 int main() {
     int t ;
     cin >> t;
