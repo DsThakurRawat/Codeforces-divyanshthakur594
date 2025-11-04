47475c1ba0fb98ea29b84d373c13437a2d07252a
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-11-04 11:08:52 +0530

    Auto-sync: 2025-11-04 11:08:52

--- 112A.cpp
+++ 112A.cpp
@@ -5,11 +5,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-/*************  ✨ Windsurf Command ⭐  *************/
-/**
- * Reads two strings from input and does nothing with them.
- */
-/*******  f0f54d05-92ff-4160-b498-fdd7b789220f  *******/
+
 void solve() {
     string s1; cin >> s1;
     string s2; cin >>s2
