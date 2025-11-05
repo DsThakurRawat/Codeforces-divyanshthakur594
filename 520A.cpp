63263a6378dd7bfc2dbd9f4da33099a2993f8270
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-11-05 21:52:00 +0530

    Auto-sync: 2025-11-05 21:52:00

--- 520A.cpp
+++ 520A.cpp
@@ -6,20 +6,7 @@
 using namespace std;
 
 void solve() {
-    int n; cin >> n;
-    string s; cin >> s;
-
-    vector<int>words;
-    int flag = 0;
-
-    for(char c = 'a'; c <= 'z'; c++){
-
-        if(s.find(c) == string::npos){
-           flag = 1;
-           
-          break;
-        }
-
+   
 
         
 
