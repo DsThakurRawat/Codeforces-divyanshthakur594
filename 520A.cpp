ed2fdf44e1494f8f7480f6e7502771f20fc83d94
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-11-05 21:43:57 +0530

    Auto-sync: 2025-11-05 21:43:57

--- 520A.cpp
+++ 520A.cpp
@@ -11,13 +11,11 @@ void solve() {
     vector<int>words;
 
     for(char c = 'a'; c <= 'z'; c++){
-        words.push_back(c);
+        
 
       
 
     }
-
-    for(int i = 0; i < s.size(); i++)
 
 
   
