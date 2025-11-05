ef78e110bdf68416f07967abb0ce08d823408d2c
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-11-05 21:47:36 +0530

    Auto-sync: 2025-11-05 21:47:36

--- 520A.cpp
+++ 520A.cpp
@@ -12,7 +12,7 @@ void solve() {
 
     for(char c = 'a'; c <= 'z'; c++){
 
-        if(s.find(c) == string){
+        if(s.find(c) == string::){
             cout << "NO" << endl;
             return ;
         }
