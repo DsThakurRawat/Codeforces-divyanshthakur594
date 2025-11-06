8c7488f3a966ecdfa414b55666f895e8176a5323
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-11-06 08:14:42 +0530

    Auto-sync: 2025-11-06 08:14:42

--- 520A.cpp
+++ 520A.cpp
@@ -1,41 +1,45 @@
 
 // simple template-test2
 // code by divyansh rawat
-
+ 
 #include <bits/stdc++.h>
 using namespace std;
-
+ 
 void solve() {
-   string s;
-    getline(cin, s);
-    set<char> letters;
-
-    for (char c : s) {
-        if (isalpha(c)) {
-            letters.insert(tolower(c));
+    int n; cin >> n;
+    string s; cin >> s;
+ 
+    vector<int>words;
+    int flag = 0;
+ 
+    for(char c = 'a'; c <= 'z'; c++){
+ 
+        if(s.find(c) == string::npos){
+           flag = 1;
+           
+          break;
         }
-    }
-
-    cout << (letters.size() == 26 ? "YES" : "NO") << endl;
-
+ 
+ 
         
-
+ 
       
-
+ 
     }
-
-
-
+ 
+     cout << (flag ? "NO" : "YES") << endl;
+ 
+ 
    
-
-
-
+ 
+ 
+ 
   
-
-
+ 
+}
   
-
-
+ 
+ 
 int main() {
    // int t ;
    // cin >> t;
