0a16d7abe3166161b03ec3525ebbdf5a47a13439
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-11-14 18:01:30 +0530

    Auto-sync: 2025-11-14 18:01:30

---
+++ 1807A.cpp
@@ -0,0 +1,29 @@
+
+// simple template-test2
+// code by divyansh rawat
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+
+    // a + b = c;
+    // a- b = c;
+    int a,b,c;cin>>a>>b>>c;
+    if(c == a+b) cout << "+"<<endl;
+    else cout <<"-"<<endl;
+
+  
+
+}
+  
+
+
+int main() {
+    int t ;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
