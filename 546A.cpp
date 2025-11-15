525d37a5f84aefc98c061d2722dd9d91a13fc66b
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-11-15 19:44:31 +0530

    Auto-sync: 2025-11-15 19:44:31

---
+++ 546A.cpp
@@ -0,0 +1,33 @@
+
+// simple template-test2
+// code by divyansh rawat
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    int a,b,c;cin>>a>>b>>c;
+    int sum = 0;
+
+    for(int i = 1; i<=4; i++){
+        sum += a*i;
+    }
+    cout << sum <<endl;
+
+
+
+
+  
+
+}
+  
+
+
+int main() {
+   // int t ;
+    //cin >> t;
+   // while (t--) {
+        solve();
+  //  }
+    return 0;
+}
