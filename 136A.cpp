4c24513564070753d60eeb836b9e6837d9de2156
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-11-15 15:10:35 +0530

    Auto-sync: 2025-11-15 15:10:35

--- 136A.cpp
+++ 136A.cpp
@@ -8,7 +8,7 @@ using namespace std;
 
 void solve() {
     int n;cin>>n;
-    vector<int>nums;
+    vector<int>nums(n);
     for(int i =0; i < n; i++) cin >> nums[i];
 
     for(int i = 0; i< n; i++) cout << nums[i] << " ";
