448b82cc3e956c1ef69a03ec4b054f43b82ae5e0
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-11-15 10:34:49 +0530

    Auto-sync: 2025-11-15 10:34:49

--- 1560A.cpp
+++ 1560A.cpp
@@ -10,12 +10,16 @@ void solve() {
     vector<int>nums;
 
     for(int i = 1; i < 1e4; i++){
-      if(i % 3 != 0 || i % 2 == 1) nums.push_back(i);
-     
+     if(i % 2 == 1){
+     if( i % 3 != 0) 
+       nums.push_back(i);
+      }
+      else if( i % 3 != 0){
+        if(i % 2 ==1) nums.push_back(i);
+      }
+    
     }
-    for(int i = 0; i <10; i++ ){
-        cout << nums[i] << endl;
-    }
+    for(int i = 0; i < 10; i++) cout << nums[i]<<endl;
 
     
 
