13f2753af623c202648204d4f872f51b165b4155
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-11-15 11:00:25 +0530

    Auto-sync: 2025-11-15 11:00:25

---
+++ 1560B.cpp
@@ -0,0 +1,38 @@
+
+// simple template-test2
+// code by divyansh rawat
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    int a,b,c;cin>>a>>b>>c;
+    int n = 2*abs(a-b);
+
+    if( 1<= a <=n && 1 <= b <= n &&  1 <= c <= n) {
+        cout << -1 << endl;
+        return;
+
+    }
+    cout << c + n/2<<endl;;
+    
+
+
+
+    
+
+
+  
+
+}
+  
+
+
+int main() {
+    int t ;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
