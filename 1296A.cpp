a80e956402d73727d3c04a2ee5204e1a9c3b95c4
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-11-15 16:52:01 +0530

    Auto-sync: 2025-11-15 16:52:01

--- 1296A.cpp
+++ 1296A.cpp
@@ -10,9 +10,31 @@ void solve() {
     vector<int>nums(n);for(int i = 0; i< n; i++) cin >> nums[i];
     long long sum =0;
     for(int i = 0; i < n; i++) sum+=nums[i];
-    if(sum % 2 != 0) cout <<"YES"<<endl;
-    else cout <<"NO"<<endl;
+    if(sum % 2 != 0){ 
+    cout << "YES"<<endl;
+    return;
+    };
+    int count1 = 0,count2 = 0;
 
+    for(int i = 0; i < n; i++){
+        if(nums[i] % 2 == 0) count1++;
+        else count2++;
+
+    }
+    if(count1 >=1 && count2 >= 1 ){ 
+
+    cout <<"YES"<<endl;
+    return;
+
+    }
+    cout << "NO"<<endl;
+
+
+
+
+
+
+   
 
   
 
