caac790f692b7c41717cda7aec90a27b92d9a88f
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-11-15 10:25:25 +0530

    Auto-sync: 2025-11-15 10:25:25

---
+++ 1560A.cpp
@@ -0,0 +1,33 @@
+
+// simple template-test2
+// code by divyansh rawat
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    int k;cin>>k;
+    vector<int>nums;
+
+    for(int i = 0; i < 1e4; i++){
+      if(i % 3 != 0 && i % 2 != 1) nums.push_back(i);
+    }
+
+    cout << nums[k] << endl;
+
+
+
+  
+
+}
+  
+
+
+int main() {
+    int t ;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
