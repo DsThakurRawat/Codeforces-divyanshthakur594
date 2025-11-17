35c74844ccd2252bfa7617500a19a1b38e9b699f
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-11-17 09:25:24 +0530

    Auto-sync: 2025-11-17 09:25:24

---
+++ 2166A.cpp
@@ -0,0 +1,52 @@
+
+// simple template-test2
+// code by divyansh rawat
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    int n;cin>>n;
+    string s;
+    cin >> s;
+  
+
+  int count = 0;
+ 
+
+  for(int i=0;  i<s.size(); i++){
+    if(s[i]!=s.back())count++;
+  }
+  cout << count << endl;
+  
+
+
+
+  
+
+
+
+
+
+
+   
+
+
+    
+
+
+
+  
+
+}
+  
+
+
+int main() {
+    int t ;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
