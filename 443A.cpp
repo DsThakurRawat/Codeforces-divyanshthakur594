1aff0781a5476714091619272def2bb73a85c574
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-11-17 08:48:37 +0530

    Auto-sync: 2025-11-17 08:48:37

---
+++ 443A.cpp
@@ -0,0 +1,24 @@
+
+// simple template-test2
+// code by divyansh rawat
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+  string s;getline(cin,s);
+ cout << s <<endl;
+
+
+}
+  
+
+
+int main() {
+    int t ;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
