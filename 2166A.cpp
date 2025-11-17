80019007b18e1c8d29de503b79cc0f8b7051fc9f
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-11-17 09:25:46 +0530

    Auto-sync: 2025-11-17 09:25:46

--- 2166A.cpp
+++ 2166A.cpp
@@ -9,9 +9,7 @@ void solve() {
     int n;cin>>n;
     string s;cin >> s;
     int count = 0;
- 
-
-  for(int i=0;  i<s.size(); i++){
+    for(int i=0;  i<s.size(); i++){
     if(s[i]!=s.back())count++;
   }
   cout << count << endl;
