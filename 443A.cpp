01be7937f1fe983c31a87d0087ad4b811da5bb74
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-11-17 08:50:02 +0530

    Auto-sync: 2025-11-17 08:50:02

--- 443A.cpp
+++ 443A.cpp
@@ -15,10 +15,10 @@ void solve() {
 
 
 int main() {
-    int t ;
-    cin >> t;
-    while (t--) {
+   // int t ;
+   // cin >> t;
+   // while (t--) {
         solve();
-    }
+   // }
     return 0;
 }
