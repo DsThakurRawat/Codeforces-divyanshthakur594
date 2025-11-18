ffcbed75d479be0d3577e12be20a05d10b8805af
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-11-18 22:23:06 +0530

    Auto-sync: 2025-11-18 22:23:06

--- 1399A.cpp
+++ 1399A.cpp
@@ -13,9 +13,13 @@ void solve() {
   if(v.size() == 1) cout << "YES"<<endl;
 
   for(int i=0;i<n-1;i++){
-    if(abs(v[i]-v[i+1]) >=1) cout <<"NO"<<endl;
+    if(abs(v[i]-v[i+1]) >=1){ 
+    cout <<"NO"<<endl;
+    return;
+    }
   }
-  cout << "YES"<<endl;
+  cout<<"YES"<<endl;
+  
 
 
 
