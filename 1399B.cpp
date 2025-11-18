af921c3e855cda43a346fa3fa43d99fbdb400484
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-11-18 23:38:15 +0530

    Auto-sync: 2025-11-18 23:38:15

--- 1399B.cpp
+++ 1399B.cpp
@@ -10,6 +10,22 @@ void solve() {
     int n;cin>>n;
     vector<long long >a(n);for(long long  i=0; i<n; i++)cin>>a[i];
     vector<long long >b(n);for(long long  i=0; i<n; i++)cin>>b[i];
+    long long min_a = *min_element(a.begin(),a.end());
+    long long min_b = *min_element(b.begin(),b.end());
+
+    long long maxi_count = 0;
+
+    for(int i = 0; i < n; i++){
+        maxi_count += max(a[i]-min_a,b[i]-min_b);
+    }
+    cout << maxi_count<<endl;
+
+   
+
+
+
+
+
 
 
 
