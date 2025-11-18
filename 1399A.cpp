71578b9fdb2b3c35a74a786aa78e37739dbdb645
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-11-18 22:24:27 +0530

    Auto-sync: 2025-11-18 22:24:27

--- 1399A.cpp
+++ 1399A.cpp
@@ -10,7 +10,10 @@ void solve() {
   vector<int>v(n);for(int i = 0; i < n; i++)cin>>v[i];
   //abs(a-b)<=1 and choose c = min(a,b);
 
-  if(v.size() == 1) cout << "YES"<<endl;
+  if(v.size() == 1){
+   cout << "YES"<<endl;
+  return;
+}
 
   for(int i=0;i<n-1;i++){
     if(abs(v[i]-v[i+1]) >1){ 
