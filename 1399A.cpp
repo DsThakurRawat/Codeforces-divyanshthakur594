0e57e5e83f877e2fed8298293039045519a2e836
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-11-18 22:22:02 +0530

    Auto-sync: 2025-11-18 22:22:02

---
+++ 1399A.cpp
@@ -0,0 +1,35 @@
+
+// simple template-test2
+// code by divyansh rawat
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+  int n;cin>>n;
+  vector<int>v(n);for(int i = 0; i < n; i++)cin>>v[i];
+  //abs(a-b)<=1 and choose c = min(a,b);
+
+  if(v.size() == 1) cout << "YES"<<endl;
+
+  for(int i=0;i<n-1;i++){
+    if(abs(v[i]-v[i+1]) >=1) cout <<"NO"<<endl;
+  }
+  cout << "YES"<<endl;
+
+
+
+
+
+}
+  
+
+
+int main() {
+    int t ;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
