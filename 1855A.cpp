32c93ab6906f4cb6d88a8358e6740ce136c39f75
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-11-19 21:02:38 +0530

    Auto-sync: 2025-11-19 21:02:38

--- 1855A.cpp
+++ 1855A.cpp
@@ -8,7 +8,7 @@ using namespace std;
 void solve() {
     int a,b,c;cin>>a>>b>>c;
 
-    if(a+b > 10 || a+c > 10 || b+c > 10){
+    if(a+b >= 10 || a+c >= 10 || b+c >= 10){
         cout << "YES"<<endl;
         
     }
