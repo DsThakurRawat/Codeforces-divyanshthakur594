abe656777be59e98d770b24eaede04097b5c4866
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-11-19 21:53:32 +0530

    Auto-sync: 2025-11-19 21:53:32

---
+++ 1850D.cpp
@@ -0,0 +1,24 @@
+
+// simple template-test2
+// code by divyansh rawat
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+  int n,k;cin>>n>>k;
+  vector<int>v(n);for(int i= 0;i<n; i++)cin>>v[i];
+
+
+}
+  
+
+
+int main() {
+    int t ;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
