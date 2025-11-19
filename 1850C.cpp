be4f208200008c0a3d72d20fab70f7870e03c854
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-11-19 21:20:40 +0530

    Auto-sync: 2025-11-19 21:20:40

---
+++ 1850C.cpp
@@ -0,0 +1,28 @@
+
+// simple template-test2
+// code by divyansh rawat
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    int n;cin>>n;
+    vector<string>grid(n);
+
+    for(int i = 0; i < n; i++){
+        cin >> grid[i];
+    }
+  
+
+}
+  
+
+
+int main() {
+    int t ;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
