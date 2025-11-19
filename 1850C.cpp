f797f6269068ed816334cfa393bbe3b24b02dbc4
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-11-19 21:24:17 +0530

    Auto-sync: 2025-11-19 21:24:17

--- 1850C.cpp
+++ 1850C.cpp
@@ -8,10 +8,20 @@ using namespace std;
 void solve() {
     int n;cin>>n;
     vector<string>grid(n);
+    string s;
 
     for(int i = 0; i < n; i++){
         cin >> grid[i];
     }
+    for(int i = 0; i < n; i++){
+        string t = grid[i];
+        for(int j = 0; j < t.size(); j++){
+            if(t[i] != '.') s+=t[i];
+
+        }
+    }
+    cout << s << endl;
+
   
 
 }
