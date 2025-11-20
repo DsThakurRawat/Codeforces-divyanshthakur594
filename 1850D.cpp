551ede21abec5ba5f77beb12a5d28cea60d3ac39
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-11-20 08:59:03 +0530

    Auto-sync: 2025-11-20 08:59:03

--- 1850D.cpp
+++ 1850D.cpp
@@ -9,7 +9,7 @@ void solve() {
   long long  n,k;cin>>n>>k;//less or equal than k
   vector<long long >v(n);for(long long  i= 0;i<n; i++)cin>>v[i];
   sort(v.begin(),v.end());
- long long l =0,ans = 0;
+ long long l =0,ans = 1;
 
  for(long long r = 1; r < n; r++){
   if(v[r] - v[r-1] > k){
