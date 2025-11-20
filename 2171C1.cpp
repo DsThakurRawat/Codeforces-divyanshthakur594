32bc97036afd7a57ae252d5464918bc04b9669a5
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-11-20 21:51:40 +0530

    Auto-sync: 2025-11-20 21:51:40

---
+++ 2171C1.cpp
@@ -0,0 +1,44 @@
+
+// simple template-test2
+// code by divyansh rawat
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    int n;cin>>n;
+    vector<int>a(n);for(int i = 0; i < n; i++)cin>>a[i];
+    vector<int>b(n);for(int i = 0; i < n; i++)cin>>b[i];
+    vector<int>aji_a(n),mai(n);
+    
+         for(int i = 1; i < n; i++) {
+        swap(a[i], b[i]);
+    }
+    int xor_a = a[0];
+    int xor_b = b[0];
+    for(int i = 1; i < n; i++){
+        xor_a=xor_a^a[i];
+        xor_b=xor_b^b[i];
+    }
+
+
+
+
+    
+
+  
+
+}
+  
+
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t ;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
