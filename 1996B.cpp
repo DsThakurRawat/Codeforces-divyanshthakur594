7b357c6548b7faeef2aa8b9119b5bf221bb321aa
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-11-21 18:13:34 +0530

    Auto-sync: 2025-11-21 18:13:34

--- 1996B.cpp
+++ 1996B.cpp
@@ -7,6 +7,24 @@ using namespace std;
 
 void solve() {
     int n,k;cin>>n>>k;
+    vector<vector<int>> grid(n, vector<int>(n));
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            char c;
+            cin >> c;             
+            grid[i][j] = c - '0'; 
+        }
+    }
+    for(int i = 0; i < n; i+=k){
+        for(int j = 0; j < n; j+=k){
+            cout << grid[i][j];
+        }
+        cout <<endl;
+    }
+
+
+
+
 
     
 
