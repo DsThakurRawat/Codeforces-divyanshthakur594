d0eeaf79ab537726e262cd352ff12146e9636a84
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-11-22 14:10:31 +0530

    Auto-sync: 2025-11-22 14:10:31

--- 1837B.cpp
+++ 1837B.cpp
@@ -12,14 +12,14 @@ void solve() {
     // score is len+1;
 
     int max_len = 0;
-    int len = 0;
+    int len = 1;
 
     for(int i=0; i < s.size()-1;i++){
         if(s[i] == s[i+1]){
             len++;
         }
       else{
-        len = 0;
+        len = 1;
       }
       max_len = max(len,max_len);
 
