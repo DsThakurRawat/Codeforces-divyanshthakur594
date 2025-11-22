72e347682b1cac4af9626771c249ff0bc41ca17b
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-11-22 10:01:45 +0530

    Auto-sync: 2025-11-22 10:01:45

---
+++ 2148B.cpp
@@ -0,0 +1,32 @@
+
+// simple template-test2
+// code by divyansh rawat
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+  long long  n,m,x,y;cin>>n>>m>>x>>y;
+
+  vector<long long>a(n);for(auto &in:a)cin>>in;
+  vector<long long>b(m);for(auto &in:b)cin>>in;
+  
+
+
+
+
+}
+  
+
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t ;
+    t=1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
