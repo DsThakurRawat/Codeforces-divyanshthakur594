685884a78392e4b34a2cc2a52b57f701a776b846
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-11-22 14:05:07 +0530

    Auto-sync: 2025-11-22 14:05:07

---
+++ 1837B.cpp
@@ -0,0 +1,45 @@
+
+// simple template-test2
+// code by divyansh rawat
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    int n;cin>>n;
+    string s;cin>>s;
+
+    // score is len+1;
+
+    int max_len = 0;
+    int len = 0;
+
+    for(int i=0; i < s.size()-1;i++){
+        if(s[i] == s[i+1]){
+            len++;
+        }
+        max_len = max(len,max_len);
+
+    }
+
+
+
+    
+    
+  
+
+}
+  
+
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t ;
+    t=1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
