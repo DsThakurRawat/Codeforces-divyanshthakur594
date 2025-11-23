b8efdf4f64cac7f6623f994f4dac013500ac52ad
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-11-23 13:35:29 +0530

    Auto-sync: 2025-11-23 13:35:29

--- 1828B.cpp
+++ 1828B.cpp
@@ -7,39 +7,11 @@ using namespace std;
 
 void solve() {
 
-    vector<int>nums;
-    for(auto &in:nums){
-        cin >>in;
-        nums.push_back(in);
-
-    }
-
-
-
-        unordered_map<long long, unordered_map<int, int>> mp;
-
-        long long px = 0; // prefix XOR
-        int diff = 0;      // evens - odds
-        int ans = 0;
-
-        // initial state for index = -1
-        mp[0][0] = -1;
-
-        for(int i = 0; i < nums.size(); i++){
-            px ^= nums[i];
-
-            if(nums[i] % 2 == 0) diff++;
-            else diff--;
-
-            if(mp[px].count(diff)){
-                ans = max(ans, i - mp[px][diff]);
-            } else {
-                mp[px][diff] = i;
-            }
-        }
-
-        cout << ans;
+  int n;cin>>n;
+  vector<int>p(n);for(auto &in:p)cin>>in;
+  vector<int>v(n);
   
+
 
 }
   
