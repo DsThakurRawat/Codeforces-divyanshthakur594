9cb218f0034fe360cdd71f340f9944cb4b96fe70
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-11-24 23:55:20 +0530

    Auto-sync: 2025-11-24 23:55:20

---
+++ 1696A.cpp
@@ -0,0 +1,33 @@
+
+// simple template-test2
+// code by divyansh rawat
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+  	int n, z;
+	cin >> n >> z;
+	vector<int> a(n);
+	int mx;
+	for(int i = 0; i < n; i++){
+		cin >> a[i];
+		a[i] |= z;
+	}
+	cout << *max_element(a.begin(), a.end()) << endl;
+	return;
+}
+  
+
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t ;
+    t=1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
