2c88ab6d262f3c5f21727d6ce7508891f6920d5d
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-11-24 23:13:32 +0530

    Auto-sync: 2025-11-24 23:13:32

--- 1794B.cpp
+++ 1794B.cpp
@@ -11,19 +11,12 @@ void solve() {
     vector<int>v(n);for(auto &in:v)cin>>in;
     vector<int>res;
 
-    for(int i = 0; i < n;i++){
-        if(v[i] == 1)v[i]=v[i]+1;
-    }
     for(int i = 1;i<n;i++){
 
-    
+    if(v[i]%v[i-1] == 0){
+        v[i-1]=v[i-1]+1;
+    }
 
-        if(v[i-1]%2 == 0 && v[i]%2 == 0){
-            v[i] = v[i]+1;
-        }
-        else if(v[i-1]%2==1 && v[i]%2==1){
-            v[i] = v[i]+1;
-        }
 
 
 
