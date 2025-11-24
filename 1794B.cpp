270254abeb24e5c4015d2da59f0acb941c525f79
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-11-24 23:05:15 +0530

    Auto-sync: 2025-11-24 23:05:15

---
+++ 1794B.cpp
@@ -0,0 +1,41 @@
+
+// simple template-test2
+// code by divyansh rawat
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+
+    int n;cin>>n;
+    vector<int>v(n);for(auto &in:v)cin>>in;
+    vector<int>res;
+    for(int i = 1;i<n;i++){
+        if(v[i-1]%2 == 0 && v[i]%2 == 0){
+            v[i] = v[i]+1;
+        }
+        else if(v[i-1]%2==1 && v[i]%2==1){
+            v[i] = v[i]+1;
+        }
+    }
+    for(auto &ot:v) cout << ot ;
+    cout <<endl;
+
+
+  
+
+}
+  
+
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t ;
+    t=1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
