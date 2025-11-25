f15c50411fe19cd7e49f5a85a9bb4d16db45f16a
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-11-25 11:32:23 +0530

    Auto-sync: 2025-11-25 11:32:23

---
+++ 1665A.cpp
@@ -0,0 +1,29 @@
+
+// simple template-test2
+// code by divyansh rawat
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+        int n;cin>>n;
+        vector<long long >v(n);for(auto &in:v)cin>>in;
+      
+  
+   
+    
+}
+  
+
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t ;
+    t=1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
