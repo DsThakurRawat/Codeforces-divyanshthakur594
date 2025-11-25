53441cafd0ef6a82512f066ceb4268f39e1e686c
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-11-25 15:17:15 +0530

    Auto-sync: 2025-11-25 15:17:15

--- 1679A.cpp
+++ 1679A.cpp
@@ -17,7 +17,7 @@ void solve() {
     cout << -1<<endl;
     return;
  }
- cout << (ceil((double)n/6)) << " "<< floor(n/4)<<endl;
+ cout << (ceil((double)n/6)) << " "<< ceil ((double)n/4)<<endl;
 
   
 
