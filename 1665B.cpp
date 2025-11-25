4cac41b9fcedc242abe5360428682571a6054539
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-11-25 11:28:42 +0530

    Auto-sync: 2025-11-25 11:28:42

--- 1665B.cpp
+++ 1665B.cpp
@@ -6,8 +6,38 @@
 using namespace std;
 
 void solve() {
-  
+        int n;cin>>n;
+        vector<long long >v(n);for(auto &in:v)cin>>in;
+        map<int,int>mp;
+        for(int i = 0;i<n;i++){
+            mp[v[i]]++;
+        }
+    // first the key your element ;
+    //second the
+    int max_freq =0;
+    for(auto &p:mp){
+        max_freq = max(max_freq,p.second);
+    }
+    int op = 0;
 
+    while(max_freq<n){
+        op++;
+        if(2*max_freq<=n){
+            op+=max_freq;
+            max_freq *=max_freq;
+        }
+        else{
+            op+=(n-max_freq);
+            max_freq = n;
+        }
+
+    }
+    cout<<op<<endl;
+
+
+
+  
+   
     
 }
   
