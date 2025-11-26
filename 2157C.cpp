6b410afd57e5cf15ad8a03d8b8dc57626dc710c6
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-11-26 09:31:38 +0530

    Auto-sync: 2025-11-26 09:31:38

--- 2157C.cpp
+++ 2157C.cpp
@@ -6,6 +6,62 @@
 using namespace std;
 
 void solve() {
+    int n,k,q;cin>>n>>k>>q;
+    vector<long long >ans(n,k+5);
+    vector<array<int,3>>type2;
+    vector<array<int,3>>type1;
+
+
+    while(q--){
+
+        int c,l,r;cin>>c>>l>>r;
+        l--;r--;
+        if(c==1){
+            type1.push_back({c,l,r});
+        }
+        else{
+            type2.push_back({c,l,r});
+            }
+
+        }
+
+        for(auto &t :type1){
+            int l = t[1];
+            ans[l] = k;
+        }
+
+        for(auto &t: type2){
+            int l = t[1];
+            int r = t[2];
+
+            for(int x =0;x<k;x++){
+                if(l+x<=r)ans[l+x]=x;
+            }
+            for(int i =l;i<=r;i++){
+                if(ans[i]==k)ans[i]==k+5;
+            }
+
+
+        }
+        for(auto &ot:ans){
+            cout << ot <<" ";
+        }
+        cout << endl;
+
+        
+
+        
+
+
+
+
+
+
+
+
+
+
+    }
   
 
     
