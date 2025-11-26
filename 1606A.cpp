f2099be5cfd575c85117da1cb0cc013e2b31a7de
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-11-26 12:45:39 +0530

    Auto-sync: 2025-11-26 12:45:39

---
+++ 1606A.cpp
@@ -0,0 +1,37 @@
+
+// simple template-test2
+// code by divyansh rawat
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    int n;cin>>n;
+    string s;cin>>s;
+    string s = "";
+    if(s[0]!=s[n-1]){
+        if(s[0]!='a')s[0]='b';
+        else s[0]='a';
+    }
+     cout << s <<endl;
+
+
+
+  
+
+    
+}
+  
+
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t ;
+    t=1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
