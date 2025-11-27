d23d81a6f9ed146c7f4d2c7f70bd326c3c41e854
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-11-27 09:45:41 +0530

    Auto-sync: 2025-11-27 09:45:41

--- 1593B.cpp
+++ 1593B.cpp
@@ -12,8 +12,9 @@ void solve() {
    string ans ="";
 
    for(long long  i = 0;i<s.size();i++){
+     no += s[i];
     for(long long j = i; j<s.size();j++){
-        no += s[j];
+       
         if(stoll(no)%25 == 0){
            ans=no;
         }
