cc6161ba3f45641149e38b8038ea02086eda3b7a
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-11-27 11:51:20 +0530

    Auto-sync: 2025-11-27 11:51:20

--- 1582B.cpp
+++ 1582B.cpp
@@ -8,33 +8,16 @@ using namespace std;
 void solve() {
  int n;cin>>n;                                        // a number is divisble by 25 if last two digits are one of 00,25,50,75
  vector<int>v(n);for(auto & in : v)cin>>in;
- long long sum = accumulate(v.begin(),v.end(),0);
- long res = sum-1;
+ long long x;
+ long long cnt = 0;
+ for(int i = 0;i<n;i++){
+    cin>>x;
+    if(x==1)cnt++;
 
- vector<vector<int>>subs;
- subs.push_back({});
- int count = 0;
+ }
+ cout << cnt<<endl;
 
-   for(int i = 0;i<n;i++){
-    int sz = subs.size();
-   
-    for(int  j = 0;j<sz;j++){
-       vector<int>p = subs[j];
-       p.push_back(v[i]);
-       int summ = accumulate(p.begin(),p.end(),0);
-       if(summ = res)count++;
-       subs.push_back(p);
-
-    
-    }
-
-   }
-   cout << count << endl;
-
-
-
-    
-
+ 
 
 
    
