fc715c844c6bda291ba4187c493ab6366268127b
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-11-28 17:27:06 +0530

    Auto-sync: 2025-11-28 17:27:06

--- 1440B.cpp
+++ 1440B.cpp
@@ -8,7 +8,7 @@ using namespace std;
 void solve() {
  
     int n,k;cin>>n>>k;
-    vector<int>v(n*k);for(int j = 0;j<=k*n;j++)cin>>v[j];
+    vector<int>v(n*k);for(int j = 0;j<k*n;j++)cin>>v[j];
 
     long long i=0;
     long long sum =0;
