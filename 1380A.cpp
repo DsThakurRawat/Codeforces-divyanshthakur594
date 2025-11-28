6ae8ef3af4d5da43dd0619bdeda3a8853d11897c
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-11-28 10:13:31 +0530

    Auto-sync: 2025-11-28 10:13:31

--- 1380A.cpp
+++ 1380A.cpp
@@ -12,7 +12,7 @@ void solve() {
     int flag = 0;
 
    
-    for(int i =0;i<n;i++){
+    for(int i =1;i<n;i++){
         for(int j = i+1;j<n;j++){
             
             for(int k = j+1;k<n;k++){
