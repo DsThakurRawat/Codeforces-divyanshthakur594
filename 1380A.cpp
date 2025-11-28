5bab0729a4689d7a6b94256393d008c833929327
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-11-28 10:31:40 +0530

    Auto-sync: 2025-11-28 10:31:40

--- 1380A.cpp
+++ 1380A.cpp
@@ -13,32 +13,60 @@ for (int i = 1; i <= n; i++) {
     cin >> v[i];
 }
 
-    vector<int>ans;
-    int flag = 0;
-
-   
-    for(int i =1;i<=n;i++){
-        for(int j = i+1;j<=n;j++){
-            
-            for(int k = j+1;k<=n;k++){
-                if(v[i]<v[j]&&v[j]>v[k]){
-                    flag = 1;
-                    ans.push_back(i);
-                    ans.push_back(j);
-                    ans.push_back(k);
-                    cout << "YES"<<endl;
-                   cout << ans[0]<< " "<<ans[1]<<" "<<ans[2]<<endl;
-                
-                    return;
-                } 
-            }
-            if(flag==1)break;
-        }
-         if(flag==1)break;
+   /*
+   you're checking triplets i < j < k such that:
+   v[i]<v[j]andv[j]>v[k]
+   This means v[j] is a peak, and you want one smaller 
+   element before and one smaller element after.
+   This is basically checking whether an index j exists such that:
+   there is some i < j with v[i] < v[j]
+   there is some k > j with v[k] < v[j]
 
 
+
+        O(n) Optimized Approach
+        Step 1: Precompute prefix-min indices
+
+        leftMin[j] = index of smallest element from 1…j
+
+        Step 2: Precompute suffix-min indices
+
+        rightMin[j] = index of smallest element from j…n
+
+        if v[leftMin[j-1]] < v[j] and v[rightMin[j+1]] < v[j]
+    => we found our i, j, k
+
+
+
+
+   */
+
+   vector<int>prefixmin(n+1),suffixmin(n+1);
+   prefixmin[1]=1;
+   for(int i = 2;i<=n;i++){
+    if(v[i]< v[prefixmin[i-1]]) prefixmin[i] = i;
+    else prefixmin[i] = prefixmin[i-1];
+   }
+   suffixmin[n]=n;
+   for(int i = n-1;i>=1;i--){
+    if(v[i]<v[suffixmin[i+1]])suffixmin[i]=i;
+    else suffixmin[i]=suffixmin[i+1];
+   }
+
+   for(int i = 2;i<=n-1;i++){
+    int j = prefixmin[j-1];
+    int k = suffixmin[j+1];
+
+    if(v[j]<v[i] && v[k]<v[i]){
+        cout << "YES"<<endl;
+        cout<<i<<" "<<j<<" "<<k<<endl;
+        return;
+    
     }
-    cout << "NO"<<endl;
+   }
+   cout << "NO"<<endl;
+   
+
     
 
    
