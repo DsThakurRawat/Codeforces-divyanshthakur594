4169597b7761c0d2c03ed63f62f7bd1529150d4b
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-11-29 11:28:19 +0530

    Auto-sync: 2025-11-29 11:28:19

--- 2170B.cpp
+++ 2170B.cpp
@@ -8,6 +8,17 @@ using namespace std;
 void solve() {
     int n;cin>>n;
     vector<int>v(n);for(auto &in : v)cin>>in;
+    long long cnt = 0;            
+    long long sum = 0;            
+    for (long long x : v) {
+        if (x > 0) cnt++;
+        sum += x;
+    }
+
+   
+    long long ans = min(cnt, sum - (n - 1));
+
+    cout << ans << endl;;
 
   
 
