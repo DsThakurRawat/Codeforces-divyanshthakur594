6e535e959db09e786ab3ccee8e12a134f29347ee
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-11-29 11:16:31 +0530

    Auto-sync: 2025-11-29 11:16:31

--- 2170B.cpp
+++ 2170B.cpp
@@ -9,48 +9,6 @@ void solve() {
     int n;cin>>n;
     vector<int>v(n);for(auto &in : v)cin>>in;
 
-    int count0=0;
-    int count_element = 0;
-    set<int>st(v.begin(),v.end());
-    int flag = 1;
-    
-    
-    
-      if(st.size()==1){// all elemnts are same
-        cout << 1 << endl;
-        return;
-      }
-    
-    for(int i =0;i<n;i++){
-        if(v[i]==0){
-            count0++;
-        }
-        else{
-            count_element++;
-       
-        }
-    }
-    if(count0 ==0){// count of zro is 0
-        cout << n<<endl;
-        return;
-    }
-    else{
-        cout << count_element <<endl;
-        return;
-    }
-
-
-  
-
- 
-
-
-
-
-
-
-
-
   
 
 }
