2652cecf32d0f2571f1a053189fcf69d1a5b2942
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-12-01 00:01:37 +0530

    Auto-sync: 2025-12-01 00:01:37

---
+++ 379A.cpp
@@ -0,0 +1,49 @@
+
+// simple template-test2
+// code by divyansh rawat
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+        int a,b;cin>>a>>b;
+        // so like he can make new candles from b available candles
+       int sum =0;
+        while(a>=b){
+            sum +=a;
+            a = a/b;
+
+        }
+        cout << sum <<endl;
+
+
+
+
+     
+
+        
+    
+     
+
+      
+      
+      
+      
+  
+   
+    
+}
+  
+
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+   // int t ;
+   // t=1;
+   // cin >> t;
+    //while (t--) {
+        solve();
+    //}
+    return 0;
+}
