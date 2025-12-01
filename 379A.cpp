cc7c11ebb5dd9f27eab86fa97fa6e09df193cbf4
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-12-01 09:12:45 +0530

    Auto-sync: 2025-12-01 09:12:45

--- 379A.cpp
+++ 379A.cpp
@@ -8,14 +8,16 @@ using namespace std;
 void solve() {
         int a,b;cin>>a>>b;
         // so like he can make new candles from b available candles
-       int sum =0;
-        while(a>=b){
-            sum +=a;
-            a = a/b;
-
-        }
-        cout << sum +a<<endl;
-
+       
+        int sum =0;
+        int rem =0;
+       while(a>=b){
+          sum +=a;
+          a = a/b;
+          rem = a % b;
+          a +=rem;
+       }
+       cout << a + sum <<endl;
 
 
 
