8f94f262c85271676cc1407c4e70f1001d1d1dc6
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-12-01 10:42:05 +0530

    Auto-sync: 2025-12-01 10:42:05

--- 1405B.cpp
+++ 1405B.cpp
@@ -8,15 +8,15 @@ using namespace std;
 void solve() {
  
     long long  n;cin>>n;
-    vector<long long>v(n);for(int in : v) cin>>in;
+    vector<long long>v(n);for(long long  &in : v) cin>>in;
     // we have to find maximum suffix sum
-    vector<long long>suffixsum;
+    vector<long long>suffixsum(n);
     suffixsum[n-1] = v[n-1];
 
     for(int i = n-2;i>=0;i--){
         suffixsum[i] = v[i]+suffixsum[i+1];
     }
-    long long max_sum = INT_MIN;
+    long long max_sum = LLONG_MIN;
     for(int i = 0;i<n;i++){
         max_sum = max(max_sum,suffixsum[i]);
     }
