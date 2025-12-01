746fe0d826c1c452afb4f8ecd457130e6afb2f20
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-12-01 10:39:55 +0530

    Auto-sync: 2025-12-01 10:39:55

---
+++ 1405B.cpp
@@ -0,0 +1,59 @@
+
+// simple template-test2
+// code by divyansh rawat
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+ 
+    long long  n;cin>>n;
+    vector<long long>v(n);for(int in : v) cin>>in;
+    // we have to find maximum suffix sum
+    vector<long long>suffixsum;
+    suffixsum[n-1] = v[n-1];
+
+    for(int i = n-2;i>=0;i--){
+        suffixsum[i] = v[i]+suffixsum[i+1];
+    }
+    long long max_sum = INT_MIN;
+    for(int i = 0;i<n;i++){
+        max_sum = max(max_sum,suffixsum[i]);
+    }
+    cout << max_sum <<endl;
+
+
+   
+
+
+
+
+
+
+
+
+
+
+     
+
+
+    
+    
+  
+
+    
+}
+  
+
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t ;
+    t=1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
