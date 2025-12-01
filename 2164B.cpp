1a36de6d4f40a88492bd2d53df9b310a0483ae19
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-12-02 00:02:01 +0530

    Auto-sync: 2025-12-02 00:02:01

---
+++ 2164B.cpp
@@ -0,0 +1,30 @@
+
+// simple template-test2
+// code by divyansh rawat
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+   int n;cin>>n;
+   vector<int>a(n);for(int &in:a)cin>>in;
+   for(int i = 0;i<n;i++){
+    int y = a[i+1]%a[i];
+   }
+   cout << 1117%117
+    
+}
+  
+
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t ;
+    t=1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
