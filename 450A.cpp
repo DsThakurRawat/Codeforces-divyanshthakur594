5c25842644805a5996fee040e7906211f5161581
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-12-01 12:28:03 +0530

    Auto-sync: 2025-12-01 12:28:03

--- 450A.cpp
+++ 450A.cpp
@@ -7,12 +7,12 @@ using namespace std;
 
 void solve() {
   int n,m;cin>>n>>m;
-  vector<int>a(n);for(int i = 0;i<n;i++)cin>>a[i];
+  vector<int>a(n+1);for(int i = 1;i<=n;i++)cin>>a[i];
   int max_elementindex = 0;
 
   int maxi = INT_MIN;
 
-  for(int i = 0;i<n;i++){
+  for(int i = 1;i<n=;i++){
   if(a[i]==max(maxi,a[i])) max_elementindex = i;
   }
   cout << max_elementindex <<endl;
