80c212723175d1b6da1db1a61e8067eb1f6933ab
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-12-02 16:46:32 +0530

    Auto-sync: 2025-12-02 16:46:32

---
+++ 2124B.cpp
@@ -0,0 +1,37 @@
+
+// simple template-test2
+// code by divyansh rawat
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+  int n;cin>>n;
+  vector<int>a(n);for(int &in:a)cin>>in;
+  
+  
+  cout << a[0] + min(a[1],a[0])<< endl;
+
+ 
+ 
+
+  
+
+
+
+    
+}
+  
+
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t ;
+    t=1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
