78efa2911897f2bf6409987017ba96d8823dd21c
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-12-02 09:26:44 +0530

    Auto-sync: 2025-12-02 09:26:44

--- 2147B.cpp
+++ 2147B.cpp
@@ -9,11 +9,11 @@ void solve() {
   int n;cin>>n;
   // in this case there is simple construction where each number is place at x and 2x
   vector<int>v;
-  for(int i = 1;i<n;i++){
+  for(int i = 1;i<=n;i++){
     v.push_back(i);
 
   }
-  for(int i = 1;i<n;i++){
+  for(int i = 1;i<=n;i++){
     v.push_back(i);
 
   }
