03b40f8db7a2b2955ec9b7301f8639c591c80292
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-12-02 10:57:58 +0530

    Auto-sync: 2025-12-02 10:57:58

---
+++ 2154B.cpp
@@ -0,0 +1,48 @@
+
+// simple template-test2
+// code by divyansh rawat
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+  
+    int n;cin>>n;
+    vector<int>a(n);for(int &in : a)cin>>in;
+    int maxi = a[0];
+
+    for(int i = 0;i<n;i++){
+        maxi = max(maxi,a[i]);
+        if( i % 2 == 0){
+            a[i] =  maxi;
+        }
+    }
+    int cost = 0;
+    for(int i = 0;i<n;i++){
+        if( i % 2 == 0 ){
+            if( a[i-1] >= a[i]  ) cost = a[i-1]-a[i];
+            else if(a[i+1] >= a[i]) cost = a[i+1]-a[i];
+        }
+    }
+    cout << cost << endl;
+
+
+
+
+
+    
+}
+  
+
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t ;
+    t=1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
