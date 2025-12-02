f76fecc0f6917d1c78cd3a0e25dd1e309bc00e04
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-12-02 14:15:28 +0530

    Auto-sync: 2025-12-02 14:15:28

--- 2154B.cpp
+++ 2154B.cpp
@@ -1,70 +1,74 @@
-
-// simple template-test2
+// simple template-test-2
 // code by divyansh rawat
 
 #include <bits/stdc++.h>
 using namespace std;
 
 void solve() {
-  
-    int n;cin>>n;
-    vector<int>a(n);for(int &in : a)cin>>in;
-    int maxi = a[0];
 
-    for(int i = 0;i<n;i++){
-        maxi = max(maxi,a[i]);
-        if( i % 2 == 0){
-            a[i] =  maxi;
+    int n; cin >> n;
+
+    vector<int> v(n); for(int &it : v) cin >> it;
+
+    vector<int> maxm(n);
+    maxm[0] = v[0];
+
+    for(int i = 1; i < n; i++) {
+        maxm[i] = max(maxm[i - 1], v[i]);
+    }
+
+    for(int i = 1; i < n; i += 2) {
+        v[i] = maxm[i];
+    }
+
+    int ans = 0;
+
+    if(n == 2) {
+        v[1] = max(v[1], v[0]);
+
+        if(v[1] > v[0]) {
+            cout << 0 << endl;
+        } 
+        else {
+            int res = v[0] - (v[1] - 1);
+            cout << res << endl;
+        }
+        return;
+    }
+
+    if(n >= 3) {
+        for(int i = 1; i + 1 < n; i += 2) {
+
+            int mid = v[i];
+            int left = v[i - 1];
+            int right = v[i + 1];
+
+            if(mid > left && mid > right) continue;
+
+            if(mid <= left) {
+                ans += left - (mid - 1);
+                v[i - 1] = mid - 1;
+            }
+
+            if(mid <= right) {
+                ans += right - (mid - 1);
+                v[i + 1] = mid - 1;
+            }
         }
     }
-   
-    
-    long long cost = 0;
 
-    for(int i = 0; i < n; i++){
-        if(i % 2 == 0){   
-            long long left = (i-1 >= 0 ? a[i-1] : LLONG_MIN);
-            long long right = (i+1 < n ? a[i+1] : LLONG_MIN);
-
-            long long need = max(left, right) - a[i] + 1;
-
-            if(need > 0) cost += need;
-        }
-    }
-
-    cout << cost << endl;
-
-
-   
-   
-        
-          
-          
-          
-
-        
-    }
-    
-
-
-
-
-
-
-
-    
-
-  
-
+    cout << ans << endl;
+}
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    int t ;
-    t=1;
-    cin >> t;
-    while (t--) {
+
+    int t = 1;
+     cin >> t;
+    while(t--) {
         solve();
     }
+
     return 0;
 }
