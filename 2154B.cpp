6425d98b3f035ba578780c48cc4eb5921371f941
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-12-02 11:19:50 +0530

    Auto-sync: 2025-12-02 11:19:50

--- 2154B.cpp
+++ 2154B.cpp
@@ -20,11 +20,13 @@ void solve() {
     int cost = 0;
     for(int i = 0;i<n;i++){
         if( i % 2 == 0 ){
-             if(i-1 >= 0 && a[i-1] >= a[i])
+        if(i-1 >= 0 && a[i-1] >= a[i])
             cost = a[i-1] - a[i] + 1;
 
         if(i+1 < n && a[i+1] >= a[i])
-            cost = a[i+1] - a[i]+1 ;
+            cost = a[i+1] - a[i]+1;
+          if(i == 0 && a[1] >= a[0])cost = a[1]-a[0]+1;
+          if( i == n-1 && a[n-1] <= a[n-2]) cost = a[n-2] - a[n-1] + 1;  
            
         }
     }
