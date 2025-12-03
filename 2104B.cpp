7e3394f045eb0af0e500214dba79247b7e7e5027
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-12-03 10:23:06 +0530

    Auto-sync: 2025-12-03 10:23:06

---
+++ 2104B.cpp
@@ -0,0 +1,48 @@
+
+// simple template-test2
+// code by divyansh rawat
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    int n;cin>>n;
+    vector<int>v(n);for(auto &in : v)cin>>in;
+    vector<int>ans;
+    vector<int>suffixsum(n),prefixmax(n);
+
+    // calculate prefixmax
+    prefixmax[0] = v[0];
+    for(int i = 1;i<n;i++){
+        prefixmax[i] = max(prefixmax[i-1],v[i]);
+    }
+
+    // calculate suffixsum
+    suffixsum[n-1] = v[n-1];
+    for(int i = n-2;i>=0;i--){
+        suffixsum[i] = suffixsum[i+1]+v[i];
+    }
+    for(int i = 0;i<n;i++){
+        ans[i] = suffixsum[i+1]+prefixmax[i];
+    }
+
+    for(int & ot : ans) cout << ot << " ";
+    cout << endl;
+
+
+    
+}
+  
+
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t ;
+    t=1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
