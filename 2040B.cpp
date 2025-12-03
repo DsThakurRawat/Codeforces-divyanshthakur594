98771ac42720e785fc1bf231dc8f052e95f60f3f
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-12-03 19:01:52 +0530

    Auto-sync: 2025-12-03 19:01:52

---
+++ 2040B.cpp
@@ -0,0 +1,31 @@
+
+// simple template-test2
+// code by divyansh rawat
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    int n;cin>>n;
+    vector<int>v(n,0);
+    cout << ans = ceil(log2(n)) << endl;
+
+
+
+
+    
+}
+  
+
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t ;
+    t=1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
