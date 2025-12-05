cfd3ec061a7b45aeecbd2530795d772ce865e8cd
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-12-05 21:44:20 +0530

    Auto-sync: 2025-12-05 21:44:20

---
+++ DSA&CP NOTES/file.py
@@ -0,0 +1,23 @@
+from PIL import Image
+
+# Open images
+img1 = Image.open("image.png")
+img2 = Image.open("image copy.png")
+
+# Make both images the same height (optional but usually nicer)
+h = min(img1.height, img2.height)
+img1 = img1.resize((int(img1.width * h / img1.height), h))
+img2 = img2.resize((int(img2.width * h / img2.height), h))
+
+# Create blank canvas for collage (side by side)
+collage_width = img1.width + img2.width
+collage_height = h
+collage = Image.new("RGB", (collage_width, collage_height), (0, 0, 0))
+
+# Paste images
+collage.paste(img1, (0, 0))
+collage.paste(img2, (img1.width, 0))
+
+# Save result
+collage.save("collage.png")
+print("Saved as collage.png")
