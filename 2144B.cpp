a5df687152e9053aa0f246434f3efa3a3ea621d7
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-12-05 11:21:10 +0530

    Auto-sync: 2025-12-05 11:21:10

--- 2144B.cpp
+++ 2144B.cpp
@@ -6,7 +6,60 @@
 using namespace std;
 
 void solve() {
+
+  /*
+ Recall that a permutation of length n
+ is a sequence of length n
+ such that each integer from 1
+ to n
+ appears in it exactly once.
+ Let's define the cost of a permutation as the minimum 
+ length of its contiguous subsegment (possibly empty) 
+ that needs to be sorted so that the entire permutation
+  becomes sorted in ascending order.*/
+
+  int n;cin>>n;
+  vector<int>p(n);for(int &in:p)cin>>in;
+  vector<int>v;
+  for(int i = 1;i<n;i++){
+    v.push_back(i);
+  }
+  for(int i =0;i<n;i++){
+    if(p[i]==0){
+        p[i] = v[i];
+    }
+  }
+  int n = p.size();
+  int l = 0,r = n-1;
+
+  while (l < n && p[l] == v[l]) l++;
+    while (r >= 0 && p[r] == v[r]) r--;
+
+    if (l >= r){
+        cout << 0 <<endl;
+        return;
+    }
+
+    cout << (r - l + 1)<<endl;
+
   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     
 }
