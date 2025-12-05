910e46b9d63e099b00eba98f9375b5cf9893a3fc
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-12-05 17:01:00 +0530

    Auto-sync: 2025-12-05 17:01:00

--- 2143B.cpp
+++ 2143B.cpp
@@ -8,8 +8,8 @@
 void solve() {
     
     int n,k;cin>>n>>k;
-    vector<int>a(n);for(int &in:a)cin>>in;
-    vector<int>b(k);for(int &in:b)cin>>in;
+    vector<long long >a(n);for(long long  &in:a)cin>>in;
+    vector<long long >b(k);for(long long  &in:b)cin>>in;
     if(a.size()==1 && b.size()==1){
         cout << 0 << endl;
         return;  
@@ -17,8 +17,8 @@ void solve() {
     sort(a.begin(),a.end(),greater<int>());
     sort(b.begin(),b.end());
     int ptr = 0;
-    int total_free = 0;
-    int sum_prices = accumulate(a.begin(),a.end(),0);
+    long long total_free = 0;
+    long long  sum_prices = accumulate(a.begin(),a.end(),0LL);
 
     for(int i = 0;i<b.size();i++){
         if(ptr+b[i]>n) break;
