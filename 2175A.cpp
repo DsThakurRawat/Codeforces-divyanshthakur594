7b794911701117952c5126ab978cc89fec1792e2
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-12-06 13:55:00 +0530

    Auto-sync: 2025-12-06 13:55:00

---
+++ 2175A.cpp
@@ -0,0 +1,33 @@
+
+// simple template-test2
+// code by divyansh rawat
+
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long 
+;
+void solve() {
+    ll n;cin>>n;
+    vector<ll>a(n);for(ll &in:a)cin>>in;
+    ll dif = 1e18-(n+1);
+    cout << dif<<endl;
+
+
+  
+
+    
+}
+  
+
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t ;
+    t=1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
