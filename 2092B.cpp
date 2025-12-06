0d67c76b23b4d325cdd3554afd1d76625fe04886
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-12-06 22:38:47 +0530

    Auto-sync: 2025-12-06 22:38:47

--- 2092B.cpp
+++ 2092B.cpp
@@ -1,70 +1,47 @@
 
 // simple template-test2
 // code by divyansh rawat
-
 #include <bits/stdc++.h>
 using namespace std;
-using ll = long long 
-;
+
 void solve() {
-    ll n;cin>>n;
-    string s1,s2;cin>>s1>>s2;
-   
-   string zigzag_1,zigzag_2;
-   for(int i = 0;i<n;i++){
-    if(i%2 == 0){
-        zigzag_1 = zigzag_1+s1[i];
+    int n;
+    cin >> n;
+
+    string a, b;
+    cin >> a >> b;
+
+    int z1 = 0, z2 = 0;
+
+    for (int i = 0; i < n; i++) {
+      
+        if (i % 2 == 0) {
+            if (a[i] == '0') z1++;   
+        } else {
+            if (b[i] == '0') z1++;    
+        }
+
+       
+        if (i % 2 == 0) {
+            if (b[i] == '0') z2++;  
+        } else {
+            if (a[i] == '0') z2++;   
+        }
     }
-    if(i%2!=0){
-        zigzag_1 = zigzag_1 + s2[i];
-    }
-   }
-   for(int i = 0;i<n;i++){
-    if(i%2!=0){
-        zigzag_2 = zigzag_2+s1[i];
-    }
-    if(i%2==0){
-        zigzag_2 = zigzag_2+s2[i];
-    }
-   }
-   int z1 = 0;
-   int z2 = 0;
-   for(int i = 0;i<zigzag_1.size();i++){
-    if(zigzag_1[i]=='0') z1++;
-    if(zigzag_2[i]=='0')z2++;
-   }
-    int need1 = (n + 1) / 2;   
-    int need2 = n / 2;       
-   if(z1>=need1 && z2>=need2){
-    cout << "YES"<<endl;
-   }
-   else{
-    cout <<"No"<<endl;
-   }
 
-
-
-
-
-   
-
-
-
-    
-
-    
+    int need1 = (n + 1) / 2;  
+    int need2 = n / 2;         
+    if (z1 >= need1 && z2 >= need2)
+        cout << "YES\n";
+    else
+        cout << "NO\n";
 }
-  
-
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    int t ;
-    t=1;
+
+    int t;
     cin >> t;
-    while (t--) {
-        solve();
-    }
-    return 0;
+    while (t--) solve();
 }
