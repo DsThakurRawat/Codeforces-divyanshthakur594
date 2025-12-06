0b9c6598efd4c1eee3e20f68be42e58f9cf6b5d8
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-12-06 22:27:39 +0530

    Auto-sync: 2025-12-06 22:27:39

--- 131A.cpp
+++ 131A.cpp
@@ -33,11 +33,11 @@ void solve() {
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    int t ;
-    t=1;
-    cin >> t;
-    while (t--) {
+ //   int t ;
+   // t=1;
+   // cin >> t;
+   // while (t--) {
         solve();
-    }
+  //  }
     return 0;
 }
