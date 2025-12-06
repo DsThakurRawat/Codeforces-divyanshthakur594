d850e7c5d8f2e1b85d5c26bdc7d69bcb39fc3e89
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-12-06 18:28:40 +0530

    Auto-sync: 2025-12-06 18:28:40

--- 2092B.cpp
+++ 2092B.cpp
@@ -10,33 +10,42 @@ void solve() {
     ll n;cin>>n;
     string s1,s2;cin>>s1>>s2;
    
-    string z1 , z2;
-    for(int i = 0;i<n-1;i++){
-        z1 = s1[i]+z1;
-        z1 = z1+s2[i+1];
+   string zigzag_1,zigzag_2;
+   for(int i = 0;i<n;i++){
+    if(i%2 == 0){
+        zigzag_1 = zigzag_1+s1[i];
+    }
+    if(i%2!=0){
+        zigzag_1 = zigzag_1 + s2[i];
+    }
+   }
+   for(int i = 0;i<n;i++){
+    if(i%2!=0){
+        zigzag_2 = zigzag_2+s1[i];
+    }
+    if(i%2==0){
+        zigzag_2 = zigzag_2+s2[i];
+    }
+   }
+   int z1 = 0;
+   int z2 = 0;
+   for(int i = 0;i<zigzag_1.size();i++){
+    if(zigzag_1[i]=='0') z1++;
+    if(zigzag_2[i]=='0')z2++;
+   }
+   int requirement = (n+1)/2;
+   if(z1>=requirement && z2>=requirement){
+    cout << "YES"<<endl;
+   }
+   else{
+    cout <<"No"<<endl;
+   }
 
-    }
-    for(int i = 0;i<n-1;i++){
-        z2 = s2[i]+z2;
-        z2 = z2+s1[i+1];
 
-    }
-    ll z_1 = 0,z_2 = 0;
-    for(int i =0;i<n;i++){
-        if(z1[i]=='0')z_1++;
-    }
-    for(int i = 0;i<n;i++){
-        if(z2[i]=='0')z_2++;
 
-    }
-    ll need1 = ceil(n/2);
-    ll need2 = ceil(n/2);
-    if(z_1>=need1 && z_2>=need2){
-        cout<<"YES"<<endl;
-    }
-    else {
-        cout << "NO"<<endl;
-    }
+
+
+   
 
 
 
