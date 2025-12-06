f773c91e8e8df61ba31be3030bd761cf2d813559
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-12-06 23:51:19 +0530

    Auto-sync: 2025-12-06 23:51:19

---
+++ 2090B.cpp
@@ -0,0 +1,28 @@
+
+// simple template-test2
+// code by divyansh rawat
+
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long 
+;
+void solve() {
+  ll n,m;cin>>n>>m;
+  
+
+    
+}
+  
+
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t ;
+    t=1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
