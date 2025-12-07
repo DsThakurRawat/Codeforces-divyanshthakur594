2eb11e44cda518c48cf60d3e7fdfa3cadae3fa43
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-12-07 14:54:03 +0530

    Auto-sync: 2025-12-07 14:54:03

--- 2090B.cpp
+++ 2090B.cpp
@@ -8,6 +8,41 @@ using ll = long long
 ;
 void solve() {
   ll n,m;cin>>n>>m;
+  vector<string>grid(n);
+  for(int i = 0;i<n;i++){
+    cin>>grid[i];
+  }
+  // he can push any number of balls
+
+  vector<bool>checkrow(n,false),checkcol(m,false);
+
+  for(int i = 0;i<n;i++){
+    for(int j = 0;j<m;j++){
+      if(grid[i][j]=='0')checkrow[i]=true;
+    }
+  }
+  for(int j = 0;j<m;j++)
+     for(int i = 0;i<n;i++){
+      if(grid[i][j]=='0') checkcol[j] = true;
+     }
+
+
+
+
+
+  for(int i = 0;i<n;i++){
+    for(int j =0;j<m;j++){
+      if(grid[i][j]==1){
+
+                if(checkcol[i]&&checkcol[j]){
+                  cout << "NO"<<endl;
+                  return;
+                }
+      }
+    }
+  }
+  cout << "YES"<<endl;
+
   
 
     
