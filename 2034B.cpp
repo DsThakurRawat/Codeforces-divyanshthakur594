927315b04c5135281385ff88382708589e3e8c9b
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-12-07 21:21:30 +0530

    Auto-sync: 2025-12-07 21:21:30

--- 2034B.cpp
+++ 2034B.cpp
@@ -9,15 +9,42 @@ void solve() {
   
    ll n,m,k;cin>>n>>m>>k;
    string s;cin>>s;
-   ll r = 0;
-   int ops = 0;
-
-   for(int i = 0;i<s.size(); i++){
-    while(r<=i){
-        
-
+   ll i = 0;
+   ll ops = 0;
+    vector<int>a(n);
+    for(int i = 0;i<n;i++){
+        a[i]=(s[i]=='1');
     }
-   }
+    while(i<=n-m){
+        bool allzero = true;
+        for(int j = i;j<i+m;j++){
+            if(a[j]==1){
+                allzero = false;
+                break;
+            }
+        }
+        if(allzero){
+            ops++;
+            ll lastindex = i+m-1;
+            ll l = max(0LL,lastindex-k+1);
+            int rem = lastindex;
+            for(int x = l ;x<=rem;x++){
+                a[x]=1;
+                i = rem+1;
+            }
+        }
+        else{
+            i++;
+        }
+    }
+    cout << ops << endl;
+  
+
+
+
+
+
+
 
    
 
