90ad40eb11fc4510afc393a45a888cb8fff1ff5d
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-12-07 23:52:39 +0530

    Auto-sync: 2025-12-07 23:52:39

--- 1999B.cpp
+++ 1999B.cpp
@@ -7,31 +7,11 @@ using namespace std;
 using ll = long long;
 void solve() {
   int a1,a2,b1,b2;cin>>a1>>a2>>b1>>b2;
-  // a1.a2->card sunnet had
-  // b1,b2 ->card slavic has
-  ll a11 = a1,a12 = a2;
-  ll a21 = a2,a22 = a1;
-  ll b11 = b1,b12 = b2;
-  ll b21 = b2,b22 = b1;
-
-  ll count = 0;
-  bool  res1 = (a11>b11&&(a12>b12));// game1
-  bool res2 = (a11>b21 && a12>b22);// game2
-  bool res3 = (a21>b11&&a22>b12);//// game3
-  bool res4 = (a21>b21&&a22>b22);//game 4;
-  if(res1&&res2)count+=1;
-  if(res3&&res4)count+=1;
-  if(res1&&res3)count+=1;
-  if(res1&&res4)count+=1;
-
- 
-  if(res2&&res3)count+=1;
-  if(res2&&res4)count+=1;
-
- 
+   ll ans = 0;
+        if(a1>b1 && a2>=b2 || a1>=b1 && a2>b2) ans+=2;
+        if(a1>b2 && a2>=b1 || a1>=b2 && a2>b1) ans+=2;
+        cout<<ans<<endl;
   
-  
-  cout << count<<endl;
    
 
 
