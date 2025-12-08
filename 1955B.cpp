a3969b1094bf66f4e59500a7b7dde43c55b5cec8
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-12-08 20:23:39 +0530

    Auto-sync: 2025-12-08 20:23:39

--- 1955B.cpp
+++ 1955B.cpp
@@ -7,7 +7,11 @@ using namespace std;
 using ll = long long 
 ;
 void solve() {
-  int n,c,d;cin>>n>>c>>d;
+  ll n,c,d;cin>>n>>c>>d;
+  vector<ll>b(n*n);for(ll i = 0;i<n*n;i++)
+  vector<vector<int>>mat(n,vector<int>(n));
+  
+
 
     
 }
