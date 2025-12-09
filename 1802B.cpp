5bb9570a9be01f6003195aeb03d8349a92dcc9cd
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-12-09 18:51:30 +0530

    Auto-sync: 2025-12-09 18:51:30

---
+++ 1802B.cpp
@@ -0,0 +1,45 @@
+
+// simple template-test2
+// code by divyansh rawat
+
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+void solve(){
+   int n;cin>>n;
+   vector<ll>a(n+1);for(int i = 1;i<=n;i++){
+    cin>>a[i];
+   }
+   // ai = gcd(ai,i);
+   // gcd(a[i],1)==1
+   // gcd(a,b)==1
+   /*if a and b are coprime 
+   //like i have given list of array if it contains 1 then over all gcd of array is one
+   
+   */
+    for(ll i =1;i<=n;i++){
+        for(ll j = i;j<=i;j++){
+            if(gcd(a[i],a[j])==1){
+                cout << 0 << endl;
+            }
+        }
+    }
+   
+
+
+    
+}
+  
+
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t ;
+    t=1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
