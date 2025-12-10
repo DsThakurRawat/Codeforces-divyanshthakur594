d7fee02d4618cafc310341367ffa2c5a917b1417
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-12-10 11:47:31 +0530

    Auto-sync: 2025-12-10 11:47:31

---
+++ 1766B.cpp
@@ -0,0 +1,43 @@
+
+// simple template-test2
+// code by divyansh rawat
+
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+void solve(){
+
+    int n;cin>>n;
+    string s;cin>>s;
+    // let say x is subtring of s and x is repeating in s if length of x >=2 then possiblity ->yes else no
+    set<string>st;
+    bool ok = false;
+
+    for(int i =0;i<n-1;i++){
+        string p = s.substr(i,2);
+        if(st.count(p)){
+            cout << "YES"<<endl;
+            return;
+        }
+        st.insert(p);
+    }
+    
+
+  
+
+    
+}
+  
+
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t ;
+    t=1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
