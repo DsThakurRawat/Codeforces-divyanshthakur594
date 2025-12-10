3a64e514815c45460fd59b7dd0db860c0c010315
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-12-10 16:29:48 +0530

    Auto-sync: 2025-12-10 16:29:48

--- 1721B.cpp
+++ 1721B.cpp
@@ -26,25 +26,7 @@ using  int128 = __int128_t;
 void solve() {
   int n,m,sx,sy,d;cin>>n>>m>>sx>>sy>>d;
   // n , m points should be away from laser
-  if((abs(n-sx)+abs(m-sy))<=d){
-    cout << -1 << endl;
-    return;
-  }
-  vector<vector<ll>>grid(n+1,vector<ll>(m+1,1));
-
-  for(int i = 1;i<=n;i++){
-    for(int j =1;j<=m;j++){
-        if((abs(i-sx)+abs(j-sy))<=d){
-            grid[i][j]=-1;
-        }
-
-    }
-  }
-  if(grid[n][m-1]==-1 && grid[n-1][m]==-1){
-    cout << -1 << endl;
-    return;
-  }
-  if(grid[2][1]==-1 && grid[1][2]== -1){
+  if(((sx-d<=1) && (sx+d>n) ) || ((sy-d<=1) && (sy+d>=m))){
     cout << -1 << endl;
     return;
   }
