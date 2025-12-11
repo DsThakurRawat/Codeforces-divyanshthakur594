c215c0d4a746dde4069bf8c27d08f0cf11720850
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-12-11 20:12:11 +0530

    Auto-sync: 2025-12-11 20:12:11

--- 2176A.cpp
+++ 2176A.cpp
@@ -28,7 +28,7 @@ void solve() {
     vector<int>v(n);for(auto &in:v)cin>>in;
     ll cnt=0;
   for(ll i=0;i<n;i++){
-    for(ll j=n-1;j>=0;j--){
+    for(ll j=n-1;j>=i;j--){
         if(v[i]>v[j] && i <j)cnt++;
     }
   }
