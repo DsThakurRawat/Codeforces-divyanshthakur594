dc0312828cc117635b83f14e0d5f42cefa0efde8
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-12-12 09:58:15 +0530

    Auto-sync: 2025-12-12 09:58:15

---
+++ 1430C.cpp
@@ -0,0 +1,54 @@
+
+// simple template-test2
+// code by divyansh rawat
+
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using  ld = long double;
+using  int128 = __int128_t;
+
+
+//vector<vector<ll>> grid(n, vector<int>(m));
+//vector<vector<ll>> arr(n, vector<int>(m,1));
+//indexes to run from 1…n and 1…m, not from 0.
+//vector<vector<ll>> grid(n + 1, vector<ll>(m + 1, 1));
+
+
+
+
+
+#define nl '\n'
+
+
+void solve() {
+    int n;cin>>n;
+    vector<ll>v;
+    
+    
+
+
+
+    ll ans = (n+1)/2;
+   
+
+   
+
+
+
+}
+  
+
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t ;
+    t=1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
