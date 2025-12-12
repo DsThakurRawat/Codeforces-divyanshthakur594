b0b7a6cb75dafdbb7d9c532fdb6a2d2ec7635427
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-12-12 09:49:28 +0530

    Auto-sync: 2025-12-12 09:49:28

---
+++ 2176B.cpp
@@ -0,0 +1,53 @@
+
+// simple template-test2
+// code by divyansh rawat
+
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using  ld = long double;
+using  int128 = __int128_t;
+
+
+//vector<vector<ll>> grid(n, vector<int>(m));
+//vector<vector<ll>> arr(n, vector<int>(m,1));
+//indexes to run from 1…n and 1…m, not from 0.
+//vector<vector<ll>> grid(n + 1, vector<ll>(m + 1, 1));
+
+
+
+
+
+#define nl '\n'
+
+
+void solve() {
+    int n;cin>>n;
+    string s;cin>>s;
+    ll curr = 0;
+    ll maxi=curr;
+
+    for(ll i=0;i<n;i++){
+        if(s[i]=='0')curr++;
+        else curr=0;
+        maxi=max(curr,maxi);
+    }
+cout << maxi << nl;
+
+
+}
+  
+
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t ;
+    t=1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
