f7d299ef05f32cea4853f00d8118852048772473
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-12-13 16:12:51 +0530

    Auto-sync: 2025-12-13 16:12:51

---
+++ 1562B.cpp
@@ -0,0 +1,72 @@
+
+// simple template-test2
+// code by divyansh rawat
+
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using  ld = long double;
+using  int128 = __int128_t;
+
+
+//vector<vector<ll>> grid(n, vector<int>(m));
+//vector<vector<ll>> arr(n, vector<int>(m,1));
+//indexes to run from 1…n and 1…m, not from 0.
+//vector<vector<ll>> grid(n + 1, vector<ll>(m + 1, 1));
+auto checkprime=[&](ll a){
+                if (a <= 1) return false;
+                if (a <= 3) return true;
+                if (a % 2 == 0 || a % 3 == 0) return false;
+
+                for (ll i = 5; i * i <= a; i += 6) {
+                    if (a % i == 0 || a % (i + 2) == 0)
+                        return false;
+                }
+                return true;
+
+           };
+
+
+
+
+#define nl '\n'
+
+
+void solve() {
+           ll k;cin>>k;
+           ll n;cin>>n;
+           string num = to_string(n);
+
+           auto checkprime=[&](ll a){
+                if (n <= 1) return false;
+                if (n <= 3) return true;
+                if (n % 2 == 0 || n % 3 == 0) return false;
+
+                for (ll i = 5; i * i <= n; i += 6) {
+                    if (n % i == 0 || n % (i + 2) == 0)
+                        return false;
+                }
+                return true;
+
+           };
+
+           for(ll i=0;i<num.size();i++){
+                 bool checkprime(num[i]);
+           }
+
+}
+  
+
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t ;
+    t=1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
