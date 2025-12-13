c0422fcedccb4b76e65e98a5d7c05fb00657a360
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-12-13 22:19:02 +0530

    Auto-sync: 2025-12-13 22:19:02

--- test2.cpp
+++ test2.cpp
@@ -80,41 +80,7 @@ NOTE-3 :
 void solve() {
 
 
-           ll k;cin>>k;
-           ll n;cin>>n;
-           string s = to_string(n);
-           vector<bool>prime(100);
-           prime[0] = prime[1] = false;
-
-        
-
-           for(int i =2;i<100;i++){
-            prime[i]=true;
-            for(int j=2;j*j<=i;j++){
-                if(i%j==0) prime[i]=false;
-            }
-           }
-
-           for(int i =0;i<s.size();i++){
-            if(s[i]=='1'||s[i]=='4'|| s[i]=='6'|| s[i]=='8'||s[i]=='9'){
-                cout << 1 << nl;
-                cout << s[i]<<nl;
-                return;
-            }
-           }
-
-           for (int i = 0; i < s.size(); i++) {
-              for (int j = i + 1; j < s.size(); j++) {
-
-                int val = (s[i] - '0') * 10 + (s[j] - '0');
-
-                if (!prime[val]) {
-                    cout << 2 << nl;
-                    cout << s[i] << s[j] << nl;
-                return;
-            }
-        }
-    }
+         
 
 
 
