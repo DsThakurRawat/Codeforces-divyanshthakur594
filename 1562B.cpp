74442305fa9c2afd45e10c4262165532e8cd8cfd
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-12-13 21:54:51 +0530

    Auto-sync: 2025-12-13 21:54:51

--- 1562B.cpp
+++ 1562B.cpp
@@ -74,7 +74,10 @@ void solve() {
            for(int i =2;i<100;i++){
             prime[i]=true;
             for(int j=2;j*j<=i;j++){
-                if(i%j==0) prime[i]=false;
+                if(i%j==0){
+                 prime[i]=false;
+                  break;
+                }
             }
            }
 
