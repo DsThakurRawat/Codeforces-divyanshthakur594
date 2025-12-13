b2eaf5ec1e5ffd208104b101aa5af76c9830dd92
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-12-13 23:08:16 +0530

    Auto-sync: 2025-12-13 23:08:16

--- 1598B.cpp
+++ 1598B.cpp
@@ -37,7 +37,7 @@ void solve() {
 
 
             for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
+            for (int j = 0; j < n; j++) {
                 cin >> grid[i][j];
             }
         }
