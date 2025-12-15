a7c70e40c0af450019f5da7b4568049323e165eb
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-12-15 16:10:53 +0530

    Auto-sync: 2025-12-15 16:10:53

--- 1907C.cpp
+++ 1907C.cpp
@@ -280,9 +280,9 @@ void solve() {
     int n;cin>>n;
     string s;cin>>s;
 
-    for(int i =0;i<n-1;i++){
-        if(s[i]==s[i+1])continue;
-        if(s[i]!=s[i+1]){
+    for(int i =1;i<n;i++){
+        if(s[i-1]==s[i])continue;
+        if(s[i-1]!=s[i]){
 
             s.erase(i,2);
 
