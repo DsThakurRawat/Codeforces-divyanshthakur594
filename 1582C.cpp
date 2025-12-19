f143d626c1ded276d188ded08687ea063ad0229e
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-12-19 18:59:55 +0530

    Auto-sync: 2025-12-19 18:59:55

--- 1582C.cpp
+++ 1582C.cpp
@@ -334,7 +334,40 @@ void solve() {
      reverse(t.begin(),t.end());
      if(s==t){
         cout << 0 << nl;
+        return;
      }
+     int ans = n+1;
+
+     for(int c=0;c<26;c++){
+        int l =0,r=n-1,cnt =0;
+        while(l<=r){
+           if(s[l]==s[r]){
+            l++,r--;
+           }
+           else if(s[l]==char('a'+c)){
+            cnt++,l++;
+           }
+           else if(s[r]==char('a'+c)){
+            cnt++,r--;
+           }
+           else{
+            cnt = n+1;
+            break;
+           }
+        }
+        ans = min(ans,cnt);
+     }
+
+     if(ans == n+1)ans = -1;
+     cout << ans << nl;
+
+     
+
+     
+
+
+
+
 
 
      
