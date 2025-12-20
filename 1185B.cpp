f19ad0dd7d018cbcb750ea569c10513466999261
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-12-20 23:28:10 +0530

    Auto-sync: 2025-12-20 23:28:10

--- 1185B.cpp
+++ 1185B.cpp
@@ -374,13 +374,13 @@ pair<int, int> a[N];
 
 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    int t ;
-    t=1;
-    cin >> t;
-    while (t--) {
+  //  ios::sync_with_stdio(false);
+   // cin.tie(nullptr);
+   // int t ;
+   //// t=1;
+    //cin >> t;
+    //while (t--) {
         solve();
     }
-    return 0;
-}
+    //return 0;
+
