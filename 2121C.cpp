1c31c6fb1df82a5f2aa64e672fa87913ae6d2c11
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-12-22 21:01:21 +0530

    Auto-sync: 2025-12-22 21:01:21

--- 2121C.cpp
+++ 2121C.cpp
@@ -331,8 +331,7 @@ void solve() {
 
     int n,m;cin>>n>>m;
     vector<vector<int>>grid(n,vector<int>(m));
-    vector<int>all_values;
-
+    
     for(int i =0;i<n;i++){
         for(int j =0;j<m;j++){
             cin>>grid[i][j];
