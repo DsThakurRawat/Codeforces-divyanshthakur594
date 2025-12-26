0ed85b93a8bb76d0872f64f14718542132bb3460
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-12-26 23:54:27 +0530

    Auto-sync: 2025-12-26 23:54:27

--- 2021B.cpp
+++ 2021B.cpp
@@ -384,6 +384,33 @@ void solve() {
      */
    
 
+    int n, k, s;
+    cin >> n >> k >> s;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++) cin >> a[i];
+ 
+    vector<int> b(k);
+    for (int i = 0; i < k; i++) cin >> b[i];
+ 
+    int ans = 0;
+    for (int i = 0; i < s && i <= 2 * n; i++)
+    {
+        int cur = 0;
+        for (int j = 0; j < n; j++)
+        {
+            cur += (a[j] == j + 1);
+        }
+        cur += (s - i - 1) / 2;
+        if (cur > ans)
+        {
+            ans = cur;
+        }
+        for (int j = 0; j < b[i % k]; j++)
+        {
+            a[j]++;
+        }
+    }
+    cout << ans << endl;
       
           
 
