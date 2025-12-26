0f2fad35c3df06c0c5329de1f3da17bd6bf3863e
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-12-26 23:54:55 +0530

    Auto-sync: 2025-12-26 23:54:55

--- 2021B.cpp
+++ 2021B.cpp
@@ -393,20 +393,16 @@ void solve() {
     for (int i = 0; i < k; i++) cin >> b[i];
  
     int ans = 0;
-    for (int i = 0; i < s && i <= 2 * n; i++)
-    {
+    for (int i = 0; i < s && i <= 2 * n; i++) {
         int cur = 0;
-        for (int j = 0; j < n; j++)
-        {
+        for (int j = 0; j < n; j++){
             cur += (a[j] == j + 1);
         }
         cur += (s - i - 1) / 2;
-        if (cur > ans)
-        {
+        if (cur > ans){
             ans = cur;
         }
-        for (int j = 0; j < b[i % k]; j++)
-        {
+        for (int j = 0; j < b[i % k]; j++){
             a[j]++;
         }
     }
