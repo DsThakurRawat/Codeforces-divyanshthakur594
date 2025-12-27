11b3a2f523056a09b6d90c9835a7d61f7e1551ce
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-12-27 23:34:54 +0530

    Auto-sync: 2025-12-27 23:34:54

--- 2117D.cpp
+++ 2117D.cpp
@@ -385,7 +385,21 @@ void solve() {
 
 
 
+    int n, x;
+    cin >> n >> x;
 
+    int l = 1e5, r = -1;
+    for(int i = 0; i < n; i++) {
+        int door;
+        cin >> door;
+
+        if(door == 1) {
+            l = min(l, i);
+            r = max(r, i);
+        }
+    }
+
+    cout << (x >= r - l + 1 ? "YES" : "NO") << endl;
 
 
 
