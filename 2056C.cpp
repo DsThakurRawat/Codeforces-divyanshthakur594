b7270f4873ff777438e7261613fd444c1cdb5ab3
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-12-29 23:46:12 +0530

    Auto-sync: 2025-12-29 23:46:12

--- 2056C.cpp
+++ 2056C.cpp
@@ -382,28 +382,6 @@ void solve() {
      ans strength pi
 
      */
-  string s;
-    cin>>s;
-    int last=0,c2=0,c3=0,inv2=5;
-    for(auto& it:s)
-    {
-        int ch=it-'0';
-        last=(last+ch)%9;
-        if(ch==2)c2++;
-        if(ch==3)c3++;
-    }
-    int M=(-last+9)%9;
-    for(int i=0;i<3;++i)
-    {
-        int res=(6*i)%9;
-        int P=(M-res+9)%9,Q=(inv2*P)%9;
-        if(Q<=c2 && i<=c3)
-        {
-            cout<<"YES"<<endl;
-            return;
-        }
-    }
-    cout<<"NO"<<endl;
 
             
         
