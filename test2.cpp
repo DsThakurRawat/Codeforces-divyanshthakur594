f0d34f20066173766e699ccfaf8930836e0138ef
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-12-30 23:19:38 +0530

    Auto-sync: 2025-12-30 23:19:38

--- test2.cpp
+++ test2.cpp
@@ -388,7 +388,7 @@ void solve() {
         for (auto &i: a) {
             for (auto &j: i) cin >> j;
         }
-        if (n * m == 1) cout << "-1\n";
+        if (n * m == 1) cout << "-1"<<nl;
         else {
             for (int i = 0; i < n; ++i) {
                 for (int j = 0; j < m; ++j) {
