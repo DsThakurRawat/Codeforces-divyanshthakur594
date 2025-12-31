61b3568858ac8b95d2a13ffecb878163ed6a1dce
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-12-31 21:50:06 +0530

    Auto-sync: 2025-12-31 21:50:06

--- test2.cpp
+++ test2.cpp
@@ -420,11 +420,11 @@ void solve() {
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-   // int t ;
-   // t=1;
-   // cin >> t;
-  //  while (t--) {
-        solve();
-   // }
+    int t ;
+    t=1;
+    cin >> t;
+    while (t--) {
+     solve();
+    }
     return 0;
 }
