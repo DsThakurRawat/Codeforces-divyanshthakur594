375604a56a2dc09108b8655a451e12df186338a8
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-12-31 21:43:40 +0530

    Auto-sync: 2025-12-31 21:43:40

--- 2020B.cpp
+++ 2020B.cpp
@@ -355,6 +355,55 @@ double dist = hypot(x2 - x1, y2 - y1);
                 Condition: Always
 
 ==============================================================
+ //////////////////////////////////////////////////////////////
+
+ ===========lambda function====================================
+       // Lambda reverse fn
+        auto rev = [&](int a) {
+            int b = 0;
+            while (a > 0) {
+                b = b * 10 + a % 10;
+                a /= 10;
+            }
+            return b;
+        };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 */
 
 
@@ -382,25 +431,10 @@ void solve() {
      ans strength pi
 
      */
-    ll k;cin>>k;
-    ll low = 0;
-    ll high = 2e18;
-    ll ans = 0;
 
-    while(low <= high){
-        ll mid = (low + high)/2;
-        if(pred(mid,k)){
-            ans = mid;
-            high = mid-1;
-        }
-        else low = mid + 1;
-    }
-    cout << ans << nl;
-
-
-
-   
-
+     long long k;cin>>k;
+     cout << k + int(sqrtl(k) + 0.5) << nl;
+  
 
 
             
