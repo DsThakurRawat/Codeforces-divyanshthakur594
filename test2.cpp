1529fd182f198f274fd1daf31e7b63fb2928b29a
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-12-31 21:50:57 +0530

    Auto-sync: 2025-12-31 21:50:57

--- test2.cpp
+++ test2.cpp
@@ -383,25 +383,7 @@ void solve() {
 
      */
 
-    string s;
-    cin >> s;
-    int n = s.size();
-    vector<int>v(n, -1);
-    int p = -1;
- 
-    for (int i = n - 4; i >= 0; --i) {
-        if (s[i]=='b' && s[i+1]=='e' && s[i+2]=='a' && s[i+3]=='r') p = i;
-        v[i] = p;
-    }
- 
-    int ans = 0;
-    for (int i=0;i<n;i++) {
-        int x = v[i];
-        if (x != -1) ans += n - (x + 3);
-    }
- 
-    cout <<ans<<nl;
-
+    
 
             
         
