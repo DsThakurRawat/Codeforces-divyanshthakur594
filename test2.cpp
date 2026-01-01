432ff99e7b25d57092bea57cf2d98d7da293cb6a
DsThakurRawat <divyanshrawatofficial@gmail.com>
2026-01-01 16:57:30 +0530

    Auto-sync: 2026-01-01 16:57:30

--- test2.cpp
+++ test2.cpp
@@ -403,6 +403,8 @@ void solve() {
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
+    cout.tie(0);
+    auto begin = chrono::high_resolution_clock::now();
     int t ;
     t=1;
     cin >> t;
