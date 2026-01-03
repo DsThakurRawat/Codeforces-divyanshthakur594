3e70c4038e3c720e87d045daa6ae3fab3badeb15
DsThakurRawat <divyanshrawatofficial@gmail.com>
2026-01-03 18:30:01 +0530

    Auto-sync: 2026-01-03 18:30:01

--- test2.cpp
+++ test2.cpp
@@ -458,30 +458,25 @@ auto sq = [&] (ll x){return x*x;};
 
         
           */
-         int N;
-        cin >> N;
+         ll n;cin>>n;
+         vector<ll>ans;
+         ll cnt = 0;
 
-        vector<int> cnt(N + 1, 0);
+         for(ll i = 1;i*i<=n;i++){
 
-        int LIM = sqrt(N);
+             for(ll j = i+1;j*j+i*i<=n;j++){
+                  
+                    cnt++;
+                   ans.push_back(sq(i) + sq(j));
+                 
+             }
 
-        for (int x = 1; x <= LIM; x++) {
-            int xx = x * x;
-            for (int y = x + 1; ; y++) {
-                int s = xx + y * y;
-                if (s > N) break;
-                cnt[s]++;
-            }
-        }
+         }
+         cout << cnt << nl;
+         sort(ans.begin(),ans.end());
+         for(auto &ot :ans) cout << ot <<" ";
+         cout << nl;
 
-        vector<int> ans;
-        for (int i = 1; i <= N; i++) {
-            if (cnt[i] == 1) ans.push_back(i);
-        }
-
-        cout << ans.size() << '\n';
-        for (int x : ans) cout << x << ' ';
-        cout << '\n';
 
 
 
