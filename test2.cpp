a8075b7d85cd02a54941e801b74ca745a9cd75fc
DsThakurRawat <divyanshrawatofficial@gmail.com>
2026-01-04 23:15:24 +0530

    Auto-sync: 2026-01-04 23:15:24

--- test2.cpp
+++ test2.cpp
@@ -444,12 +444,12 @@ double dist = hypot(x2 - x1, y2 - y1);
 void solve() {
 
     ll n;cin>>n;
-    ll ans = n;
+    ll ans = LLONG_MAX;
     for(ll a = 0;a<3;a++){
         for(ll b = 0;b<2;b++){
             for(ll c = 0;c<5;c++){
                 for(ll d = 0;d<3;d++){
-                    ll res = n - a-3*b-6*c-10*d;
+                    ll res = n - a- 3*b- 6*c-10*d;
                     if(res >= 0 && res % 15 == 0){
                         ans = min(res , (res / 15) + a + b + c + d);
                     }
