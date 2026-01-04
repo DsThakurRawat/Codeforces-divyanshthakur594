f5b4c4670377ab548455a8a02ae5cc187a9b9bb2
DsThakurRawat <divyanshrawatofficial@gmail.com>
2026-01-04 18:49:50 +0530

    Auto-sync: 2026-01-04 18:49:50

--- 1982B.cpp
+++ 1982B.cpp
@@ -491,12 +491,12 @@ int main() {
     cin.tie(nullptr);
  
    
- //   int t ;
-   // t=1;
-    //cin >> t;
-    //while (t--) {
+    int t ;
+    t=1;
+    cin >> t;
+    while (t--) {
      solve();
-   // }
+    }
 
   
     return 0;
