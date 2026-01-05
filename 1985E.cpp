167c5c974b4ac9d8e7153a52cbea66cbc0de6525
DsThakurRawat <divyanshrawatofficial@gmail.com>
2026-01-05 19:51:50 +0530

    Auto-sync: 2026-01-05 19:51:50

--- 1985E.cpp
+++ 1985E.cpp
@@ -385,6 +385,23 @@ void solve() {
 
      */
     ll x,y,z,k;cin>>x>>y>>z>>k;
+    int h, n; cin >> h >> n;
+		vector<int> a(n), c(n);
+		for(int& i: a) cin >> i;
+		for(int& i: c) cin >> i;
+		set<pair<long long, int>> S;
+		for(int i = 0; i < n; i++){
+			S.insert({1, i});
+		}
+		long long last_turn = 1;
+		while(h > 0){
+			auto [turn, i] = *S.begin();
+			S.erase(S.begin());
+			last_turn = turn;
+			h -= a[i];
+			S.insert({turn + c[i], i});
+		}
+		cout << last_turn << nl;
     
 
     
