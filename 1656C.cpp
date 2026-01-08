d5dd9587f358a332e57a69460a672e443e8f43d0
DsThakurRawat <divyanshrawatofficial@gmail.com>
2026-01-08 19:43:03 +0530

    Auto-sync: 2026-01-08 19:43:03

--- 1656C.cpp
+++ 1656C.cpp
@@ -552,18 +552,16 @@ void solve() {
         int i = lower_bound(a.begin(), a.end(), x) - a.begin();
         ll ans = LLONG_MAX;
 
-        // Case 1: take a[i-1] (need to increase to x)
+       
         if (i > 0) {
             ans = min(ans,
                       (x - a[i - 1]) +
                       max(0LL, y - (sum - a[i - 1])));
         }
 
-        // Case 2: take a[i] (already >= x or need small increase)
+       
         if (i < n) {
-            ans = min(ans,
-                      max(0LL, x - a[i]) +
-                      max(0LL, y - (sum - a[i])));
+            ans = min(ans, max(0LL, x - a[i]) + max(0LL, y - (sum - a[i])));
         }
 
         cout << ans << nl;
