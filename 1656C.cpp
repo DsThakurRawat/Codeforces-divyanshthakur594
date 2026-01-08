f4ade081a4fcc8c396ce0cd4d486b351bc78e909
DsThakurRawat <divyanshrawatofficial@gmail.com>
2026-01-08 19:41:44 +0530

    Auto-sync: 2026-01-08 19:41:44

--- 1656C.cpp
+++ 1656C.cpp
@@ -548,7 +548,8 @@ void solve() {
     int i = lower_bound(a.begin(), a.end(), x) - a.begin();
     ll ans = 2e18;
     if (i > 0) ans = min(ans, (x - a[i - 1]) + max(0LL, y - sum + a[i - 1]));
-    if (i < n) ans = min(ans, max(0LL, y - sum + a[i]));
+    if (i < n) ans = min(ans, max(0LL, x - a[i]) + max(0LL, y - (sum - a[i])));
+
     cout << ans << nl;
 
     
