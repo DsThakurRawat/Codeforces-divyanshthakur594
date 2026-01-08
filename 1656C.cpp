9192ceaf96713ece07dcb88d0348dd3713cd5774
DsThakurRawat <divyanshrawatofficial@gmail.com>
2026-01-08 19:42:50 +0530

    Auto-sync: 2026-01-08 19:42:50

--- 1656C.cpp
+++ 1656C.cpp
@@ -534,33 +534,42 @@ int main() {
 
 void solve() {
 
-  int n;cin >> n;
-  vector<ll> a(n);
-  for (auto &x : a) cin >> x;
-  int m;
-  cin >> m;
-  sort(a.begin(), a.end());
-  ll sum = accumulate(a.begin(), a.end(), 0LL);
+       int n; 
+    cin >> n;
+    vector<ll> a(n);
+    for (auto &x : a) cin >> x;
+
+    int m; 
+    cin >> m;
+
+    sort(a.begin(), a.end());
+    ll sum = accumulate(a.begin(), a.end(), 0LL);
+
+    while (m--) {
+        ll x, y;
+        cin >> x >> y;
+
+        int i = lower_bound(a.begin(), a.end(), x) - a.begin();
+        ll ans = LLONG_MAX;
+
+        // Case 1: take a[i-1] (need to increase to x)
+        if (i > 0) {
+            ans = min(ans,
+                      (x - a[i - 1]) +
+                      max(0LL, y - (sum - a[i - 1])));
+        }
+
+        // Case 2: take a[i] (already >= x or need small increase)
+        if (i < n) {
+            ans = min(ans,
+                      max(0LL, x - a[i]) +
+                      max(0LL, y - (sum - a[i])));
+        }
+
+        cout << ans << nl;
+
   
-  while (m--) {
-    ll x, y;
-    cin >> x >> y;
-    int i = lower_bound(a.begin(), a.end(), x) - a.begin();
-    ll ans = 2e18;
-    if (i > 0) ans = min(ans, (x - a[i - 1]) + max(0LL, y - sum + a[i - 1]));
-    if (i < n) ans = min(ans, max(0LL, x - a[i]) + max(0LL, y - (sum - a[i])));
-
-    cout << ans << nl;
-
-    
-                
-
-  }
-
-
-    
-
-
+    }
          
 
 
