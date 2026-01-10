69ae900b9498b1860417be44c1172979817b1196
DsThakurRawat <divyanshrawatofficial@gmail.com>
2026-01-10 11:56:53 +0530

    Auto-sync: 2026-01-10 11:56:53

--- 1734C.cpp
+++ 1734C.cpp
@@ -551,7 +551,7 @@ void solve() {
     string b;cin>>b;
     string s,t;
     for(ll i = 1;i<=n;i++){
-        s +=string(1,i);
+        s +=string(1,i+'0');
         if(b[i]=='1') t +=string(1,i);
     }
     cout << s << nl;
