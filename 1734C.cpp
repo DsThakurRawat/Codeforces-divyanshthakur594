4a7f68f39bf06b76e76edc484d8e57f90a2e102c
DsThakurRawat <divyanshrawatofficial@gmail.com>
2026-01-10 16:16:04 +0530

    Auto-sync: 2026-01-10 16:16:04

--- 1734C.cpp
+++ 1734C.cpp
@@ -552,10 +552,10 @@ void solve() {
 
         string b; 
         cin >> b;
-        vector<bool>alive(n),removed(n,false);
+        vector<bool>alive(n+1),removed(n+1,false);
 
-        for(int i =0;i<n;i++){
-            alive[i] = (b[i]=='1');
+        for(int i =1;i<=n;i++){
+            alive[i] = (b[i-1]=='1');
         }
         ll cost = 0;
 
