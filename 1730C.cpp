e7496e921df29ac79875bfcc69ca22227950820b
DsThakurRawat <divyanshrawatofficial@gmail.com>
2026-01-10 10:15:18 +0530

    Auto-sync: 2026-01-10 10:15:18

--- 1730C.cpp
+++ 1730C.cpp
@@ -538,27 +538,10 @@ void solve() {
     /*
     ai+i->should be perfect square
     */
+     string s;cin>>s;
      
      
-        int n; cin >> n;
-    
-      
-        vector<int> a(n);
-
-       
-
-            for (auto& x : a) cin >> x;
-            vector<int> b({a[n - 1]});
-            for (int i = n - 2; i >= 0; --i) {
-            if (a[i] > b.back()) {
-                b.push_back(a[i] % 10);
-                b.push_back(a[i] / 10);
-            } else {
-                b.push_back(a[i]);
-            }
-            }
-            cout << (is_sorted(b.rbegin(), b.rend()) ? "YES" : "NO") << '\n';
-
+     
         
    
     
