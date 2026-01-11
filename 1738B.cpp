5c67ebed5d1eb2026d4b7867c9641f1c8453336f
DsThakurRawat <divyanshrawatofficial@gmail.com>
2026-01-11 21:05:08 +0530

    Auto-sync: 2026-01-11 21:05:08

--- 1738B.cpp
+++ 1738B.cpp
@@ -556,7 +556,7 @@ void solve() {
     }
 
     for(int i = 0;i+1<n;i++){
-        if(!(nums[i]<=nums[i+1])){
+        if((nums[i]<=nums[i+1])){
                cout << "NO"<<nl;
                return;
         }
