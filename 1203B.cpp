0f01af3820ab41488325be182c8b5d761111bf79
DsThakurRawat <divyanshrawatofficial@gmail.com>
2026-01-16 19:11:48 +0530

    Auto-sync: 2026-01-16 19:11:48

--- 1203B.cpp
+++ 1203B.cpp
@@ -594,8 +594,8 @@ void solve() {
     for(int i = 0;i<k;i++)cin>>v[i];
     sort(v.begin(),v.end());
 
-    for(int i = 0;i<k;i++){
-        if(v[i] != v[i+1]){
+    for(int i = 0;i<k;i){
+        if(v[i] - v[i+1] !=0 ){
             cout << "NO"<<nl;
             return;
         }
