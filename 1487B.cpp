1472a152cba52a15fbf3a6ad2aaa3155b6dece49
DsThakurRawat <divyanshrawatofficial@gmail.com>
2026-01-24 09:51:39 +0530

    Auto-sync: 2026-01-24 09:51:39

--- 1487B.cpp
+++ 1487B.cpp
@@ -780,81 +780,20 @@ void solve(){
 }
 
 void MASTER(){
-        
 
-   
-    string x, y;
-    cin >> x >> y;
-    int n = x.size(), m = y.size();
-    vector<int> frq(27);
-
-    for(int i = 0; i < n; i++) frq[x[i] - 'a']--;
-    for(int i = 0; i < m; i++) frq[y[i] - 'a']++;
-
-    int bad = 0;
-    for(int i = 0; i < 26; i++) bad += (frq[i] < 0);
-
-    if(bad)
-    {
-        cout << "Impossible"<<nl;;
+    /*if n is even they never colllide */
+    ll n,k;cin>>n>>k;
+    if(n%2==0){
+        cout << (k-1)%n + 1 << nl;
         return;
     }
-
-    string jump = x, ans = "";
-    jump += '{';
-    x = jump;
-
-    for(int i = (int)x.size() - 2; i >= 0; i--)
-    {
-        if(x[i] == x[i + 1]) jump[i] = jump[i + 1];
-        else jump[i] = x[i + 1];
-    }
-
-    for(int i = 0; i < (int)x.size(); i++)
-    {
-        for(int j = 0; j < x[i] - 'a'; j++)
-            while(frq[j] > 0)
-            {
-                ans += (char)(j + 'a');
-                frq[j]--;
-            }
-
-        if(x[i] == '{') break;
-
-        if(jump[i] > x[i])
-            while(frq[x[i] - 'a'] > 0)
-            {
-                ans += x[i];
-                frq[x[i] - 'a']--;
-            }
-
-        ans += x[i];
-    }
-
-    cout << ans << nl;
-            
-        
-
-
+           
 
      
 
         
 
-
-      
-
-    
-                
-
-    
-
-
-    
-
-
-         
-
+   
 
 
            
