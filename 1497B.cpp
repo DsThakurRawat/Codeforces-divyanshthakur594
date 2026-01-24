0eba2a910b05c7da1623d55a7d0321fcae7a2f70
DsThakurRawat <divyanshrawatofficial@gmail.com>
2026-01-24 22:55:15 +0530

    Auto-sync: 2026-01-24 22:55:15

--- 1497B.cpp
+++ 1497B.cpp
@@ -645,7 +645,7 @@ void solve(){
        ll total_ans = 0;
        int flag = 0;
        for(int i =0;i<n;i++){
-        if(v2[i] =='0'){
+        if(v2[i] ==0){
             v2[i] = -1;
             v[i] = -1;
             flag = 1;
