ad681311ab74e22be3522b7669e2565ace3e7df2
DsThakurRawat <divyanshrawatofficial@gmail.com>
2026-01-26 21:08:53 +0530

    Auto-sync: 2026-01-26 21:08:53

--- 1537C.cpp
+++ 1537C.cpp
@@ -828,10 +828,10 @@ void MASTER() {
 
 
 
-
-   // ans[0] = h[0];
-   // ans[n-1] = h[1];
-
+   if(flag ==0){
+    ans[0] = h[0];
+    ans[n-1] = h[1];
+   }
 
     for(int i = 2; i < n; i++){
     ans[i-1] = h[i];
