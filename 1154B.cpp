cbc445a4c6ff4e7ecc1577fe7c2383d8757b23fe
DsThakurRawat <divyanshrawatofficial@gmail.com>
2026-01-27 18:30:48 +0530

    Auto-sync: 2026-01-27 18:30:48

--- 1154B.cpp
+++ 1154B.cpp
@@ -799,8 +799,8 @@ void MASTER() {
         return;
     }
     sort(all(v2));//for size is equal 3;
-    ll diff = abs(v[1] - v[0]);
-    ll diff2 = abs(v[2]-v[1]);
+    ll diff = abs(v2[1] - v2[0]);
+    ll diff2 = abs(v2[2]-v2[1]);
 
     if(diff==diff2){
         cout << diff << nl;
