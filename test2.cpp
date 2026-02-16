fd9fd56222f6f72ea83c7d1dd28b83b64b199706
DsThakurRawat <divyanshrawatofficial@gmail.com>
2026-02-16 18:11:03 +0530

    Auto-sync: 2026-02-16 18:11:03

--- test2.cpp
+++ test2.cpp
@@ -791,12 +791,12 @@ void MASTER(){
     vector<ll>a(n);
     for (int i = 0; i < n; i++) cin >> a[i];
  
-    int ans = 0;
+    int res = 0;
     for (int i = 1; i < n; i++){
-        if (a[i] + a[i-1] == 7 or a[i] == a[i-1]) ans++, i++;
+        if (a[i] + a[i-1] == 7 or a[i] == a[i-1])res++, i++;
     }
  
-    cout << ans<<nl;
+    cout << res<<nl;
    
   
 }
