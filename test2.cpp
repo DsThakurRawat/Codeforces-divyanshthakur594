067feff47b82b5212ffced74529f7e794590fc13
DsThakurRawat <divyanshrawatofficial@gmail.com>
2026-02-17 07:00:39 +0530

    Auto-sync: 2026-02-17 07:00:39

--- test2.cpp
+++ test2.cpp
@@ -791,15 +791,13 @@ void MASTER(){
 
 		int p[n + 5], ind = 1;
 
-		for( int i = 1; i <= n; i ++ )
-		{
+		for( int i = 1; i <= n; i ++ ){
 			cin >> p[i];
 		}
 		while( ind <= n && p[ind] == n - ind + 1 ) ind ++;
 		int id = -1;
 
-		for( int i = ind; i <= n; i ++ )
-		{
+		for( int i = ind; i <= n; i ++ ){
 			if( p[i] == n - ind + 1 ) id = i;
 		}
 		for( int i = 1; i < ind; i ++ ) cout << p[i] << ' ';
