4b91d62a388e076ed2b7dc21ec15b527d9fff066
DsThakurRawat <divyanshrawatofficial@gmail.com>
2026-02-18 17:16:08 +0530

    Auto-sync: 2026-02-18 17:16:08

--- test4.cpp
+++ test4.cpp
@@ -14,7 +14,21 @@ void solve() {
 }
 
 void MASTER() {
+    int n;
+    cin >> n;
     
+    ll a[n+1];
+    for (int i = 1; i <= n; i++) cin >> a[i];
+    
+    ll sum = 0;
+    for (int i = 2; i <= n; i++) sum += abs(a[i]-a[i-1]);
+    
+    ll ans = min(sum - abs(a[2]-a[1]), sum - abs(a[n]-a[n-1]));
+    for (int i = 2; i < n; i++) {
+        ans = min(ans, sum - abs(a[i+1]-a[i]) - abs(a[i]-a[i-1]) + abs(a[i+1]-a[i-1]));
+    }
+    
+    cout << ans << nl;
 }
 
 int main() {
