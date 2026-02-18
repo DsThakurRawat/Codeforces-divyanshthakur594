bec0244f50c95f4fbb260e568b36d53f45bdd4e4
DsThakurRawat <divyanshrawatofficial@gmail.com>
2026-02-18 17:15:05 +0530

    Auto-sync: 2026-02-18 17:15:05

---
+++ test4.cpp
@@ -0,0 +1,32 @@
+/*
+DIVYANSH RAWAT
+MASTER TEMPLATE
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+
+#define nl '\n'
+
+void solve() {
+    return;
+}
+
+void MASTER() {
+    
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+    while (t--) {
+        MASTER();
+        solve();
+    }
+
+    return 0;
+}
