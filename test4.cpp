81a4c7637f93185af9468f6ddc68c972c6919f67
DsThakurRawat <divyanshrawatofficial@gmail.com>
2026-02-21 22:27:19 +0530

    Auto-sync: 2026-02-21 22:27:19

--- test4.cpp
+++ test4.cpp
@@ -16,35 +16,7 @@ void solve() {
 }
 
 void MASTER() {
-     string s;
-    cin>>s;
-    int n=s.size();
-    if(n==1){
-        cout<<1<<nl;
-        return;
-    }
-    bool flag1=0,flag=0;
-    for(int i=0;i<n;i++){
-        if(s[i]=='>') flag1=1;
-        if((s[i]=='<' || s[i]=='*') && flag1) flag=1;
-        if(s[i]=='*') flag1=1;
-    }
-    if(flag){
-        cout<<-1<<nl;
-        return;
-    }
-    int cnt=0;
-    int cnt2=0;
-    for(int i=0;i<n;i++){
-        if(s[i]=='>') break;
-        cnt++;
-    }
-    for(int i=n-1;i>=0;i--){
-        if(s[i]=='<') break;
-        cnt2++;
-    }
-    cout<<max(cnt,cnt2)<<nl;
-    return;
+   
 }
 
 int main() {
